second example added, for non zero dirichlet BC flux computation is wrong.

CHT/partitioned_heat/fenicsfenics/heat.py
@@ -24,8 +24,10 @@ Heat equation with mixed boundary conditions. (Neumann problem)
   f = beta - 2 - 2*alpha
 """
 
-from __future__ import print_function
-from fenics import *
+from __future__ import print_function, division
+from fenics import Function, SubDomain, RectangleMesh, FunctionSpace, Point, Expression, Constant, DirichletBC, \
+    TrialFunction, TestFunction, File, solve, plot, lhs, rhs, grad, inner, dot, dx, ds, assemble, interpolate, project, near
+import dolfin
 import numpy as np
 from enum import Enum
 from matplotlib import pyplot as plt
@@ -41,10 +43,16 @@ class ProblemType(Enum):
     NEUMANN = 2  # Neumann problem
 
 
+class Tests(Enum):
+    SIN = 1
+    FENICS = 2
+
+
 class ComplementaryBoundary(SubDomain):
     def __init__(self, subdomain):
         self.complement = subdomain
         SubDomain.__init__(self)
+
     def inside(self, x, on_boundary):
         tol = 1E-14
         if on_boundary and not self.complement.inside(x, on_boundary):
@@ -56,18 +64,17 @@ class ComplementaryBoundary(SubDomain):
 class CouplingBoundary(SubDomain):
     def inside(self, x, on_boundary):
         tol = 1E-14
-        if on_boundary and near(x[0], x_coupling, tol):
+        if on_boundary and near(x[0], x_coupling, tol) and not near(x[1], y_bottom, tol) and not near(x[1], y_top, tol):
             return True
         else:
             return False
 
 
-def fluxes_from_temperature_full_domain(mesh, u_new, u_old, dt):
-    func_space = FunctionSpace(mesh, 'CG', 1)
-    fluxes_test = TestFunction(func_space)
-    fluxes_vector = assemble(inner((u_new - u_old)/dt, fluxes_test) * dx + inner(grad(u_new), grad(fluxes_test)) * dx)
-    fluxes = Function(func_space)
-    fluxes.vector()[:] = fluxes_vector[:]
+def fluxes_from_temperature_full_domain(F, bcs, mesh):
+    V = FunctionSpace(mesh, 'CG', 1)
+    fluxes_vector = assemble(F)
+    fluxes = Function(V)
+    fluxes.vector()[:] = - fluxes_vector[:]
     return fluxes
 
 
@@ -104,34 +111,50 @@ elif problem is ProblemType.NEUMANN:
     read_data_name = "Flux"
     write_data_name = "Temperature"
 
-T = 2.0  # final time
+T = 1.0  # final time
 num_steps = 10  # number of time steps
 dt = T / num_steps  # time step size
 alpha = 3  # parameter alpha
-beta = 1.2  # parameter beta
-x_coupling = 1  # x coordinate of coupling interface
+beta = 1.3  # parameter beta
+x_coupling = .7  # x coordinate of coupling interface
+y_bottom = 0
+y_top = 1
+
+TEST_NAME = Tests.FENICS
+
+if TEST_NAME is Tests.FENICS:
+    lam = 1
+elif TEST_NAME is Tests.SIN:
+    lam = .01
+
+lam_c = Constant(lam)
 
 # Create mesh and define function space
-nx = ny = 8
+nx = ny = 20
 
 if problem is ProblemType.DIRICHLET:
-    p0 = Point(0, 0)
-    p1 = Point(1, 1)
+    p0 = Point(0, y_bottom)
+    p1 = Point(x_coupling, y_top)
 elif problem is ProblemType.NEUMANN:
-    p0 = Point(1, 0)
-    p1 = Point(2, 1)
+    p0 = Point(x_coupling, y_bottom)
+    p1 = Point(2, y_top)
 
 mesh = RectangleMesh(p0, p1, nx, ny)
 V = FunctionSpace(mesh, 'P', 1)
 
 # Define boundary condition
-u_D = Expression('1 + x[0]*x[0] + alpha*x[1]*x[1] + beta*t',
-                 degree=2, alpha=alpha, beta=beta, t=0)
+if TEST_NAME is Tests.FENICS:
+    u_D = Expression('1 + x[0]*x[0] + alpha*x[1]*x[1] + beta*t', degree=2, alpha=alpha, beta=beta, t=dt)
+elif TEST_NAME is Tests.SIN:
+    u_D = Expression('exp(-5 * t *lam * pi * pi / 4) * (500 * sin(pi/2 * x[0]) * sin (pi * x[1]))', t=0, lam=lam,
+                     degree=2)
 u_D_function = interpolate(u_D, V)
 # Define flux on coupling interface (grad(u_D) in normal direction)
-f_N = Expression('2 * x[0]', degree=1)
+if TEST_NAME is Tests.FENICS:
+    f_N = Expression('2 * x[0]', degree=1)
+elif TEST_NAME is Tests.SIN:
+    f_N = Expression('500 * pi/2 * cos(pi/2 * x[0]) * sin (pi * x[1])', degree=1)
 f_N_function = interpolate(f_N, V)
-
 
 coupling_boundary = CouplingBoundary()
 remaining_boundary = ComplementaryBoundary(coupling_boundary)
@@ -139,8 +162,12 @@ remaining_boundary = ComplementaryBoundary(coupling_boundary)
 # todo put all the function calls below into one?
 coupling = fenics_adapter.Coupling(config_file_name, solver_name)
 coupling.set_coupling_mesh(mesh, coupling_boundary, coupling_mesh_name)
-coupling.set_read_field(read_data_name, u_D_function)
-coupling.set_write_field(write_data_name, f_N_function)
+if problem is ProblemType.DIRICHLET:
+    coupling.set_read_field(read_data_name, u_D_function)
+    coupling.set_write_field(write_data_name, f_N_function)
+elif problem is ProblemType.NEUMANN:
+    coupling.set_read_field(read_data_name, f_N_function)
+    coupling.set_write_field(write_data_name, u_D_function)
 coupling.initialize_data()
 # todo until here.
 
@@ -152,9 +179,12 @@ u_n = interpolate(u_D, V)
 # Define variational problem
 u = TrialFunction(V)
 v = TestFunction(V)
-f = Constant(beta - 2 - 2 * alpha)
+if TEST_NAME is Tests.FENICS:
+    f = Constant(beta - 2 - 2 * alpha)
+elif TEST_NAME is Tests.SIN:
+    f = Constant(0)
 
-F = u * v * dx + dt * dot(grad(u), grad(v)) * dx - (u_n + dt * f) * v * dx
+F = u * v * dx + dt * lam_c * dot(grad(u), grad(v)) * dx - (u_n + dt * f) * v * dx
 
 if problem is ProblemType.DIRICHLET:
     # apply Dirichlet boundary condition on coupling interface
@@ -165,12 +195,24 @@ if problem is ProblemType.NEUMANN:
 
 a, L = lhs(F), rhs(F)
 
+"""
+normal = dolfin.FacetNormal(mesh)
+mesh_function = dolfin.MeshFunction("size_t", mesh, mesh.topology().dim()-1, 0)
+mesh_function.set_all(0)
+remaining_boundary.mark(mesh_function, 1)
+non_coupling_ds = dolfin.Measure('ds', domain=mesh, subdomain_data=mesh_function)
+"""
+
 # Time-stepping
 u = Function(V)
+F_alternative = (u - (u_n + dt * f)) / dt * v * dx + lam_c * dot(grad(u), grad(v)) * dx # - dot(normal, grad(u)) * v * non_coupling_ds
+u.rename("Temperature", "")
 t = coupling.precice_tau
-assert(dt == coupling.precice_tau)
+u_D.t = t
+assert (dt == coupling.precice_tau)
 
-file_out = File("%s.pvd" % solver_name)
+file_out = File("out/%s.pvd" % solver_name)
+ref_out = File("out/ref%s.pvd" % solver_name)
 
 while coupling.is_coupling_ongoing():
 
@@ -179,7 +221,7 @@ while coupling.is_coupling_ongoing():
 
     if problem is ProblemType.DIRICHLET:
         # Dirichlet problem obtains flux from solution and sends flux on boundary to Neumann problem
-        fluxes = fluxes_from_temperature_full_domain(mesh, u, u_n, dt)
+        fluxes = fluxes_from_temperature_full_domain(F_alternative, bcs, mesh)
         coupling.exchange_data(fluxes, dt)
     elif problem is ProblemType.NEUMANN:
         # Neumann problem obtains sends temperature on boundary to Dirichlet problem
@@ -188,21 +230,23 @@ while coupling.is_coupling_ongoing():
     is_converged = coupling.check_convergence()
 
     if is_converged:
+        # plot solution
+        # plot(u)
+        # plt.pause(.1)
+        # Compute error at vertices
+        u_e = interpolate(u_D, V)
+        u_e.rename("reference", " ")
+        # error = np.abs(u_e.vector().get_local() - u.vector().get_local()).max()
+        error = assemble(inner(u_e - u, u_e - u) * dx)
+        print('t = %.2f: error = %.3g' % (t, error))
+        # Update previous solution
+        file_out << u
+        ref_out << u_e
         # Update current time
         t += coupling.precice_tau
         # Update dirichlet BC
         u_D.t = t
-        # plot solution
-        plot(u)
-        file_out << u
-        plt.pause(.1)
-        # Compute error at vertices
-        u_e = interpolate(u_D, V)
-        error = np.abs(u_e.vector().get_local() - u.vector().get_local()).max()
-        print('t = %.2f: error = %.3g' % (t, error))
-        # Update previous solution
         u_n.assign(u)
 
 # Hold plot
-plt.show()
 coupling.finalize()