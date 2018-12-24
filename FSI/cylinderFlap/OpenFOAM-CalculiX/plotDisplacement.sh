c257e1458a069360f745bf087695c28e9b738f66
Makis Chourdakis <makishourdakis@gmail.com>
2018-12-24 15:12:04 +0200

    Port plotDisplacement.sh to new preCICE log file naming format

--- FSI/cylinderFlap/OpenFOAM-CalculiX/plotDisplacement.sh
+++ FSI/cylinderFlap/OpenFOAM-CalculiX/plotDisplacement.sh
@@ -4,6 +4,6 @@ set grid
 set title 'Displacement of the Flap Tip'
 set xlabel 'Time [s]'
 set ylabel 'Y-Displacement [m]'
-plot "point1.watchpoint.txt" using 1:9 with lines title ""
+plot "precice-Calculix-watchpoint-point1.log" using 1:9 with lines title ""
 EOF
 
