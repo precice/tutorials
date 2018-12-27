a4dd17404894281383a552f632912a28c6d36af3
Makis Chourdakis <makishourdakis@gmail.com>
2018-12-28 00:04:10 +0200

    FSI OpenFOAM: Improvements in clean & run scripts

--- FSI/flap_perp/OpenFOAM-CalculiX/removeObsoleteFolders.sh
+++ FSI/flap_perp/OpenFOAM-CalculiX/removeObsoleteFolders.sh
@@ -1,16 +1,19 @@
 #! /bin/bash
+
+echo "Looking for any time directories without results (e.g. stray functionObjectProperties files). See issue #26 on GitHub."
+
 cd Fluid
-for f in *[0-9].*; do
-	if ! [ -f $f/U ]; then
-		rm -rf $f >/dev/null 2>&1
+for f in *[0-9] *[0-9].[0-9]*; do
+	if ! [ -f $f/U ] && ! [ -f $f/T ]; then
+		rm -rfv $f
 	fi
 done
 if [ -d processor0 ]; then
 	for g in processor*; do
 		cd $g
-		for f in *[0-9].*; do
-			if ! [ -f $f/U ]; then
-				rm -rf $f >/dev/null 2>&1
+		for f in *[0-9] *[0-9].[0-9]*; do
+			if ! [ -f $f/U ] && ! [ -f $f/T ]; then
+				rm -rfv $f
 			fi
 		done
 		cd ..
