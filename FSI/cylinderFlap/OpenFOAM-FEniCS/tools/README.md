32b18618aa5f3ecf5e245a2ca73d088293f8a3c3
BenjaminRueth <benjamin.rueth@tum.de>
2019-08-23 12:19:05 +0200

    add tools needed for convergence study and documentation

---
+++ FSI/cylinderFlap/OpenFOAM-FEniCS/tools/README.md
@@ -0,0 +1,29 @@
+# Excitement
+
+## Running
+
+Use `precice-config.xml` from this folder. Don't forget to modify `../Solid/precice-adapter-config-fsi-s.json` correspondingly.
+
+Run the following commands from `tutorials/FSI/cylinderFlap/OpenFOAM-FEniCS`:
+
+* `python3 Solid/cyl-flap.py`
+* `python3 tools/excitement.py tools/precice-config.xml`
+
+A file `out.txt` will be created in this folder. The result of the simulation is written to this folder
+
+## Using multirate / subiterations
+
+Depending on `precice_dt` and `fenics_dt` the computations are done using multirate timeintegration or not.
+
+* *no multirate:* the input force is provided for every single timestep
+* *multirate:* the input force is only provided for the timestep size `precice_dt`, if `fenics_dt < precice_dt` the same force will be applied for all fenics timesteps inside of a window.
+
+# Convergence Study
+
+Based on the results of the simulation above, one can perform a convergence study, by running `plot_convergence_study.py`.
+
+## Simulation result and Error estimation
+
+The convergence study is based on the results stored in `out.txt`. Here the final displacement of the tip is used as the basis. The error is estimated by computing the absolute difference between each result and a reference solution. The result of the simulation with the smallest time step is used as reference solution.
+
+!()[convergenceStudy.png]
---
+++ FSI/cylinderFlap/OpenFOAM-FEniCS/tools/excitement.py
@@ -0,0 +1,91 @@
+from __future__ import division
+
+import argparse
+import numpy as np
+import precice_future as precice
+
+force_excitement = np.array([0, 10, 0])
+omega_excitement = 1
+
+# beam geometry
+L = 0.35  # length
+H = 0.02  # height
+y_bottom = 0.2 - 0.5 * H  # y coordinate of bottom surface of beam
+y_top = y_bottom + H  # y coordinate of top surface of beam
+x_left = 0.25  # x coordinate of left surface of beam
+x_right = x_left + L  # x coordinate of right surface of beam
+x_attack = x_right
+y_attack = .5 * (y_bottom + y_top)
+
+
+def compute_force(time):
+    omega = omega_excitement
+    force_0 = force_excitement
+    return force_0 * np.sin(omega * time)
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("configurationFileName", help="Name of the xml config file.", type=str)
+
+try:
+    args = parser.parse_args()
+except SystemExit:
+    print("")
+    print("Usage: python ./solverdummy precice-config participant-name mesh-name")
+    quit()
+
+configuration_file_name = args.configurationFileName
+participant_name = "Excitement"
+mesh_name = "Excitement-Mesh"
+write_data_name = "Forces0"
+read_data_name = "Displacements0"
+
+n = 1
+
+solver_process_index = 0
+solver_process_size = 1
+
+interface = precice.Interface(participant_name, solver_process_index, solver_process_size)
+interface.configure(configuration_file_name)
+
+mesh_id = interface.get_mesh_id(mesh_name)
+
+dimensions = interface.get_dimensions()
+vertices = np.array([x_attack, y_attack, 0])
+
+vertex_ids = interface.set_mesh_vertices(mesh_id, vertices)
+write_data_id = interface.get_data_id(write_data_name, mesh_id)
+read_data_id = interface.get_data_id(read_data_name, mesh_id)
+
+dt = interface.initialize()
+t = 0
+
+if interface.is_action_required(precice.action_write_initial_data()):
+    forces = compute_force(t + dt)
+    interface.write_block_vector_data(write_data_id, vertex_ids, forces)
+    interface.fulfilled_action(precice.action_write_initial_data())
+
+interface.initialize_data()
+
+while interface.is_coupling_ongoing():
+
+    if interface.is_action_required(precice.action_write_iteration_checkpoint()):
+        print("DUMMY: Writing iteration checkpoint")
+        interface.fulfilled_action(precice.action_write_iteration_checkpoint())
+
+    forces = compute_force(t+dt)
+    print("sending forces: {}".format(forces))
+    interface.write_block_vector_data(write_data_id, vertex_ids, forces)
+    dt = interface.advance(dt)
+    displacement = interface.read_block_vector_data(read_data_id, vertex_ids)
+
+    if interface.is_action_required(precice.action_read_iteration_checkpoint()):
+        print("DUMMY: Reading iteration checkpoint")
+        interface.fulfilled_action(precice.action_read_iteration_checkpoint())
+    else:
+        t += dt
+        print("DUMMY: Advancing in time")
+
+interface.finalize()
+print("DUMMY: Closing python solver dummy...")
+
