2a96cacb62aaa3ac314bc8119fce5f5c5e76bb1e
BenjaminRueth <benjamin.rueth@tum.de>
2019-09-16 17:28:31 +0200

    reduce number of experiments being run

--- HT/partitioned-heat/fenics-fenics/tools/create_runexperiments.py
+++ HT/partitioned-heat/fenics-fenics/tools/create_runexperiments.py
@@ -2,9 +2,9 @@ from jinja2 import Environment, FileSystemLoader
 import os, stat
 from coupling_schemes import CouplingScheme
 
-wr_lefts = [1, 2, 5]
-wr_rights = [1, 2, 5]
-window_sizes = [1.0, 0.5, 0.2, 0.1]
+wr_lefts = [1, 2]#, 5]
+wr_rights = [1, 2]#, 5]
+window_sizes = [1.0, 0.5, 0.2]#, 0.1]
 #coupling_schemes = [CouplingScheme.SERIAL_FIRST_DIRICHLET.name, CouplingScheme.SERIAL_FIRST_NEUMANN.name, CouplingScheme.PARALLEL.name]
 coupling_schemes = [CouplingScheme.SERIAL_FIRST_DIRICHLET.name]
 
