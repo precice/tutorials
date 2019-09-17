b5ab694f10dc20eef7f2e2637840965d0feaba51
BenjaminRueth <benjamin.rueth@tum.de>
2019-09-17 14:37:20 +0200

    add another setup with quadratic interpolation in time

---
+++ HT/partitioned-heat/fenics-fenics/tools/setup_s_sdc_quadratic.sh
@@ -0,0 +1,5 @@
+python3 create_all_configs.py -mth sdc -t s -stol 100 -wri quadratic
+./config_creation.sh
+mv experiments ..
+python3 create_runexperiments.py -wrr 2 5 -wrl 2 5 
+mv runexperiments.sh ..
