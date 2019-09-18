3871b4de66cc63d367f16ab8aa08e5ced7e5633a
BenjaminRueth <benjamin.rueth@tum.de>
2019-09-18 16:39:56 +0200

    run all timestep sizes for SDC with WR55

--- HT/partitioned-heat/fenics-fenics/tools/setup_s_sdc_55_quadratic.sh
+++ HT/partitioned-heat/fenics-fenics/tools/setup_s_sdc_55_quadratic.sh
@@ -1,5 +1,5 @@
 python3 create_all_configs.py -mth sdc -t s -stol 100 -wri quadratic -qntol 0.00001
 ./config_creation.sh
 mv experiments ..
-python3 create_runexperiments.py -wrr 5 -wrl 5 -Dts 5.0 2.0 1.0
+python3 create_runexperiments.py -wrr 5 -wrl 5
 mv runexperiments.sh ..
