3affcff789708018cc998d2e84646f8841c21016
BenjaminRueth <benjamin.rueth@tum.de>
2019-09-24 09:05:25 +0200

    fix pp argument

--- HT/partitioned-heat/fenics-fenics/tools/setup_s_tr_passive.sh
+++ HT/partitioned-heat/fenics-fenics/tools/setup_s_tr_passive.sh
@@ -1,4 +1,4 @@
-python3 create_all_configs.py -mth tr -pp passive -t s -stol 100 -qntol 0.00001
+python3 create_all_configs.py -mth tr -pp qn-passive -t s -stol 100 -qntol 0.00001
 ./config_creation.sh
 mv experiments ..
 python3 create_runexperiments.py
