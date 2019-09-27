eb69ffa194e313c06a8cf1fc15dbf7046a7460ef
BenjaminRueth <benjamin.rueth@tum.de>
2019-09-27 16:00:44 +0200

    add setup scripts for convergence study

---
+++ HT/partitioned-heat/fenics-fenics/tools/convergence_study_setup_s_tr_linear.sh
@@ -0,0 +1,5 @@
+python3 create_all_configs.py -mth tr -t s -stol 100 -wri linear -qntol 0.00001 -T 1 -Dts 1.0 0.5 0.2 0.1 0.05 0.025 0.0125 0.00625 -wrr 2 5 -wrl 2 5
+./config_creation.sh
+mv experiments ..
+python3 create_runexperiments.py -Dts 1.0 0.5 0.2 0.1 0.05 0.025 0.0125 0.00625 -wrr 2 5 -wrl 2 5
+mv runexperiments.sh ..
