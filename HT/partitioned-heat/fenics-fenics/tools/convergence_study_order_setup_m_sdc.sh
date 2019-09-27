f9af5e906afbe8b67208032c4b6f8f9ce4dc160d
BenjaminRueth <benjamin.rueth@tum.de>
2019-09-27 19:37:29 +0200

    add setup scripts for convergence study on order

---
+++ HT/partitioned-heat/fenics-fenics/tools/convergence_study_order_setup_m_sdc.sh
@@ -0,0 +1,5 @@
+python3 create_all_configs.py --monolithic -mth sdc -t s -stol 100 -wrl 1 -wrr 1 -Dts 1.0 0.5 0.2 0.1 0.05 0.025 0.0125 -T 1
+./config_creation.sh
+mv experiments ..
+python3 create_runexperiments.py -wrl 1 -wrr 1 -Dts 1.0 0.5 0.2 0.1 0.05 0.025 0.0125
+mv runexperiments.sh ..
---
+++ HT/partitioned-heat/fenics-fenics/tools/convergence_study_order_setup_s_ie_linear.sh
@@ -0,0 +1,5 @@
+python3 create_all_configs.py -mth ie -t s -stol 100 -wri linear -qntol 0.00001 -T 1 -Dts 1.0 0.5 0.2 0.1 0.05 0.025 0.0125 -wrr 3 -wrl 5
+./config_creation.sh
+mv experiments ..
+python3 create_runexperiments.py -Dts 1.0 0.5 0.2 0.1 0.05 0.025 0.0125 -wrr 3 -wrl 5 
+mv runexperiments.sh ..
