c3e6ee5453dfe30ff17e8a6247bf4beff2ac5955
BenjaminRueth <benjamin.rueth@tum.de>
2019-10-16 20:11:56 +0200

    add tolerance, since we cannot get the exact solution

--- HT/partitioned-heat/fenics-fenics/tools/g_pol_setup_qrt_sdc_quartic.sh
+++ HT/partitioned-heat/fenics-fenics/tools/g_pol_setup_qrt_sdc_quartic.sh
@@ -1,4 +1,4 @@
-python3 create_all_configs.py -mth sdc -wri quartic -t qrt -Dts 1.0 0.5 0.25 0.1 0.05 0.025 0.0125 -T 1 --sdc-K 16	
+python3 create_all_configs.py -mth sdc -wri quartic -t qrt -Dts 1.0 0.5 0.25 0.1 0.05 0.025 0.0125 -T 1 --sdc-K 16 -stol 100
 ./config_creation.sh
 mv experiments ..
 python3 create_runexperiments.py -wrr 5 -wrl 5 -Dts 1.0 0.5 0.25 0.1 0.05 0.025 0.0125
