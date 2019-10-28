9a1928265922862f18959c64bc044109fb8d93fb
Benjamin Rüth <benjamin.rueth@tum.de>
2019-10-28 08:06:29 +0100

    Update g_pol_setup_qrt_sdc_quartic.sh

--- HT/partitioned-heat/fenics-fenics/tools/g_pol_setup_qrt_sdc_quartic.sh
+++ HT/partitioned-heat/fenics-fenics/tools/g_pol_setup_qrt_sdc_quartic.sh
@@ -1,5 +1,5 @@
-python3 create_all_configs.py -mth sdc -wri quartic -t qrt -Dts 1.0 0.5 0.25 0.1 0.05 0.025 0.0125 -T 1 --sdc-K 16 -stol 100
+python3 create_all_configs.py -mth sdc -wri quartic -t qrt -Dts 1.0 0.5 -T 1 --sdc-K 16 -stol 100
 ./config_creation.sh
 mv experiments ..
-python3 create_runexperiments.py -wrr 5 -wrl 5 -Dts 1.0 0.5 0.25 0.1 0.05 0.025 0.0125
+python3 create_runexperiments.py -wrr 5 -wrl 5 -Dts 1.0 0.5
 mv runexperiments.sh ..
