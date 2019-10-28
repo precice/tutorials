d46991c66a7bfe8b5b69a69a02244569c5c72b85
Benjamin Rüth <benjamin.rueth@tum.de>
2019-10-28 08:05:13 +0100

    reduce number of timestep sizes being run
    for more reasonable runtimes

--- HT/partitioned-heat/fenics-fenics/tools/g_pol_setup_c_sdc_cubic.sh
+++ HT/partitioned-heat/fenics-fenics/tools/g_pol_setup_c_sdc_cubic.sh
@@ -1,5 +1,5 @@
-python3 create_all_configs.py -mth sdc -wri cubic -t c -Dts 1.0 0.5 0.25 0.1 0.05 0.025 0.0125 -T 1 --sdc-K 16	
+python3 create_all_configs.py -mth sdc -wri cubic -t c -Dts 1.0 0.5 -T 1 --sdc-K 16	
 ./config_creation.sh
 mv experiments ..
-python3 create_runexperiments.py -wrr 5 3 -wrl 5 3 -Dts 1.0 0.5 0.25 0.1 0.05 0.025 0.0125
+python3 create_runexperiments.py -wrr 5 3 -wrl 5 3 -Dts 1.0 0.5
 mv runexperiments.sh ..
