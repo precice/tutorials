ea9665a7577151c6775f92dcf13001adfa5be39f
ishaandesai <ishaandesai@gmail.com>
2021-01-13 14:20:09 +0100

    Making z-width=1 and formatting config files

--- FSI/flap_perp/SU2-CalculiX/plotDisplacement.sh
+++ FSI/flap_perp/SU2-CalculiX/plotDisplacement.sh
@@ -4,5 +4,5 @@ set grid
 set title 'Displacement of the Flap Tip'
 set xlabel 'Time [s]'
 set ylabel 'X-Displacement [m]'
-plot "precice-Calculix-watchpoint-point1.log" using 1:5 smooth cumulative title ""
+plot "precice-Calculix-watchpoint-point1.log" using 1:4 smooth cumulative title ""
 EOF
