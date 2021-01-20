ab1aa4d8b3fec25535e57ed61b01ca4aa438716b
David Schneider <dav.schneider@tum.de>
2021-01-20 16:47:22 +0100

    Adjust ccx setting and remove directory name from OF

--- perpendicular-flap/fluid-openfoam/removeObsoleteFolders.sh
+++ perpendicular-flap/fluid-openfoam/removeObsoleteFolders.sh
@@ -2,7 +2,6 @@
 
 echo "Looking for any time directories without results (e.g. stray functionObjectProperties files, see issue #26 on GitHub)..."
 
-cd Fluid
 for f in [0-9]* [0-9]*.[0-9]*; do
 	if ! [ -f $f/U ] && ! [ -f $f/T ]; then
 		rm -rfv $f
