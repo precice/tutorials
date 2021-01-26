d216293dacf2aef3f3cce64bb1e61076318b2ec8
uekerman <benjamin.uekermann@gmail.com>
2021-01-26 15:43:04 +0100

    Update nutils fluid to new pyprecice, new Nutils and single mesh

--- perpendicular-flap/README.md
+++ perpendicular-flap/README.md
@@ -19,7 +19,7 @@ Fluid participant:
 
 * OpenFOAM. For older OpenFOAM versions, the solver name will differ. If you are using OpenFOAM v1712 / 5.x or older have a look in the `fluid-openfoam/system/controlDict` file and set the appropriate solver name. The solver can run in parallel using the command line argument `run.sh -parallel`. For more information, have a look at the [OpenFOAM adapter documentation](adapter-openfoam-overview.html).
 
-* Nutils. For more information, have a look at the [Nutils adapter documentation](adapter-nutils-overview.html).
+* Nutils. For more information, have a look at the [Nutils adapter documentation](adapter-nutils-overview.html). This Nutils solver requires at least Nutils v6.0.
 
 * SU2. As opposed to the other fluid codes, SU2 is in particular specialized for compressible flows. Therefore the default simulation parameters haven been adjusted in order to pull the setup into the compressible flow regime. For more information, have a look at the [SU2 adapter documentation](adapter-su2-overview.html).
 
--- perpendicular-flap/fluid-nutils/nsale.py
+++ perpendicular-flap/fluid-nutils/nsale.py
@@ -4,6 +4,8 @@ from nutils import mesh, function, solver, export, cli
 import numpy, treelog
 import precice
 
+# for details on this solver see https://doi.org/10.1002/nme.6443
+
 # some helper function to shift variables by one timestep
 @function.replace
 def subs0(f):
@@ -79,9 +81,10 @@ def main(inflow: 'inflow velocity' = 10,
     domain.basis('std', degree=2).vector(2),
     domain.basis('std', degree=1),
   ])
+  ns.F_i = 'ubasis_ni ?F_n' # stress field
   ns.urel_i = 'ubasis_ni ?lhs_n' # relative velocity
   ns.u_i = 'umesh_i + urel_i' # total velocity
-  ns.p = 'pbasis_n ?lhs_n'
+  ns.p = 'pbasis_n ?lhs_n' # pressure
 
 
   # initialization of dofs
@@ -99,25 +102,21 @@ def main(inflow: 'inflow velocity' = 10,
   participantName = "Fluid"
   solverProcessIndex = 0
   solverProcessSize = 1
-  interface = precice.Interface(participantName, solverProcessIndex, solverProcessSize)
-  interface.configure(configFileName)
+  interface = precice.Interface(participantName, configFileName, solverProcessIndex, solverProcessSize)
 
   # define coupling meshes
-  meshNameGP = "Fluid-Mesh" # Gauss points
-  meshIDGP = interface.get_mesh_id(meshNameGP)
+  meshName = "Fluid-Mesh" 
+  meshID = interface.get_mesh_id(meshName)
 
-  # define Gauss points
   couplinginterface = domain.boundary['flap']
-  couplingsampleGP = couplinginterface.sample('gauss', degree=4)
-  verticesGP = couplingsampleGP.eval(ns.x, arguments=dict(meshdofs=meshdofs))
-  dataIndicesGP = numpy.zeros(couplingsampleGP.npoints)
-  interface.set_mesh_vertices(meshIDGP, couplingsampleGP.npoints, verticesGP, dataIndicesGP)
+  couplingsample = couplinginterface.sample('gauss', degree=2) # mesh located at Gauss points
+  dataIndices = interface.set_mesh_vertices(meshID, couplingsample.eval(ns.x0))
 
   # coupling data
   writeData = "Force"
   readData =  "Displacement"
-  writedataID = interface.get_data_id(writeData, meshIDGP)
-  readdataID = interface.get_data_id(readData, meshIDGP)
+  writedataID = interface.get_data_id(writeData, meshID)
+  readdataID = interface.get_data_id(readData, meshID)
 
   # initialize preCICE
   precice_dt = interface.initialize()
@@ -141,6 +140,8 @@ def main(inflow: 'inflow velocity' = 10,
   resF += domain.integral('pbasis_n u_k,k d:x' @ ns, degree=4)
   resF += domain.integral('rho ubasis_ni tt(u_i d:x)' @ ns, degree=4)
   resF += domain.integral('rho ubasis_ni (u_i,j urel_j d:x)' @ ns, degree=4)
+  resF += couplinginterface.sample('gauss', 4).integral('ubasis_ni F_i d:x' @ ns)
+  consF = numpy.isnan(solver.optimize('F', couplinginterface.sample('gauss', 4).integral('F_i F_i' @ ns), droptol=1e-10))
 
   # boundary conditions mesh displacements
   sqr = domain.boundary['inflow,outflow,wall'].integral('d_i d_i' @ ns, degree=2)
@@ -149,27 +150,24 @@ def main(inflow: 'inflow velocity' = 10,
   # weak form mesh displacements
   meshsqr = domain.integral('d_i,x0_j d_i,x0_j d:x0' @ ns, degree=2)
 
-
-  # better initial guess: start from Stokes solution, comment out for comparison with OpenFOAM
+  # better initial guess: start from Stokes solution, comment out for comparison with other solvers
   #res_stokes = domain.integral('(ubasis_ni,j ((u_i,j + u_j,i) rho nu - p δ_ij) + pbasis_n u_k,k) d:x' @ ns, degree=4)
   #lhs0 = solver.solve_linear('lhs', res_stokes, constrain=cons, arguments=dict(meshdofs=meshdofs, oldmeshdofs=oldmeshdofs, oldoldmeshdofs=oldoldmeshdofs, oldoldoldmeshdofs=oldoldoldmeshdofs, dt=dt))
   lhs00 = lhs0
 
-
-  timestep = 1
+  timestep = 0
   t = 0
 
   while interface.is_coupling_ongoing():
 
     # read displacements from interface
     if interface.is_read_data_available():
-      readdata = numpy.zeros([couplingsampleGP.npoints, 2])
-      interface.read_block_vector_data(readdataID, couplingsampleGP.npoints, dataIndicesGP, readdata.ravel())
-      coupledata = couplingsampleGP.asfunction(readdata)
-      sqr = couplingsampleGP.integral(((ns.d - coupledata)**2).sum(0))
+      readdata = interface.read_block_vector_data(readdataID, dataIndices)
+      coupledata = couplingsample.asfunction(readdata)
+      sqr = couplingsample.integral(((ns.d - coupledata)**2).sum(0))
       meshcons = solver.optimize('meshdofs', sqr, droptol=1e-10, constrain=meshcons0)
       meshdofs = solver.optimize('meshdofs', meshsqr, constrain=meshcons)
-
+      
     # save checkpoint
     if interface.is_action_required(precice.action_write_iteration_checkpoint()):
       lhs_checkpoint = lhs0
@@ -179,15 +177,18 @@ def main(inflow: 'inflow velocity' = 10,
       oldmeshdofs_checkpoint = oldmeshdofs
       oldoldmeshdofs_checkpoint = oldoldmeshdofs
       oldoldoldmeshdofs_checkpoint = oldoldoldmeshdofs
-      interface.fulfilled_action(precice.action_write_iteration_checkpoint())
+      interface.mark_action_fulfilled(precice.action_write_iteration_checkpoint())
 
     # solve fluid equations
     lhs1 = solver.newton('lhs', res, lhs0=lhs0, constrain=cons, arguments=dict(lhs0=lhs0, dt=dt, meshdofs=meshdofs, oldmeshdofs=oldmeshdofs, oldoldmeshdofs=oldoldmeshdofs, oldoldoldmeshdofs=oldoldoldmeshdofs)).solve(tol=1e-9)
 
     # write forces to interface
-    R = -resF.eval(lhs00=lhs00, lhs0=lhs0, lhs=lhs1, dt=dt, meshdofs=meshdofs, oldmeshdofs=oldmeshdofs, oldoldmeshdofs=oldoldmeshdofs, oldoldoldmeshdofs=oldoldoldmeshdofs)
-    writedata = R[where]
-    interface.write_block_vector_data(writedataID, len(where), dataIndicesGC, writedata.ravel())
+    if interface.is_write_data_required(dt):
+      F = solver.solve_linear('F', resF, constrain=consF, arguments=dict(lhs00=lhs00, lhs0=lhs0, lhs=lhs1, dt=dt, meshdofs=meshdofs, oldmeshdofs=oldmeshdofs, oldoldmeshdofs=oldoldmeshdofs, oldoldoldmeshdofs=oldoldoldmeshdofs))
+      #writedata = couplingsample.eval(ns.F, F=F) # for stresses
+      writedata = couplingsample.eval('F_i d:x' @ ns, F=F, meshdofs=meshdofs) * numpy.concatenate([p.weights for p in couplingsample.points])[:,numpy.newaxis]
+      interface.write_block_vector_data(writedataID, dataIndices, writedata)
+    
 
     # do the coupling
     precice_dt = interface.advance(dt)
@@ -211,12 +212,12 @@ def main(inflow: 'inflow velocity' = 10,
       oldmeshdofs = oldmeshdofs_checkpoint
       oldoldmeshdofs = oldoldmeshdofs_checkpoint
       oldoldoldmeshdofs = oldoldoldmeshdofs_checkpoint
-      interface.fulfilled_action(precice.action_read_iteration_checkpoint())
+      interface.mark_action_fulfilled(precice.action_read_iteration_checkpoint())
 
-    else: # go to next timestep and visualize, TODO: does not work for multirate
+    if interface.is_time_window_complete():
       x, u, p = bezier.eval(['x_i', 'u_i', 'p'] @ ns, lhs=lhs1, meshdofs=meshdofs, oldmeshdofs=oldmeshdofs, oldoldmeshdofs=oldoldmeshdofs, oldoldoldmeshdofs=oldoldoldmeshdofs, dt=dt)
-      export.vtk('NSALE', bezier.tri, x, u=u, p=p)
-
+      with treelog.add(treelog.DataLog()):
+        export.vtk('Fluid_' + str(timestep), bezier.tri, x, u=u, p=p)
 
   interface.finalize()
 
