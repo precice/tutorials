79e9fc594b5722400d4763d7e4a15b0dc291ef93
David Schneider <dav.schneider@tum.de>
2021-02-19 14:33:21 +0100

    Port trivial cases (#151)
    * Restructure 3D Tube case
    
    * Fix ccx paths and cleaning script
    
    * Apply new naming conventions to config file
    
    * Format xml in precice style
    
    * Add first version of multi coupling case
    
    * Adjust paths according to restructure
    
    * Add the heat exchanger case
    
    * Add run scripts for fluid cases
    
    * Adjust download script
    
    * Add partitioned elastic beam
    
    * Apply naming conventions and provide cleaning script
    
    * Restructure steady state plate
    
    * Change 0 dir to 0.orig
    
    * Fix blockMesh in run.sh
    
    * Port heat-exchanger README from preCICE wiki
    
    * Port partitioned beam README from preCICE wiki
    
    * Add a README for the 3D tube
    
    * Add code_aster README from code-aster wiki
    
    * Apply multiple minor changes to preCICE configs and READMEs
    
    * Adjust naming scheme in multiple flaps case
    
    * Adjust naming of partitioned beam
    
    * Switch dimension in steady state heated plate
    
    * Remove vtk tag for multiple flap case
    
    * Remove vtk tag from remainig mesh
    
    Co-authored-by: uekerman <benjamin.uekermann@gmail.com>

---
+++ flow-over-heated-plate-steady-state/fluid-openfoam/run.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+cd ${0%/*} || exit 1    		    		# Run from this directory
+. $WM_PROJECT_DIR/bin/tools/RunFunctions    # Tutorial run functions
+
+# Fluid participant
+
+# Run this script in one terminal and the execute the coupled_elasto_dynamics bindary 
+# in another terminal.
+# These scripts present how the two participants would be started manually.
+
+# Run this script with "-parallel" for parallel simulations
+
+# The script "Allclean" cleans-up the result and log files.
+
+# 1 for true, 0 for false
+parallel=0
+if [ "$1" = "-parallel" ]; then
+    parallel=1
+fi
+
+echo "Preparing and running the Fluid participant..."
+
+rm -rfv 0/
+cp -r 0.orig/ 0/
+blockMesh
+checkMesh
+
+# Run
+solver=$(getApplication)
+procs=$(getNumberOfProcessors)
+if [ $parallel -eq 1 ]; then
+    decomposePar -force
+    mpirun -np $procs $solver -parallel
+    reconstructPar
+else
+    $solver
+fi
+
+# Workaround for issue #26 (OF-adapter, relevant for OF .com versions) 
+./removeObsoleteFolders.sh
---
+++ partitioned-elastic-beam/neumann-calculix/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd ${0%/*} || exit 1    # Run from this directory
+
+# This script prepares and runs the CalculiX solver
+
+# =============== Participant: Solid ===========================
+Solver="ccx_preCICE"
+
+    # Run and get the process id
+    echo "Starting the CalculiX..."
+    export OMP_NUM_THREADS=1
+    export CCX_NPROC_EQUATION_SOLVER=1
+    ${Solver} -i beam2 -precice-participant Calculix2
