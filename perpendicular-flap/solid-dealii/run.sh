5b3c9f93d81c2ac3296a9d85b1eb9a6df40fd26b
DavidSCN <david.schneider@ipvs.uni-stuttgart.de>
2021-03-18 16:14:21 +0100

    Update script and merged parameter files for deal.II

--- perpendicular-flap/solid-dealii/run.sh
+++ perpendicular-flap/solid-dealii/run.sh
@@ -7,21 +7,5 @@ cd ${0%/*} || exit 1    		    # Run from this directory
 # in another terminal.
 # These scripts present how the two participants would be started manually.
 
-# 1 for true, 0 for false
-nonlinear=0
-if [ "$1" = "-nonlinear" ]; then
-            nonlinear=1
-fi
+./elasticity parameters.prm
 
-linear=0
-if [ "$1" = "-linear" ]; then
-            linear=1
-fi
-
-if [ $linear -eq 1 ]; then
-        ./linear_elasticity linear_elasticity.prm
-elif [ $nonlinear -eq 1 ]; then
-        ./nonlinear_elasticity nonlinear_elasticity.prm
-else
-        echo "No solver type specified. Please specify -linear or -nonlinear as solver type"
-fi
