1a0d204b20de9e75ffba1687bb9577320baf266f
Gerasimos Chourdakis <makishourdakis@gmail.com>
2021-03-24 23:16:14 +0100

    Format clean-all.sh

--- clean-all.sh
+++ clean-all.sh
@@ -4,8 +4,12 @@ set -e -u
 echo "- Cleaning up all tutorials..."
 
 for tutorial in */; do
-    if [ "${tutorial}" = tools/ ] || [ "${tutorial}" = elastic-tube-1d/ ] || [ "${tutorial}" = partitioned-heat-conduction/ ] || [ "${tutorial}" = partitioned-heat-conduction-complex/ ] || [ "${tutorial}" = quickstart/ ]; then
-    # TODO: Update when the excluded cases are also ported to the new structure
+    if [ "${tutorial}" = tools/ ] ||
+       [ "${tutorial}" = elastic-tube-1d/ ] ||
+       [ "${tutorial}" = partitioned-heat-conduction/ ] ||
+       [ "${tutorial}" = partitioned-heat-conduction-complex/ ] ||
+       [ "${tutorial}" = quickstart/ ]; then
+        # TODO: Update when the excluded cases are also ported to the new structure
         continue
     fi;
     (cd "${tutorial}" && ./clean-tutorial.sh)
