01d39b1b33da382194ba08d571a94baf8113ed9c
DavidSCN <david.schneider@ipvs.uni-stuttgart.de>
2021-03-26 17:17:46 +0100

    Replace prefix by executable

--- tools/run-dealii.sh
+++ tools/run-dealii.sh
@@ -1,19 +1,13 @@
 #!/bin/sh
 set -e -u
-# Solid participant
 
-# Run this script in one terminal and the execute the Fluid participant 
-# in another terminal.
-# These scripts present how the two participants would be started manually.
-
-SEARCHPATH=""
-SOLVER="elasticity"
+EXE=""
 
 for i in "$@"
 do
 case $i in
-    -p=*|--prefix=*)
-    SEARCHPATH="${i#*=}"
+    -e=*|--exec=*)
+    EXE="${i#*=}"
     shift # past argument=value
     ;;
     *)
@@ -22,20 +16,22 @@ case $i in
 esac
 done
 
-# If a prefix has been defined
-if [ -n "${SEARCHPATH}"  ]; then
-    "${SEARCHPATH}"/"${SOLVER}" parameters.prm
+# If the executable has been defined
+if [ -n "${EXE}"  ]; then
+    "${EXE}" parameters.prm
+    exit 0;
 fi
 
+EXE="elasticity"
 # If it is in the global path
-if [ -n "$(command -v "${SOLVER}")" ]; then
-   "${SOLVER}" parameters.prm
+if [ -n "$(command -v "${EXE}")" ]; then
+   "${EXE}" parameters.prm
+   exit 0;
 fi
 
 # If it has been copied to the local directory
 if test -f "elasticity"; then
-    ./"${SOLVER}" parameters.prm
+    ./"${EXE}" parameters.prm
     else
-    echo "Unable to find the executable ${SOLVER}. Either specify a prefix (-p=/path/to/elasticity) or make it discoverable at runtime (e.g. export PATH)"
+    echo "Unable to find the executable ${EXE}. Either specify the executable explicitly (-e=/path/to/elasticity) or make it discoverable at runtime (e.g. export PATH)"
 fi
-
