5783ae56fbb10bb3ca4a5e8e9808ca248cc645b8
Benjamin Uekermann <benjamin.uekermann@ipvs.uni-stuttgart.de>
2021-04-04 21:32:40 +0200

    Comment, format, and rewrite nutils heat code

--- partitioned-heat-conduction/nutils/heat.py
+++ partitioned-heat-conduction/nutils/heat.py
@@ -1,182 +1,169 @@
 #! /usr/bin/env python3
 
-import nutils
-import numpy
-import treelog
+import nutils, treelog
+import numpy as np
 import precice
-from precice import action_write_initial_data, action_read_iteration_checkpoint, action_write_iteration_checkpoint
 
 
-def main(nelems: 'number of elements along edge' = 10,
-         btype: 'type of basis function (std/spline)' = 'std',
-         degree: 'polynomial degree' = 1,
-         side='Dirichlet',
-         timestepsize=.1):
+def main(side='Dirichlet'):
 
-    print("Running nutils")
+  print("Running nutils")
 
-    y_bottom, y_top = 0, 1
-    x_left, x_right = 0, 2
-    x_coupling = 1  # x coordinate of coupling interface
+  # domain size
+  y_bottom, y_top = 0, 1
+  x_left, x_right = 0, 2
+  x_coupling = 1  # x coordinate of coupling interface
 
-    if side == 'Dirichlet':
-        x_grid = numpy.linspace(x_left, x_coupling, nelems)
-    elif side == 'Neumann':
-        x_grid = numpy.linspace(x_coupling, x_right, nelems)
-    else:
-        raise Exception('invalid side {!r}'.format(side))
+  n = 10 # number of mesh vertices per dimension 
 
-    y_grid = numpy.linspace(y_bottom, y_top, nelems)
+  if side == 'Dirichlet':
+    x_grid = np.linspace(x_left, x_coupling, n)
+  elif side == 'Neumann':
+    x_grid = np.linspace(x_coupling, x_right, n)
+  else:
+    raise Exception('invalid side {!r}'.format(side))
 
-    domain, geom = nutils.mesh.rectilinear([x_grid, y_grid])
+  y_grid = np.linspace(y_bottom, y_top, n)
 
-    ns = nutils.function.Namespace()
-    ns.x = geom
-    ns.basis = domain.basis(btype, degree=degree)
-    ns.alpha = 3
-    ns.beta = 1.3
-    ns.u = 'basis_n ?lhs_n'
-    ns.dudt = 'basis_n (?lhs_n - ?lhs0_n) / ?dt'
-    ns.flux = 'basis_n ?fluxdofs_n'
-    ns.f = 'beta - 2 - 2 alpha'
-    ns.uexact = '1 + x_0 x_0 + alpha x_1 x_1 + beta ?t'
+  # define the Nutils mesh
+  domain, geom = nutils.mesh.rectilinear([x_grid, y_grid])
 
-    res0 = domain.integral(
-        '(basis_n dudt - basis_n f + basis_n,i u_,i) d:x' @ ns, degree=degree * 2)
+  # Nutils namespace
+  ns = nutils.function.Namespace()
+  ns.x = geom
+  
+  degree = 1 # linear finite elements
+  ns.basis = domain.basis('std', degree=degree) 
+  ns.alpha = 3 # parameter of problem
+  ns.beta = 1.3 # parameter of problem
+  ns.u = 'basis_n ?lhs_n' # solution
+  ns.dudt = 'basis_n (?lhs_n - ?lhs0_n) / ?dt' # time derivative
+  ns.flux = 'basis_n ?fluxdofs_n' # heat flux
+  ns.f = 'beta - 2 - 2 alpha' # rhs
+  ns.uexact = '1 + x_0 x_0 + alpha x_1 x_1 + beta ?t' # analytical solution
 
-    # set boundary conditions at non-coupling boundaries
-    # top and bottom boundary are non-coupling for both sides
-    sqr0 = domain.boundary['top'].integral(
-        '(u - 1 - x_0 x_0 - alpha - beta ?t)^2 d:x' @ ns, degree=degree * 2)
-    sqr0 += domain.boundary['bottom'].integral(
-        '(u - 1 - x_0 x_0 - beta ?t)^2 d:x' @ ns, degree=degree * 2)
-    if side == 'Dirichlet':  # left boundary is non-coupling
-        sqr0 += domain.boundary['left'].integral(
-            '(u - 1 - alpha x_1 x_1 - beta ?t)^2 d:x' @ ns, degree=degree * 2)
-    elif side == 'Neumann':  # right boundary is non-coupling
-        sqr0 += domain.boundary['right'].integral(
-            '(u - 1 - x_0 x_0 - alpha x_1 x_1 - beta ?t)^2 d:x' @ ns, degree=degree * 2)
+  # define the weak form
+  res0 = domain.integral('(basis_n dudt - basis_n f + basis_n,i u_,i) d:x' @ ns, degree=degree * 2)
 
-    participantName = side
-    meshName = side + "-Mesh"
+  # set boundary conditions at non-coupling boundaries
+  # top and bottom boundary are non-coupling for both sides
+  sqr0 = domain.boundary['top'].integral('(u - 1 - x_0 x_0 - alpha - beta ?t)^2 d:x' @ ns, degree=degree * 2)
+  sqr0 += domain.boundary['bottom'].integral('(u - 1 - x_0 x_0 - beta ?t)^2 d:x' @ ns, degree=degree * 2)
+  if side == 'Dirichlet':  # left boundary is non-coupling
+    sqr0 += domain.boundary['left'].integral('(u - 1 - alpha x_1 x_1 - beta ?t)^2 d:x' @ ns, degree=degree * 2)
+  elif side == 'Neumann':  # right boundary is non-coupling
+    sqr0 += domain.boundary['right'].integral('(u - 1 - x_0 x_0 - alpha x_1 x_1 - beta ?t)^2 d:x' @ ns, degree=degree * 2)
 
-    interface = precice.Interface(
-        participantName, "../precice-config.xml", 0, 1)
 
-    meshID = interface.get_mesh_id(meshName)
+  # preCICE setup
+  interface = precice.Interface(side, "../precice-config.xml", 0, 1)
+  
+  # define coupling mesh
+  mesh_name = side + "-Mesh"
+  mesh_id = interface.get_mesh_id(mesh_name)
+  coupling_boundary = domain.boundary['right' if side == 'Dirichlet' else 'left']
+  coupling_sample = coupling_boundary.sample('gauss', degree=degree * 2)
+  vertices = coupling_sample.eval(ns.x)
+  vertex_ids = interface.set_mesh_vertices(mesh_id, vertices)
 
-    writeData = "Temperature" if side == "Neumann" else "Flux"
-    readData = "Flux" if side == "Neumann" else "Temperature"
+  # coupling data
+  write_data = "Temperature" if side == "Neumann" else "Flux"
+  read_data = "Flux" if side == "Neumann" else "Temperature"
+  write_data_id = interface.get_data_id(write_data, mesh_id)
+  read_data_id = interface.get_data_id(read_data, mesh_id)
 
-    writedataID = interface.get_data_id(writeData, meshID)
-    readdataID = interface.get_data_id(readData, meshID)
+  # helper functions to project heat flux to coupling boundary
+  projection_matrix = coupling_boundary.integrate(ns.eval_nm('basis_n basis_m d:x'), degree=degree * 2)
+  projection_cons = np.zeros(res0.shape)
+  projection_cons[projection_matrix.rowsupp(1e-15)] = np.nan
+  fluxdofs = lambda v: projection_matrix.solve(v, constrain=projection_cons)
 
-    couplinginterface = domain.boundary['right' if side ==
-                                        'Dirichlet' else 'left']
-    couplingsample = couplinginterface.sample('gauss', degree=degree * 2)
+  # helper data structures to integrate heat flux correctly  
+  dx = coupling_sample.eval('d:x' @ ns)
+  dx_function = coupling_sample.asfunction(dx)
+  
+  precice_dt = interface.initialize()
 
-    vertices = couplingsample.eval(ns.x)
+  # write initial data
+  if interface.is_action_required(precice.action_write_initial_data()):
+    write_data = np.zeros(len(vertex_ids))
+    interface.write_block_scalar_data(write_data_id, vertex_ids, write_data)
+    interface.mark_action_fulfilled(precice.action_write_initial_data())
 
-    dataIndices = interface.set_mesh_vertices(meshID, vertices)
+  interface.initialize_data()
 
-    precice_dt = interface.initialize()
+  t = 0
 
-    if interface.is_action_required(action_write_initial_data()):
-        writedata = numpy.zeros(len(dataIndices))
-        interface.write_block_scalar_data(writedataID, dataIndices, writedata)
-        interface.mark_action_fulfilled(action_write_initial_data())
+  # initial condition
+  sqr = domain.integral('(u - uexact)^2' @ ns, degree=degree*2)
+  lhs0 = nutils.solver.optimize('lhs', sqr, droptol=1e-15, arguments=dict(t=t))
+  bezier = domain.sample('bezier', degree * 2)
+  x, u, uexact = bezier.eval(['x_i', 'u', 'uexact'] @ ns, lhs=lhs0, t=t)
+  with treelog.add(treelog.DataLog()):
+    nutils.export.vtk(side + '-0', bezier.tri, x, Temperature=u, reference=uexact)
+                        
+  t += precice_dt
+  timestep = 1
+  dt = 0.1
 
-    interface.initialize_data()
+  while interface.is_coupling_ongoing():
+  
+    # update (time-dependent) boundary condition
+    cons0 = nutils.solver.optimize('lhs', sqr0, droptol=1e-15, arguments=dict(t=t))
 
-    t = 0
+    # read data from interface
+    if interface.is_read_data_available():
+      read_data = interface.read_block_scalar_data(read_data_id, vertex_ids)
+      read_function = coupling_sample.asfunction(read_data)
 
-    projection = domain.integral(
-        '(u - (1 + x_0 x_0 + alpha x_1 x_1 + beta ?t))^2' @ ns, degree=degree*2)
-    lhs0 = nutils.solver.optimize(
-        'lhs', projection, droptol=1e-15, arguments=dict(t=t))
-    bezier = domain.sample('bezier', degree * 2)
-    x, u, uexact = bezier.eval(['x_i', 'u', 'uexact'] @ ns, lhs=lhs0, t=t)
+      if side == 'Dirichlet':
+        sqr = coupling_sample.integral((ns.u - read_function)**2)
+        cons = nutils.solver.optimize('lhs', sqr, droptol=1e-15, constrain=cons0, arguments=dict(t=t))
+        res = res0
+      else:
+        cons = cons0
+        res = res0 + coupling_sample.integral(ns.basis * read_function * dx_function)
+            
+    # save checkpoint
+    if interface.is_action_required(precice.action_write_iteration_checkpoint()):
+      lhs_checkpoint = lhs0
+      interface.mark_action_fulfilled(precice.action_write_iteration_checkpoint())
 
-    with treelog.add(treelog.DataLog()):
-        nutils.export.vtk(side + '-0',
-                          bezier.tri, x, Temperature=u, reference=uexact)
-                          
-    t += precice_dt
-    timestep = 1
+    # potentially adjust non-matching timestep sizes 
+    dt = min(dt, precice_dt)
 
-    cons0 = nutils.solver.optimize(
-        'lhs', sqr0, droptol=1e-15, arguments=dict(t=t))
+    # solve nutils timestep
+    lhs = nutils.solver.solve_linear('lhs', res, constrain=cons, arguments=dict(lhs0=lhs0, dt=dt, t=t))
 
-    projectionmatrix = couplinginterface.integrate(
-        nutils.function.outer(ns.basis), degree=degree * 2)
-    projectioncons = numpy.zeros(res0.shape)
-    projectioncons[projectionmatrix.rowsupp(1e-15)] = numpy.nan
+    # write data to interface
+    if interface.is_write_data_required(dt):
+      if side == 'Dirichlet':
+        flux_function = res.eval(lhs0=lhs0, lhs=lhs, dt=dt, t=t)
+        write_data = coupling_sample.eval('flux' @ ns, fluxdofs=fluxdofs(flux_function))
+      else:
+        write_data = coupling_sample.eval('u' @ ns, lhs=lhs)
 
-    def fluxdofs(v): return projectionmatrix.solve(
-        v, constrain=projectioncons)
+      interface.write_block_scalar_data(write_data_id, vertex_ids, write_data)
 
-    while interface.is_coupling_ongoing():
-        if interface.is_action_required(action_write_iteration_checkpoint()):
-            lhscheckpoint = lhs0
-            conscheckpoint = cons0
-            interface.mark_action_fulfilled(
-                action_write_iteration_checkpoint())
+    # do the coupling
+    precice_dt = interface.advance(dt)
 
-        if interface.is_read_data_available():
-            readdata = interface.read_block_scalar_data(
-                readdataID, dataIndices)
-            coupledata = couplingsample.asfunction(readdata)
+    # read checkpoint if required
+    if interface.is_action_required(precice.action_read_iteration_checkpoint()):
+      lhs0 = lhs_checkpoint
+      interface.mark_action_fulfilled(precice.action_read_iteration_checkpoint())
+    else: # go to next timestep
+      bezier = domain.sample('bezier', degree * 2)
+      x, u, uexact = bezier.eval(['x_i', 'u', 'uexact'] @ ns, lhs=lhs, t=t)
 
-            if side == 'Dirichlet':
-                sqr = couplingsample.integral((ns.u - coupledata)**2)
-                cons = nutils.solver.optimize(
-                    'lhs', sqr, droptol=1e-15, constrain=cons0, arguments=dict(t=t))
-                res = res0
-            else:
-                cons = cons0
-                res = res0 + couplingsample.integral(ns.basis * coupledata / (nelems-1))
+      with treelog.add(treelog.DataLog()):
+        nutils.export.vtk(side + "-" + str(timestep), bezier.tri, x, Temperature=u, reference=uexact)
+                                
+      t += dt
+      timestep += 1
+      lhs0 = lhs
 
-        dt = min(timestepsize, precice_dt)
-
-        lhs = nutils.solver.solve_linear(
-            'lhs', res, constrain=cons, arguments=dict(lhs0=lhs0, dt=dt, t=t))
-
-        if interface.is_write_data_required(dt):
-            if side == 'Dirichlet':
-                fluxvalues = res.eval(lhs0=lhs0, lhs=lhs, dt=dt, t=t)
-                writedata = couplingsample.eval(
-                    'flux' @ ns, fluxdofs=fluxdofs(fluxvalues)) * (nelems-1)
-            else:
-                writedata = couplingsample.eval('u' @ ns, lhs=lhs)
-
-            interface.write_block_scalar_data(
-                writedataID, dataIndices, writedata)
-
-        precice_dt = interface.advance(dt)
-
-        if interface.is_action_required(action_read_iteration_checkpoint()):
-            lhs0 = lhscheckpoint
-            cons0 = conscheckpoint
-            interface.mark_action_fulfilled(action_read_iteration_checkpoint())
-        else:
-            # prepare for next timestep
-            lhs0 = lhs
-            cons0 = nutils.solver.optimize(
-                'lhs', sqr0, droptol=1e-15, arguments=dict(t=t+dt))
-
-            if interface.is_time_window_complete():
-                bezier = domain.sample('bezier', degree * 2)
-                x, u, uexact = bezier.eval(
-                    ['x_i', 'u', 'uexact'] @ ns, lhs=lhs0, t=t)
-
-                with treelog.add(treelog.DataLog()):
-                    nutils.export.vtk(side + "-" + str(timestep),
-                                      bezier.tri, x, Temperature=u, reference=uexact)
-                                      
-            t += dt
-            timestep += 1
-
-    interface.finalize()
+  interface.finalize()
 
 
 if __name__ == '__main__':
