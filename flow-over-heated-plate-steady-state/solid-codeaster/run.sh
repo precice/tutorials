240973875fe955285ab0459b421718a1adb35ab1
Gerasimos Chourdakis <makishourdakis@gmail.com>
2021-04-11 13:21:13 +0200

    Tidy up code_aster run script

--- flow-over-heated-plate-steady-state/solid-codeaster/run.sh
+++ flow-over-heated-plate-steady-state/solid-codeaster/run.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
-    export TUTORIAL_ROOT=${PWD}
-    export PRECICE_PARTICIPANT=Solid
-    as_run --run solid.export
+#!/bin/sh
+set -e -u
+
+export TUTORIAL_ROOT=${PWD}
+export PRECICE_PARTICIPANT=Solid
+as_run --run solid.export
