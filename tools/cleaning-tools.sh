45b3876f6acb82bf85df625b837e028dcfc69a51
Gerasimos Chourdakis <makishourdakis@gmail.com>
2021-04-11 15:27:56 +0200

    Clarify heat exchanger CalculiX gitignore, add 'dummy' to clean targets

--- tools/cleaning-tools.sh
+++ tools/cleaning-tools.sh
@@ -41,7 +41,7 @@ clean_calculix() {
         set -e -u
         cd "$1"
         echo "--- Cleaning up CalculiX case in $(pwd)"
-        rm -fv ./*.cvg ./*.dat ./*.frd ./*.sta ./*.12d spooles.out
+        rm -fv ./*.cvg ./*.dat ./*.frd ./*.sta ./*.12d spooles.out dummy
         clean_precice_logs .
     )
 }
