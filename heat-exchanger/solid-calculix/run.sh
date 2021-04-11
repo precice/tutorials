54494d86458103aea6ffc8a9c5244095c4801dc4
Gerasimos Chourdakis <makishourdakis@gmail.com>
2021-04-11 12:51:54 +0200

    Simplify CalculiX run scripts

--- heat-exchanger/solid-calculix/run.sh
+++ heat-exchanger/solid-calculix/run.sh
@@ -1,21 +1,11 @@
-#!/bin/bash
-cd ${0%/*} || exit 1    # Run from this directory
-
-# This script prepares and runs all the participants in one terminal,
-# forwarding the solvers' output to log files.
-# You need to first download the mesh files separately using the Download_meshes script.
-# The script "Allclean" cleans-up the result and log files.
+#!/bin/sh
+set -e -u
 
 if [ ! -f all.msh ]; then
     echo "Mesh files not found. Use the Download_meshes script to download them."
     exit
 fi
 
-Participant="Solid"
-Solver="ccx_preCICE"
-
-# Run and get the process id
-echo "Starting the ${Participant} participant..."
 export OMP_NUM_THREADS=1
 export CCX_NPROC_EQUATION_SOLVER=1
-${Solver} -i solid -precice-participant ${Participant}
+ccx_preCICE -i solid -precice-participant Solid
