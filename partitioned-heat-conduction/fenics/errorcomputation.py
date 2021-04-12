15760a8596a777a544fc07996dd98a506bc29b99
BenjaminRodenberg <benjamin.rodenberg@in.tum.de>
2021-04-12 20:30:01 +0200

    Minor reformatting.

--- partitioned-heat-conduction/fenics/errorcomputation.py
+++ partitioned-heat-conduction/fenics/errorcomputation.py
@@ -2,10 +2,12 @@ from fenics import inner, assemble, dx, project, sqrt
 
 
 def compute_errors(u_approx, u_ref, V, total_error_tol=10 ** -4):
-    error_normalized = (u_ref - u_approx) / u_ref  # compute pointwise L2 error
-    error_pointwise = project(abs(error_normalized), V)  # project onto function space
-    error_total = sqrt(
-        assemble(inner(error_pointwise, error_pointwise) * dx))  # determine L2 norm to estimate total error
+    # compute pointwise L2 error
+    error_normalized = (u_ref - u_approx) / u_ref
+    # project onto function space
+    error_pointwise = project(abs(error_normalized), V)
+    # determine L2 norm to estimate total error
+    error_total = sqrt(assemble(inner(error_pointwise, error_pointwise) * dx))
     error_pointwise.rename("error", " ")
 
     assert (error_total < total_error_tol)
