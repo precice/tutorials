69dc9db1e547c3dbdc24270e7da3b4c8b8cd6eb0
BenjaminRodenberg <benjamin.rodenberg@in.tum.de>
2021-04-13 18:28:52 +0200

    Fix PEP8 formatting.

--- flow-over-heated-plate/plot-final-interface-temperature.py
+++ flow-over-heated-plate/plot-final-interface-temperature.py
@@ -5,7 +5,7 @@ import numpy as np
 
 def vtk_to_dict(case):
     vtkFileName = "solid-{}/preCICE-output/Fluid-Mesh-Solid.final.vtk".format(case)
-    #read the vtk file as an unstructured grid
+    # read the vtk file as an unstructured grid
     reader = vtk.vtkUnstructuredGridReader()
     reader.SetFileName(vtkFileName)
     reader.ReadAllVectorsOn()
@@ -26,15 +26,17 @@ def vtk_to_dict(case):
         if(this_data_name == name):
             data_id = i
             break
-        i+=1
-        
+        i += 1
+
     data_dict = {}
 
     if not data_id:
-        raise Exception("For file {} name {} not found. Only the following names are available: {}. Aborting!".format(vtkFileName, name, data_names))
+        raise Exception(
+            "For file {} name {} not found. Only the following names are available: {}. "
+            "Aborting!".format(vtkFileName, name, data_names))
     for i in range(n_data):
         data_dict[data.GetPoint(i)] = data.GetPointData().GetArray(data_id).GetValue(i)
-    
+
     return data_dict
 
 
@@ -44,15 +46,15 @@ cases.append('openfoam')
 cases.append('nutils')
 
 case_labels = {'fenics': 'fenics-openfoam', 'openfoam': 'openfoam-openfoam', 'nutils': 'nutils-openfoam', }
-styles = [':','-','--']
-colors = ['r','b','g','k']
+styles = [':', '-', '--']
+colors = ['r', 'b', 'g', 'k']
 i = 0
 
 for case in cases:
     case_data = vtk_to_dict(case)
     x, t = [p[0] for p in case_data.keys()], np.array(list(case_data.values()))
     theta = (t - 300) / (310 - 300)
-    plt.plot(x, theta, colors[i%4]+styles[i%3], label=case_labels[case])
+    plt.plot(x, theta, colors[i % 4] + styles[i % 3], label=case_labels[case])
     i += 1
 
 plt.ylabel("Theta")
