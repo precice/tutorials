42deaf60ee11538be5809b793ad47c1b048f4653
Frédéric Simonis <simonisfrederic@gmail.com>
2021-04-14 12:03:58 +0200

    Remove parallel from run script

--- elastic-tube-1d/fluid-cpp/run.sh
+++ elastic-tube-1d/fluid-cpp/run.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 set -e -u
 
-if [ "${1:-}" = "-parallel" ]; then
-    mpiexec -np 4 ./build/FluidSolver ../precice-config.xml 100 0.01 100 -parallel
-else
-    ./build/FluidSolver ../precice-config.xml 100 0.01 100
-fi
+./build/FluidSolver ../precice-config.xml 100 0.01 100
