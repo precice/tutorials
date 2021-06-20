b6a59ab05b5ca1b984e45a9ca70abfdefd4e022e
Gerasimos Chourdakis <makishourdakis@gmail.com>
2021-06-20 22:50:13 +0200

    What is going on here?

--- .github/workflows/install-solvers-dependencies.sh
+++ .github/workflows/install-solvers-dependencies.sh
@@ -6,3 +6,5 @@ sudo apt-get install openfoam2012-dev
 
 # Install preCICE dependencies
 sudo apt-get install -y cmake libeigen3-dev libxml2-dev libboost-all-dev petsc-dev python3-dev python3-numpy
+
+# test
