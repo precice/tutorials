2a7ba0d87d107aadf9e5dfdf5fb799ad6a3b90af
DavidSCN <david.schneider@ipvs.uni-stuttgart.de>
2021-06-29 08:12:14 +0200

    Remove license header and write some custom text

--- partitioned-heat-conduction/openfoam-neumann/solver/heatTransfer.C
+++ partitioned-heat-conduction/openfoam-neumann/solver/heatTransfer.C
@@ -1,60 +1,36 @@
-/*---------------------------------------------------------------------------*\
-  =========                 |
-  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
-   \\    /   O peration     |
-    \\  /    A nd           | www.openfoam.com
-     \\/     M anipulation  |
--------------------------------------------------------------------------------
-    Copyright (C) 2011-2017 OpenFOAM Foundation
-    Copyright (C) 2019 OpenCFD Ltd.
--------------------------------------------------------------------------------
-License
-    This file is part of OpenFOAM.
+// This solver is based on previous work of OpenCFD Ltd. In particular, major code
+// parts are part of the laplacianFoam solver of OpenFOAM 2012.
+// -------------------------------------------------------------------------------
+// Application
+//    heatTransfer
+//
+// Group
+//    grpBasicSolvers
+//
+// Description
+//    Modified version of the Laplace equation solver for a scalar quantity with
+//    a non-zero RHS.
+//
+//    \heading Solver details
+//    The solver is applicable to, e.g. for thermal diffusion in a solid.  The
+//    equation is given by:
+//
+//    \f[
+//        \ddt{T}  = \div \left( D_T \grad T \right) + F
+//    \f]
+//
+//    Where:
+//    \vartable
+//        T     | Scalar field which is solved for, e.g. temperature
+//        D_T   | Diffusion coefficient
+//        F     | The RHS which is defined as: beta - 2 - 2 * alpha
+//    \endvartable
+//
+//    \heading Required fields
+//    \plaintable
+//        T     | Scalar field which is solved for, e.g. temperature
+//    \endplaintable
 
-    OpenFOAM is free software: you can redistribute it and/or modify it
-    under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
-    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-    for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
-
-Application
-    heatTransfer
-
-Group
-    grpBasicSolvers
-
-Description
-    Modified version of the Laplace equation solver for a scalar quantity with
-    a non-zero RHS.
-
-    \heading Solver details
-    The solver is applicable to, e.g. for thermal diffusion in a solid.  The
-    equation is given by:
-
-    \f[
-        \ddt{T}  = \div \left( D_T \grad T \right) + F
-    \f]
-
-    Where:
-    \vartable
-        T     | Scalar field which is solved for, e.g. temperature
-        D_T   | Diffusion coefficient
-        F     | The RHS which is defined as: beta - 2 - 2 * alpha
-    \endvartable
-
-    \heading Required fields
-    \plaintable
-        T     | Scalar field which is solved for, e.g. temperature
-    \endplaintable
-
-\*---------------------------------------------------------------------------*/
 
 #include "fvCFD.H"
 #include "fvOptions.H"
