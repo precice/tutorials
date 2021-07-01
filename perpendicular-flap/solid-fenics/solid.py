c2e243704b2b2841cc163cdb558309ff645b3dbd
Benjamin Rodenberg <benjamin.rodenberg@in.tum.de>
2021-07-01 09:31:10 +0200

    Some more obvious changes.

--- perpendicular-flap/solid-fenics/solid.py
+++ perpendicular-flap/solid-fenics/solid.py
@@ -60,10 +60,11 @@ v_n = Function(V)
 a_n = Function(V)
 
 
-f_N_function = interpolate(Expression(("1","0"), degree=1), V)
-u_function = interpolate(Expression(("0","0"), degree=1), V)
+f_N_function = interpolate(Expression(("1", "0"), degree=1), V)
+u_function = interpolate(Expression(("0", "0"), degree=1), V)
 
-coupling_boundary = AutoSubDomain(Neumann_Boundary)
+coupling_boundary = AutoSubDomain(neumann_boundary)
+fixed_boundary = AutoSubDomain(clamped_boundary)
 
 
 # get the adapter ready
@@ -88,79 +89,85 @@ dt = Constant(precice_dt)
 force_boundary = AutoSubDomain(Neumann_Boundary)
 
 # clamp the beam at the bottom
-bc = DirichletBC(V, Constant((0,0)), clamped_boundary)
+bc = DirichletBC(V, Constant((0, 0)), fixed_boundary)
 
 # alpha method parameters
+alpha_m = Constant(0)
+alpha_f = Constant(0)
+gamma = Constant(0.5 + alpha_f - alpha_m)
+beta = Constant((gamma + 0.5)**2 / 4.)
 
-alpha_m = Constant(0.0)
-alpha_f = Constant(0.0)
-gamma   = Constant(0.5+alpha_f-alpha_m)
-beta    = Constant((gamma+0.5)**2/4.)
 
-
-#Define strain 
+# Define strain
 def epsilon(u):
-    return 0.5*(nabla_grad(u) + nabla_grad(u).T)
+    return 0.5 * (nabla_grad(u) + nabla_grad(u).T)
+
 
 # Define Stress tensor
 def sigma(u):
-    return lambda_*nabla_div(u)*Identity(dim) + 2*mu*epsilon(u)
+    return lambda_ * nabla_div(u) * Identity(dim) + 2 * mu * epsilon(u)
+
 
 # Define Mass form
-def m(u,v):
-    return rho*inner(u,v)*dx
+def m(u, v):
+    return rho * inner(u, v) * dx
+
 
 # Elastic stiffness form
-def k(u,v):
+def k(u, v):
     return inner(sigma(u), sym(grad(v))) * dx
+
 
 # External Work
 def Wext(u_):
-    return dot(u_,p)*ds
+    return dot(u_, p) * ds
 
 
 # Update functions
 
-# Update accelleration
-def update_a(u, u_old,v_old, a_old, ufl=True):
+# Update acceleration
+def update_a(u, u_old, v_old, a_old, ufl=True):
     if ufl:
-        dt_=dt
-        beta_=beta
+        dt_ = dt
+        beta_ = beta
     else:
-        dt_=float(dt)
-        beta_=float(beta)
-    
-    return ((u - u_old - dt_*v_old)/beta/dt_**2 
-            - (1-2*beta_)/2/beta_*a_old)
+        dt_ = float(dt)
+        beta_ = float(beta)
+
+    return ((u - u_old - dt_ * v_old) / beta / dt_ ** 2
+            - (1 - 2 * beta_) / 2 / beta_ * a_old)
+
 
 # Update velocity
-def update_v(a,u_old,v_old,a_old, ufl = True):
+def update_v(a, u_old, v_old, a_old, ufl=True):
     if ufl:
-        dt_=dt
-        gamma_=gamma
+        dt_ = dt
+        gamma_ = gamma
     else:
-        dt_=float(dt)
-        gamma_=float(gamma)
-    
-    return v_old + dt_*((1-gamma_)*a_old + gamma_*a)
+        dt_ = float(dt)
+        gamma_ = float(gamma)
+
+    return v_old + dt_ * ((1 - gamma_) * a_old + gamma_ * a)
+
 
 def update_fields(u, u_old, v_old, a_old):
-    '''Update all fields at the end of a timestep.'''
-    
+    """Update all fields at the end of a timestep."""
+
     u_vec, u0_vec = u.vector(), u_old.vector()
     v0_vec, a0_vec = v_old.vector(), a_old.vector()
-    
-    #call update functions
+
+    # call update functions
     a_vec = update_a(u_vec, u0_vec, v0_vec, a0_vec, ufl=False)
     v_vec = update_v(a_vec, u0_vec, v0_vec, a0_vec, ufl=False)
-    
-    #assign u->u_old
+
+    # assign u->u_old
     v_old.vector()[:], a_old.vector()[:] = v_vec, a_vec
     u_old.vector()[:] = u.vector()
 
 
 def avg(x_old, x_new, alpha):
-    return alpha*x_old + (1-alpha)*x_new
+    return alpha * x_old + (1 - alpha) * x_new
+
 
 # residual
 a_np1 = update_a(du, u_n, v_n, a_n, ufl=True)
@@ -184,9 +191,8 @@ time.append(0.0)
 u_tip.append(0.0)
 E_ext = 0
 
-
 displacement_out = File("Solid/FSI-S/u_fsi.pvd")
-    
+
 u_n.rename("Displacement", "")
 u_np1.rename("Displacement", "")
 displacement_out << u_n
@@ -222,14 +228,14 @@ xdmf_file.parameters["rewrite_function_mesh"] = False
 while precice.is_coupling_ongoing():
     A, b = assemble_system(a_form, L_form, bc)
 
-    b_forces = b.copy() # b is the same for every iteration, only forces change
-    
+    b_forces = b.copy()  # b is the same for every iteration, only forces change
+
     for ps in Forces_x:
         ps.apply(b_forces)
     for ps in Forces_y:
         ps.apply(b_forces)
-        
-    assert(b is not b_forces)
+
+    assert (b is not b_forces)
     solve(A, u_np1.vector(), b_forces)
     
     t, n, precice_timestep_complete, precice_dt, Forces_x, Forces_y = precice.advance(u_np1, u_np1, u_n, t, float(dt), n)
