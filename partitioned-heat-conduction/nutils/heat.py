c477cc4c0c4a5899d45cc58ad1cce472249fcbf0
DavidSCN <david.schneider@ipvs.uni-stuttgart.de>
2021-07-09 09:57:46 +0200

    Add an additional Dirichlet participant

--- partitioned-heat-conduction/nutils/heat.py
+++ partitioned-heat-conduction/nutils/heat.py
@@ -68,7 +68,7 @@ def main(side='Dirichlet'):
     vertex_ids = interface.set_mesh_vertices(mesh_id, vertices)
 
     # coupling data
-    write_data = "Temperature" if side == "Neumann" else "Flux"
+    write_data = "Temperature" if side == "Neumann" else "Heat-Flux"
     read_data = "Heat-Flux" if side == "Neumann" else "Temperature"
     write_data_id = interface.get_data_id(write_data, mesh_id)
     read_data_id = interface.get_data_id(read_data, mesh_id)
---
+++ partitioned-heat-conduction/openfoam-dirichlet/setInitialField.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+funkySetFields -field T -expression '1+pow(pos().x,2)+(3*pow(pos().y,2))+1.3*time()' -time '0'
+
