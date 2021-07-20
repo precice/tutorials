c01e7a1a7ba46a9ecfa412fd5cd0020ee51b7dbc
DavidSCN <david.schneider@ipvs.uni-stuttgart.de>
2021-07-20 15:31:35 +0200

    Plug everything in one script to enable easy mesh modifications

--- partitioned-heat-conduction/openfoam-dirichlet/run.sh
+++ partitioned-heat-conduction/openfoam-dirichlet/run.sh
@@ -3,6 +3,7 @@ set -e -u
 
 blockMesh
 touch openfoam-dirichlet.foam
+./setInitialField.sh
 
 ../../tools/run-openfoam.sh "$@"
 . ../../tools/openfoam-remove-empty-dirs.sh && openfoam_remove_empty_dirs
--- partitioned-heat-conduction/openfoam-neumann/run.sh
+++ partitioned-heat-conduction/openfoam-neumann/run.sh
@@ -3,6 +3,7 @@ set -e -u
 
 blockMesh
 touch openfoam-neumann.foam
+./setInitialField.sh
 
 ../../tools/run-openfoam.sh "$@"
 . ../../tools/openfoam-remove-empty-dirs.sh && openfoam_remove_empty_dirs
