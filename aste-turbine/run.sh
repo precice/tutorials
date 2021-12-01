6a01ee2a4bc2c2a1ca8ff26491e509ed49f53dfd
Kursat_Yurt <kursatyurt@gmail.com>
2021-12-01 09:30:30 +0100

    Remove ./ from scripts since they are in the path

--- aste-turbine/run.sh
+++ aste-turbine/run.sh
@@ -10,11 +10,11 @@ test -f meshes.tar.gz  || wget https://gitlab.lrz.de/precice/precice2-ref-paper-
 tar -xf meshes.tar.gz
 
 # Calculate on fine mesh
-./vtk_calculator.py 0.009.vtk x+y -t "x + y"
+vtk_calculator.py 0.009.vtk x+y -t "x + y"
 
 # Decompose both meshes to two procesors
-./partition_mesh.py 0.009.vtk -n 2 -o fine_mesh
-./partition_mesh.py 0.01.vtk -n 2 -o coarse_mesh
+partition_mesh.py 0.009.vtk -n 2 -o fine_mesh
+partition_mesh.py 0.01.vtk -n 2 -o coarse_mesh
 
 # The result directory of preciceMap needs to exist beforehand
 mkdir -p mapped
@@ -25,8 +25,8 @@ mpirun -n 2 preciceMap -v -p B --mesh coarse_mesh/coarse_mesh --output mapped/ma
 
 # Join the output files together to result.vtk,
 # Recovery cannot be used since GlobalID's are not exist in mapped mesh
-./join_mesh.py mapped/mapped -o result.vtk
+join_mesh.py mapped/mapped -o result.vtk
 
 # Measure the difference between the original function and the mapped values
 # Save into data array called difference
-./vtk_calculator.py result.vtk x+y -t difference -it "x + y" --diff
+vtk_calculator.py result.vtk x+y -t difference -it "x + y" --diff
