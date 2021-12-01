d4d27faed01691b1f9d32cdb39758c3561a77eb0
Kursat_Yurt <kursatyurt@gmail.com>
2021-12-01 09:39:50 +0100

    Expand cleaning script

--- tools/clean-tutorial-base.sh
+++ tools/clean-tutorial-base.sh
@@ -4,4 +4,9 @@ set -e -u
 # shellcheck disable=SC1091
 . ../tools/cleaning-tools.sh
 
+rm -fv result.vtk 
+rm -fvr fine_mesh coarse_mesh mapped
 clean_tutorial .
+clean_precice_logs .
+rm -fv *.log
+
