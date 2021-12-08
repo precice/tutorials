6b72935b2f65c39c1e4e753ef0fcce703ba26b3d
Gerasimos Chourdakis <chourdak@in.tum.de>
2021-12-08 10:43:40 +0100

    Fix shellcheck notice on solid-codeaster/run.sh
    Reported by the latest shellcheck as
    Notice: ./flow-over-heated-plate-steady-state/solid-codeaster/run.sh:10:22: note: Double quote to prevent globbing and word splitting. [SC2086]

--- flow-over-heated-plate-steady-state/solid-codeaster/run.sh
+++ flow-over-heated-plate-steady-state/solid-codeaster/run.sh
@@ -7,6 +7,6 @@ echo "See the tutorial and code_aster adapter documentation pages for more:"
 echo "https://www.precice.org/adapter-code_aster.html"
 echo ""
 
-export TUTORIAL_ROOT=${PWD}
+export TUTORIAL_ROOT="${PWD}"
 export PRECICE_PARTICIPANT=Solid
 as_run --run solid.export
