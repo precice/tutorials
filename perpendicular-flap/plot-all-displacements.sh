f16e8b00f60c3016dd5ce53d370f201d876511ff
Benjamin Rodenberg <benjamin.rodenberg@in.tum.de>
2022-01-31 13:38:21 +0100

    Enhance plot quality.

--- perpendicular-flap/plot-all-displacements.sh
+++ perpendicular-flap/plot-all-displacements.sh
@@ -4,7 +4,9 @@ gnuplot -p << EOF
 	set grid                                                                        
 	set title 'x-displacement of the flap tip'                                        
 	set xlabel 'time [s]'                                                           
-	set ylabel 'x-displacement [m]'                                                 
+	set ylabel 'x-displacement [m]'
+	set term pngcairo enhanced size 900,654
+	set output "images/tutorials-perpendicular-flap-displacement-all-watchpoints.png"
 	plot "watchpoints/openfoam-calculix.log" using 1:4 with lines title "OpenFOAM-CalculiX", \
 	     "watchpoints/openfoam-dealii.log" using 1:4 with lines title "OpenFOAM-deal.II", \
 	     "watchpoints/openfoam-fenics.log" using 1:4 with lines title "OpenFOAM-FEniCS", \
