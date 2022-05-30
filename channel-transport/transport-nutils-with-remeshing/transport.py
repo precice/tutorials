ef20c7ad3e335fbed3e1601a576648b06c7a67fa
Ishaan Desai <ishaandesai@gmail.com>
2022-05-30 10:48:48 +0200

    Working adaptive mesh refinement case, along with preCICE remeshing calls

--- channel-transport/transport-nutils-with-remeshing/transport.py
+++ channel-transport/transport-nutils-with-remeshing/transport.py
@@ -9,12 +9,84 @@ import treelog as log
 import numpy as np
 import precice
 from mpi4py import MPI
+import math
 
 
-def reinitialize_namespace(domain):
+def reinitialize_namespace(domain, geom):
+    # cloud of Gauss points
+    gauss = domain.sample("gauss", degree=2)
+
+    # Nutils namespace
+    ns = function.Namespace(fallback_length=2)
+    ns.x = geom
+    ns.basis = domain.basis("h-std", degree=1)  # linear finite elements
+    ns.u = "basis_n ?lhs_n"  # solution
+    ns.projectedu = "basis_n ?projectedlhs_n"
+    ns.gradu = "u_,i"  # gradient of solution
+    ns.dudt = "basis_n (?lhs_n - ?lhs0_n) / ?dt"  # time derivative
+    ns.vbasis = gauss.basis()
+    ns.velocity_i = "vbasis_n ?velocity_ni"
+    ns.k = 0.1  # diffusivity
+    ns.xblob = 1, 1
+    ns.uinit = ".5 - .5 tanh(((x_i - xblob_i) (x_i - xblob_i) - .5) / .1)"  # blob
+
+    # define the weak form
+    res = gauss.integral("(basis_n (dudt + (velocity_i u)_,i) + k basis_n,i u_,i) d:x" @ ns)
+
+    # define Dirichlet boundary condition
+    sqr = domain.boundary["inflow"].integral("u^2 d:x" @ ns, degree=2)
+    cons = solver.optimize("lhs", sqr, droptol=1e-15)
+
+    return ns, res, cons, gauss
+
+
+def refine_mesh(ns, domain_coarse, domain_nm1, solu_nm1):
+    """
+    At the time of the calling of this function a predicted solution exists in ns.phi
+    """
+    # ----- Refine the coarse mesh according to the projected solution to get a predicted refined topology ----
+    domain_ref = domain_coarse
+    for level in range(3):
+        print("refinement level = {}".format(level))
+        domain_union1 = domain_nm1 & domain_ref
+        smpl = domain_union1.sample('uniform', 5)
+        ielem, criterion = smpl.eval([domain.f_index, function.sqrt(ns.gradu[0]**2 + ns.gradu[1]**2) > 2.0], lhs=lhs0)
+
+        # Refine the elements for which at least one point tests true.
+        domain_ref = domain_ref.refined_by(np.unique(ielem[criterion]))
+        # ----------------------------------------------------------------------------------------------------
+
+    # Create a new projection mesh which is the union of the previous refined mesh and the predicted mesh
+    domain_union = domain_nm1 & domain_ref
+
+    # ----- Project the solution of the last time step on the projection mesh -----
+    ns.projectedu = function.dotarg('projectedsolu', domain_ref.basis('h-std', degree=1))
+    sqru = domain_union.integral((ns.projectedu - ns.u) ** 2, degree=2)
+    solu = solver.optimize('projectedsolu', sqru, droptol=1E-12, arguments=dict(solu=solu_nm1))
+
+    return domain, solu
+
+
+def main():
+
+    print("Running Nutils")
+
+    # Remeshing
+    n_remeshing = 10
+
+    # define the Nutils mesh
+    nx = 60
+    ny = 16
+    step_start = nx // 3
+    step_end = nx // 2
+    step_height = ny // 2
+
+    grid = np.linspace(0, 6, nx + 1), np.linspace(0, 2, ny + 1)
+    domain, geom = mesh.rectilinear(grid)
     domain = domain.withboundary(inflow="left", outflow="right", wall="top,bottom") - domain[
-        step_start:step_end, :step_hight
+        step_start:step_end, :step_height
     ].withboundary(wall="left,top,right")
+    domain_coarse = domain
 
     # cloud of Gauss points
     gauss = domain.sample("gauss", degree=2)
@@ -40,55 +112,31 @@ def reinitialize_namespace(domain):
     sqr = domain.boundary["inflow"].integral("u^2 d:x" @ ns, degree=2)
     cons = solver.optimize("lhs", sqr, droptol=1e-15)
 
-    return ns, res, cons
+    ns.xblob = 1, 1
+    ns.uinit = ".5 - .5 tanh(((x_i - xblob_i) (x_i - xblob_i) - .5) / .1)"  # blob
 
+    timestep = 0
+    dt = 0.005
 
-def refine_mesh(ns, domain_coarse, domain_nm1, solu_nm1):
-    """
-    At the time of the calling of this function a predicted solution exists in ns.phi
-    """
-    # ----- Refine the coarse mesh according to the projected solution to get a predicted refined topology ----
-    domain_ref = domain_coarse
+    # set blob as initial condition
+    sqr = domain.integral("(u - uinit)^2" @ ns, degree=2)
+    lhs0 = solver.optimize("lhs", sqr)
+
     for level in range(3):
         print("refinement level = {}".format(level))
-        domain_union1 = domain_nm1 & domain_ref
-        smpl = domain_union1.sample('uniform', 5)
-        ielem, criterion = smpl.eval([domain_nm1.f_index, abs(ns.gradu) > 0.1], lhs=solu_nm1)
+        smpl = domain.sample('uniform', 5)
+        ielem, criterion = smpl.eval([domain.f_index, function.sqrt(ns.gradu[0]**2 + ns.gradu[1]**2) > 2.0], lhs=lhs0)
+
+        print("ielem = {}, criterion = {}".format(ielem, criterion))
 
         # Refine the elements for which at least one point tests true.
-        domain_ref = domain_ref.refined_by(np.unique(ielem[criterion]))
-        # ----------------------------------------------------------------------------------------------------
+        domain = domain.refined_by(np.unique(ielem[criterion]))
 
-    # Create a new projection mesh which is the union of the previous refined mesh and the predicted mesh
-    domain_union = domain_nm1 & domain_ref
+        ns, res, cons, gauss = reinitialize_namespace(domain, geom)
 
-    # ----- Project the solution of the last time step on the projection mesh -----
-    ns.projectedu = function.dotarg('projectedsolu', domain_ref.basis('h-std', degree=1))
-    sqru = domain_union.integral((ns.projectedu - ns.u) ** 2, degree=2)
-    solu = solver.optimize('projectedsolu', sqru, droptol=1E-12, arguments=dict(solu=solu_nm1))
-
-    return domain, solu
-
-
-def main():
-
-    print("Running Nutils")
-
-    # define the Nutils mesh
-    nx = 120
-    ny = 32
-    step_start = nx // 3
-    step_end = nx // 2
-    step_hight = ny // 2
-
-    grid = np.linspace(0, 6, nx + 1), np.linspace(0, 2, ny + 1)
-    domain, geom = mesh.rectilinear(grid)
-    domain = domain.withboundary(inflow="left", outflow="right", wall="top,bottom") - domain[
-        step_start:step_end, :step_hight
-    ].withboundary(wall="left,top,right")
-    domain_coarse = domain
-
-    ns, res, cons = reinitialize_namespace(domain)
+        # set blob as initial condition after each refinement
+        sqr = domain.integral("(u - uinit)^2" @ ns, degree=2)
+        lhs0 = solver.optimize("lhs", sqr)
 
     # preCICE setup
     interface = precice.Interface("Transport", "../precice-config.xml", 0, 1)
@@ -104,31 +152,19 @@ def main():
 
     precice_dt = interface.initialize()
 
-    timestep = 0
-    dt = 0.005
-
-    # set blob as initial condition
-    sqr = domain.integral("(u - uinit)^2" @ ns, degree=2)
-    lhs0 = solver.optimize("lhs", sqr)
-
-    for level in range(3):
-        print("refinement level = {}".format(level))
-        smpl = domain.sample('uniform', 5)
-        ielem, criterion = smpl.eval([domain.f_index, abs(ns.gradu) > 0.1], lhs=lhs0)
-
-        # Refine the elements for which at least one point tests true.
-        domain = domain.refined_by(np.unique(ielem[criterion]))
-
-        reinitialize_namespace(topo)
-
-        # set blob as initial condition after each refinement
-        sqr = domain.integral("(u - uinit)^2" @ ns, degree=2)
-        lhs0 = solver.optimize("lhs", sqr)
-
     # initialize the velocity values
     velocity_values = np.zeros_like(vertices)
 
+    lhs = lhs0
+
     while interface.is_coupling_ongoing():
+
+        domain, lhs = refine_mesh(ns, domain_coarse, domain, lhs)
+        ns, res, cons, gauss = reinitialize_namespace(domain, geom)
+
+        vertices = gauss.eval(ns.x)
+        interface.reset_mesh(mesh_id)  # Throws away the entire mesh
+        vertex_ids = interface.set_mesh_vertices(mesh_id, vertices)  # Redefine the mesh
 
         if timestep % 1 == 0:  # visualize
             bezier = domain.sample("bezier", 2)
