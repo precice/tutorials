07daa50261a5b2c2fc6f765594ac4ccb0fa9ed8c
Kyle Davis <kyle.davis@ipvs.uni-stuttgart.de>
2022-06-29 17:33:51 +0200

    Adds breaking dam 3D test case

---
+++ breaking-dam-3d/fluid-openfoam/run.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -e -u
+
+#cp -r constant/polyMesh.orig constant/polyMesh
+touch fluid-openfoam.foam
+
+../../tools/run-openfoam.sh "$@"
+. ../../tools/openfoam-remove-empty-dirs.sh && openfoam_remove_empty_dirs
