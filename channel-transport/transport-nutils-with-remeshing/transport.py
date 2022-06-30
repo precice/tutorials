dc8338d07388059b51618acb9fb22a2a073565e5
Ishaan Desai <ishaandesai@gmail.com>
2022-06-30 15:15:15 +0200

    Fix several bugs

--- channel-transport/transport-nutils-with-remeshing/transport.py
+++ channel-transport/transport-nutils-with-remeshing/transport.py
@@ -20,10 +20,10 @@ def reinitialize_namespace(domain, geom):
     ns = function.Namespace(fallback_length=2)
     ns.x = geom
     ns.basis = domain.basis("h-std", degree=1)  # linear finite elements
-    ns.u = "basis_n ?lhs_n"  # solution
-    ns.projectedu = "basis_n ?projectedlhs_n"
+    ns.u = "basis_n ?solu_n"  # solution
+    ns.projectedu = "basis_n ?projectedsolu_n"
     ns.gradu = "u_,i"  # gradient of solution
-    ns.dudt = "basis_n (?lhs_n - ?lhs0_n) / ?dt"  # time derivative
+    ns.dudt = "basis_n (?solu_n - ?solu0_n) / ?dt"  # time derivative
     ns.vbasis = gauss.basis()
     ns.velocity_i = "vbasis_n ?velocity_ni"
     ns.k = 0.1  # diffusivity
@@ -35,7 +35,7 @@ def reinitialize_namespace(domain, geom):
 
     # define Dirichlet boundary condition
     sqr = domain.boundary["inflow"].integral("u^2 d:x" @ ns, degree=2)
-    cons = solver.optimize("lhs", sqr, droptol=1e-15)
+    cons = solver.optimize("solu", sqr, droptol=1e-15)
 
     return ns, res, cons, gauss
 
@@ -51,7 +51,7 @@ def refine_mesh(ns, domain_coarse, domain_nm1, solu_nm1):
         domain_union1 = domain_nm1 & domain_ref
         smpl = domain_union1.sample('uniform', 5)
         ielem, criterion = smpl.eval([domain_ref.f_index, function.sqrt(ns.gradu[0]**2 + ns.gradu[1]**2) > 2.0],
-                                     lhs=lhs0)
+                                     solu=solu_nm1)
 
         # Refine the elements for which at least one point tests true.
         domain_ref = domain_ref.refined_by(np.unique(ielem[criterion]))
@@ -65,7 +65,7 @@ def refine_mesh(ns, domain_coarse, domain_nm1, solu_nm1):
     sqru = domain_union.integral((ns.projectedu - ns.u) ** 2, degree=2)
     solu = solver.optimize('projectedsolu', sqru, droptol=1E-12, arguments=dict(solu=solu_nm1))
 
-    return domain, solu
+    return domain_ref, solu
 
 
 def main():
@@ -96,10 +96,10 @@ def main():
     ns = function.Namespace(fallback_length=2)
     ns.x = geom
     ns.basis = domain.basis("std", degree=1)  # linear finite elements
-    ns.u = "basis_n ?lhs_n"  # solution
-    ns.projectedu = "basis_n ?projectedlhs_n"
+    ns.u = "basis_n ?solu_n"  # solution
+    ns.projectedu = "basis_n ?projectedsolu_n"
     ns.gradu = "u_,i"  # gradient of solution
-    ns.dudt = "basis_n (?lhs_n - ?lhs0_n) / ?dt"  # time derivative
+    ns.dudt = "basis_n (?solu_n - ?solu0_n) / ?dt"  # time derivative
     ns.vbasis = gauss.basis()
     ns.velocity_i = "vbasis_n ?velocity_ni"
     ns.k = 0.1  # diffusivity
@@ -111,7 +111,7 @@ def main():
 
     # define Dirichlet boundary condition
     sqr = domain.boundary["inflow"].integral("u^2 d:x" @ ns, degree=2)
-    cons = solver.optimize("lhs", sqr, droptol=1e-15)
+    cons = solver.optimize("solu", sqr, droptol=1e-15)
 
     ns.xblob = 1, 1
     ns.uinit = ".5 - .5 tanh(((x_i - xblob_i) (x_i - xblob_i) - .5) / .1)"  # blob
@@ -121,12 +121,12 @@ def main():
 
     # set blob as initial condition
     sqr = domain.integral("(u - uinit)^2" @ ns, degree=2)
-    lhs0 = solver.optimize("lhs", sqr)
+    solu0 = solver.optimize("solu", sqr)
 
     for level in range(3):
         print("refinement level = {}".format(level))
         smpl = domain.sample('uniform', 5)
-        ielem, criterion = smpl.eval([domain.f_index, function.sqrt(ns.gradu[0]**2 + ns.gradu[1]**2) > 2.0], lhs=lhs0)
+        ielem, criterion = smpl.eval([domain.f_index, function.sqrt(ns.gradu[0]**2 + ns.gradu[1]**2) > 2.0], solu=solu0)
 
         # Refine the elements for which at least one point tests true.
         domain = domain.refined_by(np.unique(ielem[criterion]))
@@ -135,7 +135,7 @@ def main():
 
         # set blob as initial condition after each refinement
         sqr = domain.integral("(u - uinit)^2" @ ns, degree=2)
-        lhs0 = solver.optimize("lhs", sqr)
+        solu0 = solver.optimize("solu", sqr)
 
     # preCICE setup
     interface = precice.Interface("Transport", "../precice-config.xml", 0, 1)
@@ -154,12 +154,11 @@ def main():
     # initialize the velocity values
     velocity_values = np.zeros_like(vertices)
 
-    lhs = lhs0
+    solu = solu0
 
     while interface.is_coupling_ongoing():
-
         if timestep % n_remeshing == 0:
-            domain, lhs = refine_mesh(ns, domain_coarse, domain, lhs)
+            domain, solu = refine_mesh(ns, domain_coarse, domain, solu)
             ns, res, cons, gauss = reinitialize_namespace(domain, geom)
 
             vertices = gauss.eval(ns.x)
@@ -168,7 +167,7 @@ def main():
 
         if timestep % 1 == 0:  # visualize
             bezier = domain.sample("bezier", 2)
-            x, u = bezier.eval(["x_i", "u"] @ ns, lhs=lhs0)
+            x, u = bezier.eval(["x_i", "u"] @ ns, solu=solu0)
             with log.add(log.DataLog()):
                 export.vtk("Transport_" + str(timestep), bezier.tri, x, T=u)
 
@@ -180,16 +179,15 @@ def main():
         dt = min(dt, precice_dt)
 
         # solve nutils timestep
-        lhs = solver.solve_linear(
-            "lhs", res, constrain=cons, arguments=dict(lhs0=lhs0, dt=dt, velocity=velocity_values)
-        )
+        args = dict(solu0=solu0, dt=dt, velocity=velocity_values)
+        solu = solver.solve_linear("solu", res, constrain=cons, arguments=args)
 
         # do the coupling
         precice_dt = interface.advance(dt)
 
         # advance variables
         timestep += 1
-        lhs0 = lhs
+        solu0 = solu
 
     interface.finalize()
 
