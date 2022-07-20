a4fafda099bac1d62c60341e4bbc5ca5b2ccd5a1
Boris Martin <boris.martin.be@gmail.com>
2022-07-20 19:44:15 +0200

    format

--- flow-over-heated-plate-calculix/solid-calculix/generate_mesh.py
+++ flow-over-heated-plate-calculix/solid-calculix/generate_mesh.py
@@ -22,18 +22,18 @@ indices = dict()
 
 # Build nodes
 for i in range(0, n_x + 1):
-  for j in range(0, n_y + 1):
-    for k in range(0, n_z + 1):
-      nodes.append((xs[i], ys[j], zs[k]))
-      # CCX index starts at 1!
-      v = len(nodes)
-      indices[(i,j,k)] = v + 1
-                   
+    for j in range(0, n_y + 1):
+        for k in range(0, n_z + 1):
+            nodes.append((xs[i], ys[j], zs[k]))
+            # CCX index starts at 1!
+            v = len(nodes)
+            indices[(i, j, k)] = v + 1
+
 print("** Nodes")
 print("*Node, NSET=Nall")
 for v, (x, y, z) in enumerate(nodes):
-  print("{}, {:10.4f}, {:10.4f}, {:10.4f},".format(v + 2, x, y, z))
-  
+    print("{}, {:10.4f}, {:10.4f}, {:10.4f},".format(v + 2, x, y, z))
+
 # Build elements
 print("** Volume elements")
 print("* Element, TYPE=C3D8, ELSET=Evolumes")
@@ -42,20 +42,27 @@ elems_top_surface = []
 
 elem_id = 1
 for i in range(0, n_x):
-  for j in range(0, n_y):
-    for k in range(0, n_z):
-      print("{}, {}, {}, {}, {}, {}, {}, {}, {},".format(elem_id, 
-                                                          indices[(i,j,k)],
-                                                          indices[(i+1,j,k)],
-                                                          indices[(i+1,j+1,k)],
-                                                          indices[(i,j+1,k)],
-                                                          indices[(i,j,k+1)],
-                                                          indices[(i+1,j,k+1)],
-                                                          indices[(i+1,j+1,k+1)],
-                                                          indices[(i,j+1,k+1)],))
-      if j == n_y - 1:
-        elems_top_surface.append(elem_id)
-      elem_id += 1
+    for j in range(0, n_y):
+        for k in range(0, n_z):
+            print("{}, {}, {}, {}, {}, {}, {}, {}, {},".format(elem_id,
+                                                               indices[(
+                                                                   i, j, k)],
+                                                               indices[(
+                                                                   i+1, j, k)],
+                                                               indices[(
+                                                                   i+1, j+1, k)],
+                                                               indices[(
+                                                                   i, j+1, k)],
+                                                               indices[(
+                                                                   i, j, k+1)],
+                                                               indices[(
+                                                                   i+1, j, k+1)],
+                                                               indices[(
+                                                                   i+1, j+1, k+1)],
+                                                               indices[(i, j+1, k+1)],))
+            if j == n_y - 1:
+                elems_top_surface.append(elem_id)
+            elem_id += 1
 
 
 # Set of border. Adapt freely
@@ -63,20 +70,19 @@ print("** Nodes, border with y = y_end")
 print("*NSET, NSET=Nyend")
 str = ""
 for i in range(0, n_x + 1):
-  for k in range(0, n_z + 1):
-    str = "{}, ".format(indices[i,n_y,k])
-    print(str)
+    for k in range(0, n_z + 1):
+        str = "{}, ".format(indices[i, n_y, k])
+        print(str)
 print("** Nodes, border with y = y_begin")
 print("*NSET, NSET=Nybegin")
 str = ""
 for i in range(0, n_x + 1):
-  for k in range(0, n_z + 1):
-    str = "{}, ".format(indices[i,0,k])
-    print(str)
-
+    for k in range(0, n_z + 1):
+        str = "{}, ".format(indices[i, 0, k])
+        print(str)
 
 
 # Upper surface
 print("*SURFACE, NAME=Sflux_interface")
 for id in elems_top_surface:
-  print("{}, S5".format(id))
+    print("{}, S5".format(id))
