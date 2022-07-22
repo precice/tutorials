b11c9e84435ea173e323d746e8a2392cfc850bad
Boris Martin <boris.martin.be@gmail.com>
2022-07-22 10:58:26 +0200

    moved to one folder

--- perpendicular-flap/solid-calculix/run.sh
+++ perpendicular-flap/solid-calculix/run.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
 set -e -u
 
-ccx_preCICE -i flap -precice-participant Solid
+usage() { echo "Usage: run.sh [-modal]" 1>&2; exit 1; }
+
+if [ $# -ge 2 ] || [ $# -eq 1 -a $1 != "-modal" ]; then
+    usage
+fi
+#ccx_preCICE -i flap -precice-participant Solid
+
+
+if [ ${1-nonmodal} = "-modal" ]; then
+    echo "Modal"
+    echo ${1-nonmodal}
+else
+    echo "NonModal"
+    echo ${1-nonmodal}
+fi
