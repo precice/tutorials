4dccace6ac49f665f382b5f19d17be5cf61373d9
Philip Cardiff <philip.cardiff@ucd.ie>
2022-08-02 16:20:31 +0100

    tools/openfoam-remove-empty-dirs.sh: fix for displacement files (e.g. D, DD, pointD, ...) as used by solids4foam

--- tools/openfoam-remove-empty-dirs.sh
+++ tools/openfoam-remove-empty-dirs.sh
@@ -6,7 +6,7 @@ openfoam_remove_empty_dirs() {
 		echo "Looking for any time directories without results (e.g. stray functionObjectProperties files, see openfoam-adapter issue #26 on GitHub)..."
 
 		for f in [0-9]* [0-9]*.[0-9]*; do
-			if ! [ -f "${f}/U" ] && ! [ -f "${f}/T" ] && ! [ -f "${f}/U.gz" ] && ! [ -f "${f}/T.gz" ]; then
+                        if ! [ -f "${f}/U" ] && ! [ -f "${f}/T" ] && ! [ -f "${f}/U.gz" ] && ! [ -f "${f}/T.gz" ] && ! [ -f "${f}/D" ] && ! [ -f "${f}/pointD" ] && ! [ -f "${f}/DD" ] && ! [ -f "${f}/pointDD" ] && ! [ -f "${f}/D.gz" ] && ! [ -f "${f}/pointD.gz" ] && ! [ -f "${f}/DD.gz" ] && ! [ -f "${f}/pointDD.gz" ]; then
 				rm -rfv "${f}"
 			fi
 		done
@@ -14,7 +14,7 @@ openfoam_remove_empty_dirs() {
 			for d in processor*; do
 				cd "${d}"
 				for f in [0-9]* [0-9]*.[0-9]*; do
-					if ! [ -f "${f}/U" ] && ! [ -f "${f}/T" ] && ! [ -f "${f}/U.gz" ] && ! [ -f "${f}/T.gz" ]; then
+                                        if ! [ -f "${f}/U" ] && ! [ -f "${f}/T" ] && ! [ -f "${f}/U.gz" ] && ! [ -f "${f}/T.gz" ] && ! [ -f "${f}/D" ] && ! [ -f "${f}/pointD" ] && ! [ -f "${f}/DD" ] && ! [ -f "${f}/pointDD" ] && ! [ -f "${f}/D.gz" ] && ! [ -f "${f}/pointD.gz" ] && ! [ -f "${f}/DD.gz" ] && ! [ -f "${f}/pointDD.gz" ]; then
 						rm -rfv "${f}"
 					fi
 				done
