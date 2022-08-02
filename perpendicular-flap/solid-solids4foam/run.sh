1fa2df6f86412742679a562223bf54d853546961
Philip Cardiff <philip.cardiff@ucd.ie>
2022-08-02 17:23:57 +0100

    perpendicular-flap/solid-solids4foam: update foam file name in run.sh

--- perpendicular-flap/solid-solids4foam/run.sh
+++ perpendicular-flap/solid-solids4foam/run.sh
@@ -8,7 +8,7 @@ then
 fi
 
 blockMesh
-touch fluid-openfoam.foam
+touch solid-openfoam.foam
 
 ../../tools/run-openfoam.sh "$@"
 . ../../tools/openfoam-remove-empty-dirs.sh && openfoam_remove_empty_dirs
