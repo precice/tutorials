a85fb9a04e4795ed1f7f1f1a51c422d58a13bfd0
Philip Cardiff <philip.cardiff@ucd.ie>
2022-08-02 16:25:27 +0100

    perpendicular-flap/solid-solids4foam: update run.sh to be consistent with fluid-openfoam

--- perpendicular-flap/solid-solids4foam/run.sh
+++ perpendicular-flap/solid-solids4foam/run.sh
@@ -1,29 +1,14 @@
 #!/bin/bash
 
-# Source tutorial run functions
-. $WM_PROJECT_DIR/bin/tools/RunFunctions
-
-# Currently only checked with OpenFOAM-v2012
+# Currently, the case has only been checked with OpenFOAM-v2012
 if [[ "${WM_PROJECT}" != "OpenFOAM" || "${WM_PROJECT_VERSION}" != "v2012" ]]
 then
     echo; echo "This case currently only runs in OpenFOAM-v2012"; echo
     exit 0
 fi
-# Source solids4Foam scripts
-source solids4FoamScripts.sh
 
-# Create mesh
-runApplication blockMesh
+blockMesh
+touch fluid-openfoam.foam
 
-# Run solver
-if [ "${1:-}" = "-parallel" ]; then
-    procs=$(getNumberOfProcessors)
-    runApplication decomposePar -force
-    mpirun -np "${procs}" solids4Foam -parallel
-    runApplication reconstructPar
-else
-    runApplication solids4Foam
-fi
-
-# Remove empty time directories created when using preCICE
-solids4Foam::removeEmptyDirs
+../../tools/run-openfoam.sh "$@"
+. ../../tools/openfoam-remove-empty-dirs.sh && openfoam_remove_empty_dirs
