c6f2b0bab2034e4d1015e41d2a6170118933a568
Philip Cardiff <philip.cardiff@ucd.ie>
2022-08-04 13:46:31 +0100

    perpendicular-flap/solid-solids4foam: run.sh remove check related to OpenFOAM version

--- perpendicular-flap/solid-solids4foam/run.sh
+++ perpendicular-flap/solid-solids4foam/run.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# Currently, the case has only been checked with OpenFOAM-v2012
-if [[ "${WM_PROJECT}" != "OpenFOAM" || "${WM_PROJECT_VERSION}" != "v2012" ]]
-then
-    echo; echo "This case currently only runs in OpenFOAM-v2012"; echo
-    exit 0
-fi
-
 blockMesh
 touch solid-openfoam.foam
 
