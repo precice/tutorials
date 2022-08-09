b5733f9da8c7effbff7a4016d8c1d45b906ca515
Boris Martin <boris.martin.be@gmail.com>
2022-08-09 17:58:21 +0200

    refactored run.sh

--- perpendicular-flap/solid-calculix/run.sh
+++ perpendicular-flap/solid-calculix/run.sh
@@ -3,14 +3,17 @@ set -e -u
 
 usage() { echo "Usage: run.sh [-modal]" 1>&2; exit 1; }
 
-if [ $# -ge 2 ] || [ $# -eq 1 -a $1 != "-modal" ]; then
+# There must be either 0 arguments or 1, which is modal.
+# Send an error otherwise
+if [ $# -ge 2 ] || { [ $# -eq 1 ] && [ "$1" != "-modal" ]; }; then
     usage
 fi
 
-if [ ${1-nonmodal} = "-modal" ]; then
+# No arg => regular simulation. Otherwise, it's modal
+if [ $# -eq 0 ]; then
+    ccx_preCICE -i flap -precice-participant Solid
+else
     ccx_preCICE -i frequency
     mv frequency.eig flap_modal.eig
     ccx_preCICE -i flap_modal -precice-participant Solid
-else
-    ccx_preCICE -i flap -precice-participant Solid
 fi
