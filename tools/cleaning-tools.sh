a8c1b5d1c497c10ff9f121b691caf036bf53c9cd
Boris Martin <boris.martin.be@gmail.com>
2022-08-09 18:18:55 +0200

    schellcheck

--- tools/cleaning-tools.sh
+++ tools/cleaning-tools.sh
@@ -44,7 +44,7 @@ clean_calculix() {
         echo "--- Cleaning up CalculiX case in $(pwd)"
         rm -fv ./*.cvg ./*.dat ./*.frd ./*.sta ./*.12d spooles.out dummy
         rm -fv WarnNodeMissMultiStage.nam
-        rm -fv *.eig
+        rm -fv ./*.eig
         clean_precice_logs .
     )
 }
