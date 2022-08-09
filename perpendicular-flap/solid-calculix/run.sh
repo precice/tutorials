7733a2353fd2cc9ac21fb1998fdd6481ca700a45
Boris Martin <boris.martin.be@gmail.com>
2022-08-09 17:43:36 +0200

    removed dead code

--- perpendicular-flap/solid-calculix/run.sh
+++ perpendicular-flap/solid-calculix/run.sh
@@ -6,8 +6,6 @@ usage() { echo "Usage: run.sh [-modal]" 1>&2; exit 1; }
 if [ $# -ge 2 ] || [ $# -eq 1 -a $1 != "-modal" ]; then
     usage
 fi
-#ccx_preCICE -i flap -precice-participant Solid
-
 
 if [ ${1-nonmodal} = "-modal" ]; then
     ccx_preCICE -i frequency
