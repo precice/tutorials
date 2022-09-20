fd479c506279758c5ff22072c61af2ca09cfc92a
Boris Martin <boris.martin.be@gmail.com>
2022-09-20 17:28:21 +0200

    update postprocessing script

--- channel-transport-reaction/plot.py
+++ channel-transport-reaction/plot.py
@@ -12,7 +12,27 @@ headers = ['Time', 'A', 'B', 'C']
 
 for key, path in files.items():
     datasets[key] = pd.read_csv(files[key], names=headers, delimiter = ' ')
-    plt.plot(datasets[key]['Time'], datasets[key]['C'], label = key)
+    plt.plot(datasets[key]['Time'], datasets[key]['A'], label = key)
+    plt.title("Total amount of A")
+    plt.xlabel("Time")
+    plt.ylabel("Total amount")
+    plt.legend()
+    plt.show()
 
-plt.legend()
-plt.show()
+for key, path in files.items():
+    datasets[key] = pd.read_csv(files[key], names=headers, delimiter = ' ')
+    plt.plot(datasets[key]['Time'], datasets[key]['B'], label = key)
+    plt.title("Total amount of B")
+    plt.xlabel("Time")
+    plt.ylabel("Total amount")
+    plt.legend()
+    plt.show()
+
+for key, path in files.items():
+    datasets[key] = pd.read_csv(files[key], names=headers, delimiter = ' ')
+    plt.plot(datasets[key]['Time'], datasets[key]['C'], label = key)
+    plt.title("Total amount of C")
+    plt.xlabel("Time")
+    plt.ylabel("Total amount")
+    plt.legend()
+    plt.show()
