70c64ead9adb8c3258004d084f79c71710df5c25
David Schneider <david.schneider@ipvs.uni-stuttgart.de>
2022-09-22 12:15:26 +0200

    Tweak aste tutorial documentation (#293)
    * Fix cleaning tool
    * Port docs to new names
    * Overhaul script comments
    * Extend documentation
    
    Co-authored-by: Gerasimos Chourdakis <chourdak@in.tum.de>
    Co-authored-by: Benjamin Uekermann <benjamin.uekermann@ipvs.uni-stuttgart.de>

--- aste-turbine/run.sh
+++ aste-turbine/run.sh
@@ -6,29 +6,30 @@ set -e -x
 # Download the meshes
 test -f meshes.tar.gz  || wget https://gitlab.lrz.de/precice/precice2-ref-paper-setup/-/raw/main/meshes/meshes.tar.gz
 
-# Extract the meshes
-test -f 0.009.vtk -a 0.01.vtk ||tar -xf meshes.tar.gz
+mkdir -p meshes
 
-# Calculate on fine mesh
-vtk_calculator.py -m 0.009.vtk -f "eggholder3d" -d "EggHolder"
+# Extract the meshes
+test -f meshes/0.006.vtk -a meshes/0.01.vtk || tar -xvf meshes.tar.gz --directory meshes
+
+# Generate input data for the mapping problem using the predefined Franke's function function
+precice-aste-evaluate -m meshes/0.01.vtk -f "franke3d" -d "Franke" -o input_mesh.vtu
 
 # Decompose both meshes to two procesors
-# Choose resolution 0.009 mesh as fine mesh
-partition_mesh.py -m 0.009.vtk -n 2 -o fine_mesh --dir fine_mesh --algorithm topology
-# Choose resolution 0.01 mesh as coarse mesh
-partition_mesh.py -m 0.01.vtk -n 2 -o coarse_mesh --dir coarse_mesh --algorithm topology
+# Choose resolution 0.01 mesh as coarse mesh and partition the mesh using a uniform algorithm
+precice-aste-partition -m input_mesh.vtu -n 2 -o coarse_mesh --dir coarse_mesh --algorithm uniform
+# Choose resolution 0.006 mesh as coarse mesh and partition the mesh using a meshfree algorithm
+precice-aste-partition -m meshes/0.006.vtk -n 2 -o fine_mesh --dir fine_mesh --algorithm meshfree
 
-# The result directory of preciceMap needs to exist beforehand
+# Create results directory of precice-aste-run
 mkdir -p mapped
 
-# Map from the finer mesh to coarser mesh
-mpirun -n 2 preciceMap -v -p A --mesh fine_mesh/fine_mesh --data "EggHolder" &
-mpirun -n 2 preciceMap -v -p B --mesh coarse_mesh/coarse_mesh --output mapped/mapped --data "InterpolatedData"
+# Map from coarse mesh to fine mesh, start two ASTE instances, one for each participant
+mpirun -n 2 precice-aste-run -p A --mesh coarse_mesh/coarse_mesh --data "Franke" &
+mpirun -n 2 precice-aste-run -p B --mesh fine_mesh/fine_mesh --output mapped/mapped --data "InterpolatedData"
 
-# Join the output files together to result.vtk,
-# Recovery cannot be used since GlobalID's are not exist in mapped mesh
-join_mesh.py -m mapped/mapped -o result.vtk --recovery coarse_mesh/coarse_mesh_recovery.json
+# Join the output files together to result.vtu
+precice-aste-join -m mapped/mapped -o result.vtu --recovery fine_mesh/fine_mesh_recovery.json
 
 # Measure the difference between the original function and the mapped values
-# Save into data array called difference
-vtk_calculator.py -m result.vtk -f "eggholder3d" -d difference --diffdata "InterpolatedData" --diff --stats
+# Save into data array called Error
+precice-aste-evaluate -m result.vtu -f "franke3d" -d "Error" --diffdata "InterpolatedData" --diff --stats
