deedcf2be4bcfa03d97c82a810e87652cb2a06ee
Gerasimos Chourdakis <chourdak@in.tum.de>
2022-11-15 16:20:00 +0100

    Rename OpenFOAM result files in run.sh

--- heat-exchanger-simplified/fluid-bottom-openfoam/run.sh
+++ heat-exchanger-simplified/fluid-bottom-openfoam/run.sh
@@ -2,7 +2,7 @@
 set -e -u
 
 blockMesh
-touch fluid2-openfoam.foam
+touch fluid-bottom-openfoam.foam
 
 ../../tools/run-openfoam.sh "$@"
 . ../../tools/openfoam-remove-empty-dirs.sh && openfoam_remove_empty_dirs
--- heat-exchanger-simplified/fluid-top-openfoam/run.sh
+++ heat-exchanger-simplified/fluid-top-openfoam/run.sh
@@ -2,7 +2,7 @@
 set -e -u
 
 blockMesh
-touch fluid1-openfoam.foam
+touch fluid-top-openfoam.foam
 
 ../../tools/run-openfoam.sh "$@"
 . ../../tools/openfoam-remove-empty-dirs.sh && openfoam_remove_empty_dirs
