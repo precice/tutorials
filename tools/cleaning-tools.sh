fa393ea5da09746a131013ff95dac9607e8f5018
j-signorelli <jms26@illinois.edu>
2023-01-12 12:57:39 -0600

    Updated cleaning to remove history_*.csv files

--- tools/cleaning-tools.sh
+++ tools/cleaning-tools.sh
@@ -113,7 +113,7 @@ clean_su2() {
         set -e -u
         cd "$1"
         echo "--- Cleaning up SU2 case in $(pwd)"
-        rm -fv ./restart_flow_*.dat forces_breakdown.dat ./surface_flow_*.csv ./flow_*.vtk ./history_*.vtk
+        rm -fv ./restart_flow_*.dat forces_breakdown.dat ./surface_flow_*.csv ./flow_*.vtk ./history_*.vtk ./history_*.csv
         clean_precice_logs .
     )
 }
