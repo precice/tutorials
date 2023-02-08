ee40cd1b8ced521065d141d425c07384080cb298
Markus Muehlhaeusser <markus.muehlhaeusser@tum.de>
2023-02-08 20:03:04 +0100

    [partitioned-pipe] Add skewed mesh option for non-orthogonality demonstration

--- partitioned-pipe/fluid1-openfoam-pimplefoam/run.sh
+++ partitioned-pipe/fluid1-openfoam-pimplefoam/run.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 set -e -u
 
-blockMesh
+if [ $# -eq 0 ]; then
+    blockMesh 
+elif [ "$1" = "skewed" ]; then
+    blockMesh -dict system/blockMeshDictSkewed
+else
+    echo Error: Use \"./run.sh skewed\" to run the case with a skewed mesh
+    exit 1
+fi
 touch fluid1-openfoam-pimplefoam.foam
 
 ../../tools/run-openfoam.sh "$@"
 . ../../tools/openfoam-remove-empty-dirs.sh && openfoam_remove_empty_dirs
+
+postProcess -func "flowRatePatch(name=inlet)" -latestTime -noZero
