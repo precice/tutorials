36695bc699c4e8d7cbc78a2ffedbfd03c323fdb3
Valentin Seitz <valentin.seitz@tum.de>
2023-07-20 14:58:16 +0200

    - adopted paths to not have recursive copying. - use openfoam alias - removed the reference output for now

--- tools/tests/paths.py
+++ tools/tests/paths.py
@@ -1,5 +1,5 @@
 from pathlib import Path
 PRECICE_TUTORIAL_DIR= Path(__file__).parent.parent.parent
-PRECICE_TESTS_RUN_DIR = Path(__file__).parent.parent / "runs"
+PRECICE_TESTS_RUN_DIR = Path(__file__).parent.parent.parent / "runs"
 PRECICE_TOOLS_DIR=Path(__file__).parent.parent
 PRECICE_TESTS_DIR=Path(__file__).parent
