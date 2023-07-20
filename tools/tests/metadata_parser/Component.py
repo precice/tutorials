ff9ca014330618a231e12afe44831743c98f021e
Valentin Seitz <valentin.seitz@tum.de>
2023-07-20 13:52:41 +0200

    renamed to match purpose a bit better

--- tools/tests/metadata_parser/Component.py
+++ tools/tests/metadata_parser/Component.py
@@ -3,8 +3,8 @@ from typing import Optional,List
 import yaml
 
 @dataclass
-class Parameter:
-    """Represents a parameter needed for the instanciation of a component"""
+class BuildArgument:
+    """Represents a BuildArgument needed to run the docker container"""
 
     description: str
     """The description of the parameter."""
@@ -21,7 +21,7 @@ class Parameter:
     @property
     def required(self) -> bool:
         """
-        Check if the parameter need to be supplied via CommandLineArgs
+        Check if the BuildArgument need to be supplied via CommandLineArgs
 
         Returns:
             bool: True if the parameter is required, False otherwise.
@@ -29,7 +29,7 @@ class Parameter:
         return False if self.default else True
     
     def __eq__(self, other) -> bool:
-        if isinstance(other, Parameter):
+        if isinstance(other, BuildArgument):
             return self.key == other.key
         return False
 
@@ -41,11 +41,11 @@ class Parameter:
 
 
 
-class Parameters:
+class BuildArguments:
     """Represents a collection of parameters."""
 
-    def __init__(self, parameters: List[Parameter]):
-        self.parameters  = parameters
+    def __init__(self, arguments: List[BuildArgument]):
+        self.arguments  = arguments
 
     @classmethod
     def from_components_yaml(cls, data):
@@ -55,34 +55,33 @@ class Parameters:
         Args:
             data: The components YAML data.
         """
-        parameters = []
+        arguments = []
         for param_name, params in data['build-arguments'].items():
             ## TODO maybe **params
             description = params.get('description', f"No description provided for {param_name}")
             key = param_name
-            is_git_ref = params.get('git-ref', True)
             default = params.get('default', None)
             value_options = params.get('value_options', None)
 
-            parameters.append(Parameter(description, key, value_options, default))
+            arguments.append(BuildArgument(description, key, value_options, default))
 
-        return cls(parameters)
+        return cls(arguments)
 
     def __iter__(self):
-        return iter(self.parameters)
+        return iter(self.arguments)
 
     def __getitem__(self, index):
-        return self.parameters[index]
+        return self.arguments[index]
 
     def __setitem__(self, index, value):
-        self.parameters[index] = value
+        self.arguments[index] = value
 
     def __len__(self):
-        return len(self.parameters)
+        return len(self.arguments)
 
 
     def __repr__(self) -> str:
-        return f"{self.parameters}"
+        return f"{self.arguments}"
 
 
 
@@ -97,7 +96,7 @@ class Component:
     name: str
     template: str
     repository: str
-    parameters: Parameters
+    parameters: BuildArguments
 
     def __eq__(self, other):
         if isinstance(other, Component):
@@ -131,7 +130,7 @@ class Components(list):
         with open(path, 'r') as f:
             data = yaml.safe_load(f)
             for component_name in data:
-                parameters = Parameters.from_components_yaml(data[component_name])
+                parameters = BuildArguments.from_components_yaml(data[component_name])
                 repository = data[component_name]["repository"]
                 template = data[component_name]["template"]
                 components.append(Component(component_name, template,repository, parameters))
