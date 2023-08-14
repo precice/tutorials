006270b8670874ab2f2c4fe28dd993c3a7aa09c5
Gerasimos Chourdakis <chourdak@in.tum.de>
2023-08-14 10:00:27 +0200

    Remove preCICE log relics from cleaning-tools

--- tools/cleaning-tools.sh
+++ tools/cleaning-tools.sh
@@ -28,9 +28,6 @@ clean_precice_logs() {
         echo "---- Cleaning up preCICE logs in $(pwd)"
         rm -fv ./precice-*-iterations.log \
             ./precice-*-convergence.log \
-            ./precice-*-events.json \
-            ./precice-*-events-summary.log \
-            ./precice-postProcessingInfo.log \
             ./precice-*-watchpoint-*.log \
             ./precice-*-watchintegral-*.log \
             ./core
