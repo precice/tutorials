e8e27205907c456560e3f7f65cf0e89eb7c39396
Valentin Seitz <valentin.seitz@tum.de>
2023-09-21 12:10:22 +0200

    now with fetching refs

--- tools/tests/systemtests/Systemtest.py
+++ tools/tests/systemtests/Systemtest.py
@@ -228,6 +228,22 @@ class Systemtest:
         except Exception as e:
             raise RuntimeError(f"An error occurred while getting the current Git ref: {e}") from e
 
+
+    def _fetch_ref(self, repository: Path, ref: str):
+        try:
+            result = subprocess.run([
+                "git",
+                "-C", os.fspath(repository.resolve()),
+                "fetch", 
+                "origin", 
+                ref
+            ], check=True, timeout=60)
+            if result.returncode != 0:
+                raise RuntimeError(f"git command returned code {result.returncode}")
+
+        except Exception as e:
+            raise RuntimeError(f"An error occurred while fetching origin '{ref}':  {e}")
+
     def _checkout_ref_in_subfolder(self, repository: Path, subfolder: Path, ref: str):
         try:
             result = subprocess.run([
@@ -252,6 +268,7 @@ class Systemtest:
         ref_requested = self.params_to_use.get("TUTORIALS_REF")
         if ref_requested:
             logging.debug(f"Checking out tutorials {ref_requested} before copying")
+            self._fetch_ref(PRECICE_TUTORIAL_DIR,ref_requested)
             self._checkout_ref_in_subfolder(PRECICE_TUTORIAL_DIR, self.tutorial.path, ref_requested)
 
         self.tutorial_folder = slugify(f'{self.tutorial.path.name}_{self.case_combination.cases}_{current_time_string}')
