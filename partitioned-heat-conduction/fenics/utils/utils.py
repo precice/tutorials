fef8f2554e422cdaca268fb73b324932d779ae60
Benjamin Rodenberg <benjamin.rodenberg@cit.tum.de>
2023-11-16 13:28:15 +0100

    Cleanup.

--- partitioned-heat-conduction/fenics/utils/utils.py
+++ partitioned-heat-conduction/fenics/utils/utils.py
@@ -27,29 +27,3 @@ def b_splines(precice, degree, dt):
         b1,b2,b3 = interpol.splrep(nodes, weights_k,s=0, k=degree)
         b_splines[k] = interpol.BSpline(b1, b2, b3, extrapolate=False)
     return b_splines
-
-
-def b_splines_tmp(prec, u_expr, x_, y_, t_, t, degree, dt):
-    '''
-    :param degree: Degree of BSpline
-    :param u_expr: analytic value of u
-    :param dt: current time step size
-    '''
-
-    # grab keys
-    keys = prec.read_data(0).keys()
-    # use equidistant samples
-    # apparently you need 2k+2 samples for degree k
-    nodes = np.linspace(0,dt,2*3+3)  # <-- But these are 2k+3 samples
-
-    b_splines = {}
-
-    for k in keys:
-        weights = []
-        for i in range(2 * degree + 3):
-            # get analytic solutions at the respective spatial and temporal location
-            weights.append(u_expr.subs(x_, k[0]).subs(y_, k[1]).subs(t_,t+nodes[i]))
-        # create b spline at (k[0], k[1])
-        b1,b2,b3 = interpol.splrep(nodes, weights,s=0, k=degree)
-        b_splines[k] = interpol.BSpline(b1, b2, b3, extrapolate=False)
-    return b_splines
