89b04aa43f1cc78601c991312147e625e781ea0d
Markus Muehlhaeusser <markus.muehlhaeusser@tum.de>
2023-11-22 15:37:20 +0100

    Add interfoam pipe testcase

---
+++ interfoam-pipe/fluid2-openfoam/run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -e -u
+
+blockMesh
+cp -r 0.orig 0
+setFields
+touch fluid2.foam
+
+../../../tools/run-openfoam.sh "$@"
+. ../../../tools/openfoam-remove-empty-dirs.sh && openfoam_remove_empty_dirs
