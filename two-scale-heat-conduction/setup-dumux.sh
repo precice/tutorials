58eb156423ed234a3dbaf32a86e5ca5f0f061a6f
Mathis Kelm <mathis.kelm@iws.uni-stuttgart.de>
2023-12-13 10:46:13 +0100

    [two-scale-heat-conduction] remove dumux subdirectory

--- two-scale-heat-conduction/setup-dumux.sh
+++ two-scale-heat-conduction/setup-dumux.sh
@@ -1,10 +1,11 @@
-# This script sets up a DUNE environment in the subdirectory "dumux" to solve the two-scale-heat-conduction problem with DuMuX on one or both scales
+# This script sets up a DUNE environment in the working directory to solve the two-scale-heat-conduction problem with DuMuX on one or both scales
 
 # Get the DuMuX install scripts
 wget https://git.iws.uni-stuttgart.de/dumux-repositories/dumux/-/raw/master/bin/installdumux.py
 wget https://git.iws.uni-stuttgart.de/dumux-repositories/dumux/-/raw/master/bin/installexternal.py
 
 # Get additional required DUNE modules
+mkdir dumux
 cd dumux
 # DuMux-preCICE adapter
 git clone -b develop https://github.com/precice/dumux-adapter.git
@@ -18,3 +19,13 @@ cd ..
 
 # Install and build DuMux
 python3 installdumux.py
+
+# Move installed dependencies out of folder "dumux"
+mv dumux dumux-install
+cd dumux-install
+mv * ../
+cd ..
+rm -r dumux-install
+
+# Run dunecontrol for the participant modules
+./dune-common/bin/dunecontrol --opts=dumux/cmake.opts --only=macro-dumux all
