ea27ca7e230a7d3f324579e6669394cc837738ac
Mathis Kelm <mathis.kelm@iws.uni-stuttgart.de>
2023-12-14 13:48:28 +0100

    [two-scale-heat-conduction] revert setup script to rebuild modules in working directory

--- two-scale-heat-conduction/setup-dumux.sh
+++ two-scale-heat-conduction/setup-dumux.sh
@@ -1,31 +1,22 @@
 # This script sets up a DUNE environment in the working directory to solve the two-scale-heat-conduction problem with DuMuX on one or both scales
 
-# Get the DuMuX install scripts
+# Get the DuMuX install script and install it
 wget https://git.iws.uni-stuttgart.de/dumux-repositories/dumux/-/raw/master/bin/installdumux.py
-wget https://git.iws.uni-stuttgart.de/dumux-repositories/dumux/-/raw/master/bin/installexternal.py
+python3 installdumux.py
+
+# Take out all the module folders from the dumux/ folder and remove the dumux/ folder
+mv dumux dumux-install
+mv dumux-install/* ./
+rm -r dumux-install
 
 # Get additional required DUNE modules
-mkdir dumux
-cd dumux
 # DuMux-preCICE adapter
 git clone -b develop https://github.com/precice/dumux-adapter.git
 # DuMux phasefield implementation
 git clone -b cell_problems https://git.iws.uni-stuttgart.de/dumux-appl/dumux-phasefield.git
 # DUNE SPGrid for periodic boundary conditions
-mkdir dune-common
-python3 ../installexternal.py spgrid
-rm -r dune-common
-cd ..
+python3 dumux/bin/installexternal.py spgrid
 
-# Install and build DuMux
-python3 installdumux.py
-
-# Move installed dependencies out of folder "dumux"
-mv dumux dumux-install
-cd dumux-install
-mv * ../
-cd ..
-rm -r dumux-install
-
-# Run dunecontrol for the participant modules
-./dune-common/bin/dunecontrol --opts=dumux/cmake.opts --only=macro-dumux all
+# Clear CMake caches and re-build environment
+./dune-common/bin/dunecontrol bexec rm -r CMakeFiles CMakeCache.txt
+./dune-common/bin/dunecontrol --opts=./dumux/cmake.opts all
