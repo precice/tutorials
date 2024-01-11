1c911cf00d2c1e23f63cd0ab3fac9c1e6b807515
Benjamin Uekermann <benjamin.uekermann@ipvs.uni-stuttgart.de>
2024-01-11 10:24:43 +0100

    Update perp flap nutils solid to v3

--- perpendicular-flap/solid-nutils/solid.py
+++ perpendicular-flap/solid-nutils/solid.py
@@ -7,7 +7,7 @@ import treelog
 import precice
 
 
-def main(young=4e6, density=3e3, poisson=0.3, nelems=2, timestepsize=0.01, npoints_per_elem=3):
+def main(young=4e6, density=3e3, poisson=0.3, nelems=2, solver_dt=0.01, npoints_per_elem=3):
 
     topo, geom = mesh.rectilinear([numpy.linspace(-0.05, 0.05, nelems + 1), numpy.linspace(0, 1, 10 * nelems + 1)])
     wall = topo.boundary['left,top,right'].sample('uniform', npoints_per_elem)
@@ -29,10 +29,10 @@ def main(young=4e6, density=3e3, poisson=0.3, nelems=2, timestepsize=0.01, npoin
     ns.σ_ij = 'λ ∇_k(u_k) δ_ij + μ (∇_j(u_i) + ∇_i(u_j))'
 
     # make sure we correctly scale point forces to tractions
-    testforce = numpy.random.normal(size=(wall.npoints, 2))
+    test_force = numpy.random.normal(size=(wall.npoints, 2))
     numpy.testing.assert_almost_equal(
-        actual=wall.integrate('t_i dS' @ ns, F=testforce),
-        desired=testforce.sum(0),
+        actual=wall.integrate('t_i dS' @ ns, F=test_force),
+        desired=test_force.sum(0),
         decimal=10,
         err_msg='nutils error: failed to recover net force',
     )
@@ -51,52 +51,51 @@ def main(young=4e6, density=3e3, poisson=0.3, nelems=2, timestepsize=0.01, npoin
     arguments = solver.optimize('u,v', sqr, constrain=cons)
 
     # preCICE setup
-    solverProcessIndex = 0
-    solverProcessSize = 1
-    interface = precice.Interface("Solid", "../precice-config.xml", solverProcessIndex, solverProcessSize)
-    meshID = interface.get_mesh_id("Solid-Mesh")
-    dataIndices = interface.set_mesh_vertices(meshID, wall.eval(ns.X))
-    writedataID = interface.get_data_id("Displacement", meshID)
-    readdataID = interface.get_data_id("Force", meshID)
+    solver_process_index = 0
+    solver_process_size = 1
+    participant = precice.Participant("Solid", "../precice-config.xml", solver_process_index, solver_process_size)
+    mesh_name = "Solid-Mesh"
+    vertex_ids = participant.set_mesh_vertices(mesh_name, wall.eval(ns.X))
+    write_data_name = "Displacement"
+    read_data_name = "Force"
 
     # initialize preCICE
-    precice_dt = interface.initialize()
+    participant.initialize()
 
     timestep = 0
     force = numpy.zeros((wall.npoints, 2))
 
-    while interface.is_coupling_ongoing():
+    while participant.is_coupling_ongoing():
         with treelog.context(f'timestep {timestep}'):
-
-            # read displacements from interface
-            if interface.is_read_data_available():
-                force = interface.read_block_vector_data(readdataID, dataIndices)
-
+        
             # save checkpoint
-            if interface.is_action_required(precice.action_write_iteration_checkpoint()):
+            if participant.requires_writing_checkpoint():
                 checkpoint = timestep, arguments
-                interface.mark_action_fulfilled(precice.action_write_iteration_checkpoint())
+        
+            precice_dt = participant.get_max_time_step_size()
+            dt = min(precice_dt, solver_dt)
+            
+            # read forces from participant at the end of the timestep
+            force = participant.read_data(mesh_name, read_data_name, vertex_ids, dt)
 
             # advance variables
             timestep += 1
-            dt = min(precice_dt, timestepsize)
+            
             arguments = dict(dt=dt, u0=arguments['u'], v0=arguments['v'], F=force)
             arguments = solver.solve_linear('u:testu,v:testv', res, arguments=arguments, constrain=cons)
 
-            # write forces to interface
-            if interface.is_write_data_required(dt):
-                writedata = wall.eval(ns.u, **arguments)
-                interface.write_block_vector_data(writedataID, dataIndices, writedata)
+            # write displacements to participant
+            write_data = wall.eval(ns.u, **arguments)
+            participant.write_data(mesh_name, write_data_name, vertex_ids, write_data)
 
             # do the coupling
-            precice_dt = interface.advance(dt)
+            participant.advance(dt)
 
             # read checkpoint if required
-            if interface.is_action_required(precice.action_read_iteration_checkpoint()):
+            if participant.requires_reading_checkpoint():
                 timestep, arguments = checkpoint
-                interface.mark_action_fulfilled(precice.action_read_iteration_checkpoint())
 
-    interface.finalize()
+    participant.finalize()
 
 
 if __name__ == '__main__':
