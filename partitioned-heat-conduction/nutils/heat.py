a413d7f1a95e9e3914ee2e2b35a40b2af68500bc
Benjamin Rodenberg <benjamin.rodenberg@cit.tum.de>
2024-01-15 12:44:38 +0100

    Remove unnecessary statements. Move time step to a better place.

--- partitioned-heat-conduction/nutils/heat.py
+++ partitioned-heat-conduction/nutils/heat.py
@@ -128,17 +128,17 @@ def main(side='Dirichlet', n=10, degree=1, timestep=.1, alpha=3., beta=1.2):
         if not participant.is_coupling_ongoing():
             break
 
-        # read data from participant
-        readdata = precice_read(dt)
-
         # save checkpoint
         if participant.requires_writing_checkpoint():
             checkpoint = lhs, t, istep
 
         # prepare next timestep
+        precice_dt = participant.get_max_time_step_size()
+        dt = min(timestep, precice_dt)
         lhs0 = lhs
         istep += 1
-        dt = min(timestep, precice_dt)
+        # read data from participant
+        readdata = precice_read(dt)
         t += dt
 
         # update (time-dependent) boundary condition
@@ -168,8 +168,6 @@ def main(side='Dirichlet', n=10, degree=1, timestep=.1, alpha=3., beta=1.2):
 
         # do the coupling
         participant.advance(dt)
-        precice_dt = participant.get_max_time_step_size()
-        dt = min(timestep, precice_dt)
 
         # read checkpoint if required
         if participant.requires_reading_checkpoint():
