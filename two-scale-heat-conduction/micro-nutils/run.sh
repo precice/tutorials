b0d71a8aed2270cfcd096662eec360186fb12574
Benjamin Rodenberg <benjamin.rodenberg@cit.tum.de>
2024-01-17 17:11:08 +0100

    Use requirements.txt and create venv in run.sh.

--- two-scale-heat-conduction/micro-nutils/run.sh
+++ two-scale-heat-conduction/micro-nutils/run.sh
@@ -3,6 +3,10 @@ set -e -u
 
 usage() { echo "Usage: cmd [-s] [-p n]" 1>&2; exit 1; }
 
+python3 -m venv .venv
+. .venv/bin/activate
+pip install -r requirements.txt
+
 # Check if no input argument was provided
 if [ -z "$*" ] ; then
         echo "No input argument provided. Micro Manager is launched in serial"
