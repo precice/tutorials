28ad13acdeea52f899d9fba74ac9382c3f2f1571
Frédéric Simonis <simonisfrederic@gmail.com>
2024-02-19 12:54:06 +0100

    Change to only plot current fluids

--- elastic-tube-1d/plot-all.sh
+++ elastic-tube-1d/plot-all.sh
@@ -3,33 +3,23 @@
 gnuplot -p << EOF
   set grid
   set term pngcairo enhanced size 900,654
-  set output "images/tutorials-elastic-tube-1d-all.png"
+  set output "elastic-tube-1d-plot.png"
 
   set multiplot layout 2, 1
   set title 'Diameter of elastic-tube at x=5'
 
   set ylabel 'diameter[m]'
-  plot "runs/solid-cpp-fluid-rust/middle.log" using 1:4 with lines title "cpp-rust", \
-       "runs/solid-cpp-fluid-python/middle.log" using 1:4 with lines title "cpp-python", \
-       "runs/solid-cpp-fluid-cpp/middle.log" using 1:4 with lines title "cpp-cpp", \
-       "runs/solid-python-fluid-rust/middle.log" using 1:4 with lines title "python-rust", \
-       "runs/solid-python-fluid-python/middle.log" using 1:4 with lines title "python-python", \
-       "runs/solid-python-fluid-cpp/middle.log" using 1:4 with lines title "python-cpp", \
-       "runs/solid-rust-fluid-rust/middle.log" using 1:4 with lines title "rust-rust", \
-       "runs/solid-rust-fluid-python/middle.log" using 1:4 with lines title "rust-python", \
-       "runs/solid-rust-fluid-cpp/middle.log" using 1:4 with lines title "rust-cpp"
+  plot \
+    "fluid-cpp/precice-Fluid-watchpoint-Middle.log" using 1:4 with linespoints title "cpp", \
+    "fluid-python/precice-Fluid-watchpoint-Middle.log" using 1:4 with linespoints lw 2 title "python", \
+    "fluid-rust/precice-Fluid-watchpoint-Middle.log" using 1:4 with linespoints title "rust"
 
   set title 'Pressure of elastic-tube at x=5'
   set ylabel 'pressure'
-  plot "runs/solid-cpp-fluid-rust/middle.log" using 1:5 with lines title "cpp-rust", \
-       "runs/solid-cpp-fluid-python/middle.log" using 1:5 with lines title "cpp-python", \
-       "runs/solid-cpp-fluid-cpp/middle.log" using 1:5 with lines title "cpp-cpp", \
-       "runs/solid-python-fluid-rust/middle.log" using 1:5 with lines title "python-rust", \
-       "runs/solid-python-fluid-python/middle.log" using 1:5 with lines title "python-python", \
-       "runs/solid-python-fluid-cpp/middle.log" using 1:5 with lines title "python-cpp", \
-       "runs/solid-rust-fluid-rust/middle.log" using 1:5 with lines title "rust-rust", \
-       "runs/solid-rust-fluid-python/middle.log" using 1:5 with lines title "rust-python", \
-       "runs/solid-rust-fluid-cpp/middle.log" using 1:5 with lines title "rust-cpp"
+  plot \
+    "fluid-cpp/precice-Fluid-watchpoint-Middle.log" using 1:5 with linespoints title "cpp", \
+    "fluid-python/precice-Fluid-watchpoint-Middle.log" using 1:5 with linespoints title "python", \
+    "fluid-rust/precice-Fluid-watchpoint-Middle.log" using 1:5 with linespoints title "rust"
 
 	set xlabel 'time [s]'
 
