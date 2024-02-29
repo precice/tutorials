7f8c03adca8d5589b0a5115f8062fea7e67bd40a
Benjamin Rodenberg <benjamin.rodenberg@cit.tum.de>
2024-02-29 13:30:10 +0100

    Simplify.

--- perpendicular-flap/fluid-fake/fake.py
+++ perpendicular-flap/fluid-fake/fake.py
@@ -10,42 +10,28 @@ participant_name = "Fluid"
 mesh_name = "Fluid-Mesh"
 write_data_name = 'Force'
 
-num_vertices = 100  # Number of vertices
-
 solver_process_index = 0
 solver_process_size = 1
 
 interface = precice.Participant(participant_name, configuration_file_name, solver_process_index, solver_process_size)
-
 dimensions = interface.get_mesh_dimensions(mesh_name)
-vertices = np.zeros((num_vertices, dimensions))
-write_data = np.zeros((num_vertices, dimensions))
 
-W = .1
-H = 1
-x_left = 0 - W / 2
-y_bottom = 0
-y_top = y_bottom + H
-F_max = 1
-
+# infomation about beam geometry and position
+W = 0.1  # width of the beam
+H = 1.0  # height of the beam
+x_left = 0.0 - 0.5 * W  # x-coordinate of the left side of the beam
+y_bottom = 0.0  # bottom of the beam
+y_top = y_bottom + H  # top of the beam
+F_max = 1.0
+# define vertices where we want to prove a load
+n = 100  # Number of vertices
+vertices = np.zeros((n, dimensions))
 vertices[:, 0] = x_left  # all vertices are at left side of beam
-vertices[:, 1] = np.linspace(y_bottom, y_top, num_vertices)  # have num_vertices equally disrtibuted vertices
-
-write_data[:, 0] = F_max * vertices[:, 1] / H  # linearly increasing load
-write_data[:, 1] = 0
-
+vertices[:, 1] = np.linspace(y_bottom, y_top, n)  # have n equally disrtibuted vertices
 vertex_ids = interface.set_mesh_vertices(mesh_name, vertices)
 
 interface.initialize()
-
-t = 0
 solver_dt = np.inf  # we just want to use dt = precice_dt
-time = []
-u_tip = []
-v_tip = []
-time.append(0.0)
-u_tip.append(0.0)
-v_tip.append(0.0)
 
 while interface.is_coupling_ongoing():
     if interface.requires_writing_checkpoint():
@@ -54,6 +40,7 @@ while interface.is_coupling_ongoing():
     precice_dt = interface.get_max_time_step_size()
     dt = min([solver_dt, precice_dt])
 
+    write_data = np.zeros((n, dimensions))
     write_data[:, 0] = F_max * vertices[:, 1] / H  # linearly increasing load
     write_data[:, 1] = 0
 
