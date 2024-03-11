b3602f0df7f71a0f6fe3ac9e7b0a60ab018f4338
Gerasimos Chourdakis <chourdak@in.tum.de>
2024-03-11 11:44:35 +0100

    Further fixes in solver-opendihu/clean.sh

--- muscle-tendon-complex/solver-opendihu/clean.sh
+++ muscle-tendon-complex/solver-opendihu/clean.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e -u
 
-rm -r .* ./*.log
-rm -r build_release
-rm -r precice-profiling
+rm -rf ./*.log
+rm -rf build_release
+rm -rf precice-profiling
 
 # remove executables from partipant folders
-rm ../muscle-opendihu/muscle-solver
-rm ../tendon-bottom-opendihu/tendon-solver
-rm ../tendon-top-A-opendihu/tendon-solver
-rm ../tendon-top-B-opendihu/tendon-solver
+rm -f ../muscle-opendihu/muscle-solver
+rm -f ../tendon-bottom-opendihu/tendon-solver
+rm -f ../tendon-top-A-opendihu/tendon-solver
+rm -f ../tendon-top-B-opendihu/tendon-solver
