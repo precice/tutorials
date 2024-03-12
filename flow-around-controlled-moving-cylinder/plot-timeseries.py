aaec33c621e258e9f7d9e1912f5608f3cd66365d
Benjamin Uekermann <benjamin.uekermann@ipvs.uni-stuttgart.de>
2024-03-12 15:32:33 +0100

    Fix plot-timeseries

--- flow-around-controlled-moving-cylinder/plot-timeseries.py
+++ flow-around-controlled-moving-cylinder/plot-timeseries.py
@@ -35,28 +35,28 @@ if solver == ".":
 if solver == 'controller-fmi':
     df = pd.read_csv(filename, delimiter=',')
     if args.plotType == PlotType.E_OVER_T.name:
-        plt.plot(df['time'], df['e'])
+        plt.plot(df['time'].to_numpy(), df['e'].to_numpy())
         plt.title(PlotType.E_OVER_T.value)
     elif args.plotType == PlotType.P_OVER_T.name:
-        plt.plot(df['time'], df['P'])
+        plt.plot(df['time'].to_numpy(), df['P'].to_numpy())
         plt.title(PlotType.P_OVER_T.value)
     elif args.plotType == PlotType.I_OVER_T.name:
-        plt.plot(df['time'], df['I'])
+        plt.plot(df['time'].to_numpy(), df['I'].to_numpy())
         plt.title(PlotType.I_OVER_T.value)
     elif args.plotType == PlotType.D_OVER_T.name:
-        plt.plot(df['time'], df['D'])
+        plt.plot(df['time'].to_numpy(), df['D'].to_numpy())
         plt.title(PlotType.D_OVER_T.value)
     elif args.plotType == PlotType.U1_OVER_T.name:
-        plt.plot(df['time'], df['u_1'])
+        plt.plot(df['time'].to_numpy(), df['u_1'].to_numpy())
         plt.title(PlotType.U1_OVER_T.value)
     elif args.plotType == PlotType.U2_OVER_T.name:
-        plt.plot(df['time'], df['u_2'])
+        plt.plot(df['time'].to_numpy(), df['u_2'].to_numpy())
         plt.title(PlotType.U2_OVER_T.value)
     elif args.plotType == PlotType.Y1_OVER_T.name:
-        plt.plot(df['time'], df['y_1'])
+        plt.plot(df['time'].to_numpy(), df['y_1'].to_numpy())
         plt.title(PlotType.Y1_OVER_T.value)
     elif args.plotType == PlotType.Y2_OVER_T.name:
-        plt.plot(df['time'], df['y_2'])
+        plt.plot(df['time'].to_numpy(), df['y_2'].to_numpy())
         plt.title(PlotType.Y2_OVER_T.value)
     else:
         print("Warning: Controller can not plot this value. Please type run python3 plot-timeseries.py -h to see the available commands.")
