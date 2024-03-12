bdf593e28d3f0d588832aef1e507c780726af68d
Benjamin Uekermann <benjamin.uekermann@ipvs.uni-stuttgart.de>
2024-03-12 14:27:41 +0100

    Clean up fluid run script

--- flow-around-controlled-moving-cylinder/fluid-openfoam/run.sh
+++ flow-around-controlled-moving-cylinder/fluid-openfoam/run.sh
@@ -1,29 +1,14 @@
 #!/bin/sh
-cd ${0%/*} || exit 1                        # Run from this directory
-. $WM_PROJECT_DIR/bin/tools/RunFunctions    # Tutorial run functions
+set -e -u
 
-runApplication blockMesh
-runApplication transformPoints -scale '(0.0016  0.0016 1)'
-mv log.transformPoints log.transformPointsscale
-runApplication transformPoints -translate '(0.0  0.0 -0.05)'
-mv log.transformPoints log.transformPointsscale
+blockMesh
+transformPoints -scale '(0.0016  0.0016 1)'
+transformPoints -translate '(0.0  0.0 -0.05)'
 
-restore0Dir
+rm -rf 0
+cp -r 0.orig 0
 
 touch fluid-openfoam.foam
 
-# Single
-runApplication $(getApplication)
-
-# Parallel
-#runApplication decomposePar
-#runParallel $(getApplication)
-
-
-# ../../tools/run-openfoam.sh "$@"
-# . ../../tools/openfoam-remove-empty-dirs.sh && openfoam_remove_empty_dirs
-
-
-
-
-#------------------------------------------------------------------------------
+../../tools/run-openfoam.sh "$@"
+. ../../tools/openfoam-remove-empty-dirs.sh && openfoam_remove_empty_dirs
