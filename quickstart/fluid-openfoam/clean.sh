200a2c81e9383c76b9188566576bece64f04e3ab
Gerasimos Chourdakis <chourdak@in.tum.de>
2024-03-12 09:09:40 +0100

    Add automatic logging in Quickstart

--- quickstart/fluid-openfoam/clean.sh
+++ quickstart/fluid-openfoam/clean.sh
@@ -4,3 +4,4 @@ set -e -u
 . ../../tools/cleaning-tools.sh
 
 clean_openfoam .
+rm -fv fluid-openfoam.log
--- quickstart/fluid-openfoam/run.sh
+++ quickstart/fluid-openfoam/run.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 set -e -u
 
-blockMesh
-touch fluid-openfoam.foam
+CASENAME=fluid-openfoam
 
-../../tools/run-openfoam.sh "$@"
-. ../../tools/openfoam-remove-empty-dirs.sh && openfoam_remove_empty_dirs
+blockMesh | tee ${CASENAME}.log 2>&1
+touch ${CASENAME}.foam
+
+../../tools/run-openfoam.sh "$@"  | tee --append ${CASENAME}.log 2>&1
+. ../../tools/openfoam-remove-empty-dirs.sh && openfoam_remove_empty_dirs | tee --append ${CASENAME}.log 2>&1
