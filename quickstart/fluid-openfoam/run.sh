7d86a029a6d576c489ce9c10e8be6244cc45a156
Gerasimos Chourdakis <chourdak@in.tum.de>
2024-03-12 18:41:39 +0100

    Automatically deduce CASENAME

--- quickstart/fluid-openfoam/run.sh
+++ quickstart/fluid-openfoam/run.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 set -e -u
 
-CASENAME=fluid-openfoam
+CASENAME="$(readlink -f "$0" | xargs dirname | xargs basename)"
 
-blockMesh | tee ${CASENAME}.log 2>&1
-touch ${CASENAME}.foam
+blockMesh | tee "$CASENAME.log" 2>&1
+touch "$CASENAME.foam"
 
-../../tools/run-openfoam.sh "$@"  | tee --append ${CASENAME}.log 2>&1
-. ../../tools/openfoam-remove-empty-dirs.sh && openfoam_remove_empty_dirs | tee --append ${CASENAME}.log 2>&1
+../../tools/run-openfoam.sh "$@"  | tee --append "$CASENAME.log" 2>&1
+. ../../tools/openfoam-remove-empty-dirs.sh && openfoam_remove_empty_dirs | tee --append "$CASENAME.log" 2>&1
