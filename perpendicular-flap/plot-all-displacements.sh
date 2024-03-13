776bf6ec3b4f915889bf2d3ee14e54e1b3408f3b
Benjamin Rodenberg <benjamin.rodenberg@cit.tum.de>
2024-03-13 14:08:14 +0100

    Revert unwanted changes.

--- perpendicular-flap/plot-all-displacements.sh
+++ perpendicular-flap/plot-all-displacements.sh
@@ -7,6 +7,13 @@ gnuplot -p << EOF
 	set ylabel 'x-displacement [m]'
 	set term pngcairo enhanced size 900,654
 	set output "images/tutorials-perpendicular-flap-displacement-all-watchpoints.png"
-	plot "watchpoints/fake-fenics.log" using 1:4 with lines title "fake-FEniCS", \
-	     "watchpoints/fake-calculix.log" using 1:4 with lines title "fake-CalculiX"
+	plot "watchpoints/openfoam-calculix.log" using 1:4 with lines title "OpenFOAM-CalculiX", \
+	     "watchpoints/openfoam-dealii.log" using 1:4 with lines title "OpenFOAM-deal.II", \
+	     "watchpoints/openfoam-fenics.log" using 1:4 with lines title "OpenFOAM-FEniCS", \
+	     "watchpoints/su2-calculix.log" using 1:4 with lines title "SU2-CalculiX", \
+	     "watchpoints/su2-dealii.log" using 1:4 with lines title "SU2-deal.II", \
+	     "watchpoints/su2-fenics.log" using 1:4 with lines title "SU2-FEniCS", \
+	     "watchpoints/nutils-calculix.log" using 1:4 with lines title "Nutils-CalculiX", \
+	     "watchpoints/nutils-dealii.log" using 1:4 with lines title "Nutils-deal.II", \
+	     "watchpoints/nutils-fenics.log" using 1:4 with lines title "Nutils-FEniCS"
 EOF
