0cb426e3c9c481655cd5d3fd06d9d4485b3b54d3
Gerasimos Chourdakis <chourdak@in.tum.de>
2024-03-16 06:56:20 +0100

    Log quickstart/solid-cpp

--- quickstart/solid-cpp/run.sh
+++ quickstart/solid-cpp/run.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 set -e -u
 
+. ../../tools/log.sh
+
 solver=./rigid_body_solver
 if [ -f "${solver}" ]; then
-    ${solver}
+    log ${solver}
 else 
-    echo "Unable to locate the executable ${solver}. Have a look at the README for building instructions."
+    log echo "Unable to locate the executable ${solver}. Have a look at the README for building instructions."
 fi
+
+close_log
