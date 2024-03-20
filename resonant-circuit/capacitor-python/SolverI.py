eef2da1ca91786138e2f143e71a5876aa78c3276
Benjamin Rodenberg <benjamin.rodenberg@cit.tum.de>
2024-03-20 15:32:30 +0100

    Use black-box time stepper, subcycling, and time interpolation.

--- resonant-circuit/capacitor-python/SolverI.py
+++ resonant-circuit/capacitor-python/SolverI.py
@@ -21,59 +21,62 @@ C = 2                      # Capacitance of the capacitor
 L = 1                      # Inductance of the coil
 t0 = 0                     # Initial simulation time
 t_max = 10                 # End simulation time
-Io = 1                     # Initial current
+Io = np.array([1])                     # Initial current
 phi = 0                    # Phase of the signal
 
 w0 = 1/np.sqrt(L*C)           # Resonant frequency
 I0 = Io*np.cos(phi)           # Initial condition for I
-U0 = -w0*L*Io*np.sin(phi)     # Initial condition for U
 
-f_I = lambda t, I, U: U/L;       # Time derivative of I; ODE determining capacitor
+def f_I(t_span, t):
+    if t < t_span[1]:
+        dt = t - t_span[0]
+    else:
+        dt = t_span[1] - t_span[0]
+
+    U = participant.read_data(mesh_name, read_data_name, vertex_ids, dt)
+    return U/L;       # Time derivative of I; ODE determining capacitor
 
 # Initialize simulation
 if participant.requires_initial_data():
-    participant.write_data(mesh_name, write_data_name, vertex_ids, np.array([I0]))
+    participant.write_data(mesh_name, write_data_name, vertex_ids, I0)
 
 participant.initialize()
 
-dt = participant.get_max_time_step_size()
-
 # Start simulation
-t = t0 + dt
+t = t0
 while participant.is_coupling_ongoing():
 
     # Record checkpoint if necessary
     if participant.requires_writing_checkpoint():
         I0_checkpoint = I0
-        U0_checkpoint = U0
+        t_checkpoint = t
 
     # Make Simulation Step
-    sol = solve_ivp(lambda t, y: f_I(t, y, U0), [t0, t], np.array([I0]))
-    I0 = sol.y[-1,-1]
-    print(sol.y)
-    print(I0)
+    dt = participant.get_max_time_step_size()
+    t_span = [t, t+dt]
+    print(f"solving time {t_span}")
+    sol = solve_ivp(lambda t, y: f_I(t_span, t), t_span, I0, dense_output=True,r_tol=1e-6, a_tol=1e-9)
 
     # Exchange data
-    participant.write_data(mesh_name, write_data_name, vertex_ids, np.array([I0]))
-    participant.advance(dt)
+    evals = 10
+    for i in range(evals):
+        I0 = sol.sol(t_span[0] + (i+1)*dt/evals)
+        participant.write_data(mesh_name, write_data_name, vertex_ids, np.array(I0))
+        participant.advance(dt/evals)
 
-    # Recover checkpoint if not converged, else finish time step
+    t = t + dt
+
+    # Recover checkpoint if not converged
     if participant.requires_reading_checkpoint():
         I0 = I0_checkpoint
-        U0 = U0_checkpoint
-    else:
-        dt = participant.get_max_time_step_size()
-        U0 = participant.read_data(mesh_name, read_data_name, vertex_ids, dt)[0]
-        t0 = t
-        t = t0 + dt
+        t = t_checkpoint
 
 # Stop coupling
 participant.finalize()
 
 import math
 
-T_max = t
-assert(math.isclose(T_max, 10))
+assert(math.isclose(t, t_max))
 I_analytical = lambda t: Io*np.cos(t*w0 + phi)
-error = I0 - I_analytical(T_max)
+error = I0 - I_analytical(t_max)
 print(f"error: {error}")
--- resonant-circuit/coil-python/SolverU.py
+++ resonant-circuit/coil-python/SolverU.py
@@ -21,50 +21,55 @@ C = 2                      # Capacitance of the capacitor
 L = 1                      # Inductance of the coil
 t0 = 0                     # Initial simulation time
 t_max = 10                 # End simulation time
-Io = 1                     # Initial current
+Io = np.array([1])         # Initial current
 phi = 0                    # Phase of the signal
 
 w0 = 1/np.sqrt(L*C)           # Resonant frequency
-I0 = Io*np.cos(phi)           # Initial condition for I
 U0 = -w0*L*Io*np.sin(phi)     # Initial condition for U
 
-f_U = lambda t, U, I: -I/C      # Time derivative of U
+def f_U(t_span, t):
+    if t < t_span[1]:
+        dt = t - t_span[0]
+    else:
+        dt = t_span[1] - t_span[0]
+
+    I = participant.read_data(mesh_name, read_data_name, vertex_ids, dt)
+    return -I/C      # Time derivative of U
 
 # Initialize simulation
 if participant.requires_initial_data():
-    participant.write_data(mesh_name, write_data_name, vertex_ids, np.array([U0]))
+    participant.write_data(mesh_name, write_data_name, vertex_ids, U0)
 
 participant.initialize()
 
-dt = participant.get_max_time_step_size()
-
 # Start simulation
-t = t0 + dt
+t = t0
 while participant.is_coupling_ongoing():
 
     # Record checkpoint if necessary
     if participant.requires_writing_checkpoint():
-        I0_checkpoint = I0
         U0_checkpoint = U0
+        t_checkpoint = t
 
     # Make Simulation Step
-    sol = solve_ivp(lambda t, y: f_U(t, y, I0), [t0, t], np.array([U0]))
-    U0 = sol.y[-1,-1]
-    print(U0)
+    dt = participant.get_max_time_step_size()
+    t_span = [t, t+dt]
+    print(f"solving time {t_span}")
+    sol = solve_ivp(lambda t, y: f_U(t_span, t), t_span, U0, dense_output=True,r_tol=1e-6, a_tol=1e-9)
 
     # Exchange data
-    participant.write_data(mesh_name, write_data_name, vertex_ids, np.array([U0]))
-    participant.advance(dt)
+    evals = 10
+    for i in range(evals):
+        U0 = sol.sol(t_span[0] + (i+1)*dt/evals)
+        participant.write_data(mesh_name, write_data_name, vertex_ids, np.array(U0))
+        participant.advance(dt/evals)
 
-    # Recover checkpoint if not converged, else finish time step
+    t = t + dt
+
+    # Recover checkpoint if not converged
     if participant.requires_reading_checkpoint():
-        I0 = I0_checkpoint
         U0 = U0_checkpoint
-    else:
-        dt = participant.get_max_time_step_size()
-        I0 = participant.read_data(mesh_name, read_data_name, vertex_ids, dt)[0]
-        t0 = t
-        t = t0 + dt
+        t = t_checkpoint
 
 # Stop coupling
 participant.finalize()
