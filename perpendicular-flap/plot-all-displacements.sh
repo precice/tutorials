8b0cb9e7ad3f04ff0a06fce83065827429eed08a
Gerasimos Chourdakis <chourdak@in.tum.de>
2024-03-24 12:02:52 +0100

    Add solid-nutils in the combinations

--- perpendicular-flap/plot-all-displacements.sh
+++ perpendicular-flap/plot-all-displacements.sh
@@ -29,16 +29,19 @@ gnuplot -p << EOF
 			 "watchpoints/openfoam-dune.log" using 1:4 with lines title "OpenFOAM-DUNE", \
 			 "watchpoints/openfoam-openfoam.log" using 1:4 with lines title "OpenFOAM-OpenFOAM", \
 			 "watchpoints/openfoam-solids4foam.log" using 1:4 with lines title "OpenFOAM-solids4Foam", \
+			 "watchpoints/openfoam-nutils.log" using 1:4 with lines title "OpenFOAM-Nutils", \
 	     "watchpoints/su2-calculix.log" using 1:4 with lines title "SU2-CalculiX", \
 	     "watchpoints/su2-dealii.log" using 1:4 with lines title "SU2-deal.II", \
 	     "watchpoints/su2-fenics.log" using 1:4 with lines title "SU2-FEniCS", \
 			 "watchpoints/su2-dune.log" using 1:4 with lines title "SU2-DUNE", \
 			 "watchpoints/su2-openfoam.log" using 1:4 with lines title "SU2-OpenFOAM", \
 			 "watchpoints/su2-solids4foam.log" using 1:4 with lines title "SU2-solids4Foam", \
+			 "watchpoints/su2-nutils.log" using 1:4 with lines title "SU2-nutils", \
 	     "watchpoints/nutils-calculix.log" using 1:4 with lines title "Nutils-CalculiX", \
 	     "watchpoints/nutils-dealii.log" using 1:4 with lines title "Nutils-deal.II", \
 	     "watchpoints/nutils-fenics.log" using 1:4 with lines title "Nutils-FEniCS", \
 			 "watchpoints/nutils-dune.log" using 1:4 with lines title "Nutils-DUNE", \
 			 "watchpoints/nutils-openfoam.log" using 1:4 with lines title "Nutils-OpenFOAM", \
-			 "watchpoints/nutils-solids4foam.log" using 1:4 with lines title "Nutils-solids4Foam"
+			 "watchpoints/nutils-solids4foam.log" using 1:4 with lines title "Nutils-solids4Foam", \
+			 "watchpoints/nutils-nutils.log" using 1:4 with lines title "Nutils-Nutils"
 EOF
