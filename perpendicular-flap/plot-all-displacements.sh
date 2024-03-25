33d4c6edfbf05442bea0ccf7a710b78f7692e8d7
Benjamin Rodenberg <benjamin.rodenberg@cit.tum.de>
2024-03-25 13:24:45 +0100

    Replace tabs with spaces.

--- perpendicular-flap/plot-all-displacements.sh
+++ perpendicular-flap/plot-all-displacements.sh
@@ -6,7 +6,7 @@
 #
 # It plots watchpoints of specific combinations of Fluid and Solid participants,
 # stored in `images/`. To use this script:
-# 1. For each combination you want to plot: 
+# 1. For each combination you want to plot:
 #    1. Run the tutorial with that combination
 #    2. Copy the respective watchpoint file to watchpoints/, with names <fluid>-<solid>.log
 #    3. Clean the tutorial
@@ -16,32 +16,32 @@
 #
 # If you are only interested in a subset of combinations, remove the respective lines.
 
-gnuplot -p << EOF                                                               
-	set grid                                                                        
-	set title 'x-displacement of the flap tip'                                        
-	set xlabel 'time [s]'                                                           
-	set ylabel 'x-displacement [m]'
-	set term pngcairo enhanced size 900,654
-	set output "images/tutorials-perpendicular-flap-displacement-all-watchpoints.png"
-	plot "watchpoints/openfoam-calculix.log" using 1:4 with lines title "OpenFOAM-CalculiX", \
-	     "watchpoints/openfoam-dealii.log" using 1:4 with lines title "OpenFOAM-deal.II", \
-	     "watchpoints/openfoam-fenics.log" using 1:4 with lines title "OpenFOAM-FEniCS", \
-			 "watchpoints/openfoam-dune.log" using 1:4 with lines title "OpenFOAM-DUNE", \
-			 "watchpoints/openfoam-openfoam.log" using 1:4 with lines title "OpenFOAM-OpenFOAM", \
-			 "watchpoints/openfoam-solids4foam.log" using 1:4 with lines title "OpenFOAM-solids4Foam", \
-			 "watchpoints/openfoam-nutils.log" using 1:4 with lines title "OpenFOAM-Nutils", \
-	     "watchpoints/su2-calculix.log" using 1:4 with lines title "SU2-CalculiX", \
-	     "watchpoints/su2-dealii.log" using 1:4 with lines title "SU2-deal.II", \
-	     "watchpoints/su2-fenics.log" using 1:4 with lines title "SU2-FEniCS", \
-			 "watchpoints/su2-dune.log" using 1:4 with lines title "SU2-DUNE", \
-			 "watchpoints/su2-openfoam.log" using 1:4 with lines title "SU2-OpenFOAM", \
-			 "watchpoints/su2-solids4foam.log" using 1:4 with lines title "SU2-solids4Foam", \
-			 "watchpoints/su2-nutils.log" using 1:4 with lines title "SU2-nutils", \
-	     "watchpoints/nutils-calculix.log" using 1:4 with lines title "Nutils-CalculiX", \
-	     "watchpoints/nutils-dealii.log" using 1:4 with lines title "Nutils-deal.II", \
-	     "watchpoints/nutils-fenics.log" using 1:4 with lines title "Nutils-FEniCS", \
-			 "watchpoints/nutils-dune.log" using 1:4 with lines title "Nutils-DUNE", \
-			 "watchpoints/nutils-openfoam.log" using 1:4 with lines title "Nutils-OpenFOAM", \
-			 "watchpoints/nutils-solids4foam.log" using 1:4 with lines title "Nutils-solids4Foam", \
-			 "watchpoints/nutils-nutils.log" using 1:4 with lines title "Nutils-Nutils"
+gnuplot -p << EOF
+    set grid
+    set title 'x-displacement of the flap tip'
+    set xlabel 'time [s]'
+    set ylabel 'x-displacement [m]'
+    set term pngcairo enhanced size 900,654
+    set output "images/tutorials-perpendicular-flap-displacement-all-watchpoints.png"
+    plot "watchpoints/openfoam-calculix.log" using 1:4 with lines title "OpenFOAM-CalculiX", \
+         "watchpoints/openfoam-dealii.log" using 1:4 with lines title "OpenFOAM-deal.II", \
+         "watchpoints/openfoam-fenics.log" using 1:4 with lines title "OpenFOAM-FEniCS", \
+         "watchpoints/openfoam-dune.log" using 1:4 with lines title "OpenFOAM-DUNE", \
+         "watchpoints/openfoam-openfoam.log" using 1:4 with lines title "OpenFOAM-OpenFOAM", \
+         "watchpoints/openfoam-solids4foam.log" using 1:4 with lines title "OpenFOAM-solids4Foam", \
+         "watchpoints/openfoam-nutils.log" using 1:4 with lines title "OpenFOAM-Nutils", \
+         "watchpoints/su2-calculix.log" using 1:4 with lines title "SU2-CalculiX", \
+         "watchpoints/su2-dealii.log" using 1:4 with lines title "SU2-deal.II", \
+         "watchpoints/su2-fenics.log" using 1:4 with lines title "SU2-FEniCS", \
+         "watchpoints/su2-dune.log" using 1:4 with lines title "SU2-DUNE", \
+         "watchpoints/su2-openfoam.log" using 1:4 with lines title "SU2-OpenFOAM", \
+         "watchpoints/su2-solids4foam.log" using 1:4 with lines title "SU2-solids4Foam", \
+         "watchpoints/su2-nutils.log" using 1:4 with lines title "SU2-nutils", \
+         "watchpoints/nutils-calculix.log" using 1:4 with lines title "Nutils-CalculiX", \
+         "watchpoints/nutils-dealii.log" using 1:4 with lines title "Nutils-deal.II", \
+         "watchpoints/nutils-fenics.log" using 1:4 with lines title "Nutils-FEniCS", \
+         "watchpoints/nutils-dune.log" using 1:4 with lines title "Nutils-DUNE", \
+         "watchpoints/nutils-openfoam.log" using 1:4 with lines title "Nutils-OpenFOAM", \
+         "watchpoints/nutils-solids4foam.log" using 1:4 with lines title "Nutils-solids4Foam", \
+         "watchpoints/nutils-nutils.log" using 1:4 with lines title "Nutils-Nutils"
 EOF
