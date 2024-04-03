8791448c3b2ffbb5102d1339b3421983eab24e2d
Gerasimos Chourdakis <chourdak@in.tum.de>
2024-04-03 10:43:18 +0200

    Add more watchpoint plots

--- perpendicular-flap/plot-all-displacements.sh
+++ perpendicular-flap/plot-all-displacements.sh
@@ -23,25 +23,115 @@ gnuplot -p << EOF
     set ylabel 'x-displacement [m]'
     set term pngcairo enhanced size 900,654
     set output "images/tutorials-perpendicular-flap-displacement-all-watchpoints.png"
-    plot "watchpoints/openfoam-calculix.log" using 1:4 with lines title "OpenFOAM-CalculiX", \
-         "watchpoints/openfoam-dealii.log" using 1:4 with lines title "OpenFOAM-deal.II", \
-         "watchpoints/openfoam-fenics.log" using 1:4 with lines title "OpenFOAM-FEniCS", \
-         "watchpoints/openfoam-dune.log" using 1:4 with lines title "OpenFOAM-DUNE", \
-         "watchpoints/openfoam-openfoam.log" using 1:4 with lines title "OpenFOAM-OpenFOAM", \
-         "watchpoints/openfoam-solids4foam.log" using 1:4 with lines title "OpenFOAM-solids4Foam", \
-         "watchpoints/openfoam-nutils.log" using 1:4 with lines title "OpenFOAM-Nutils", \
-         "watchpoints/su2-calculix.log" using 1:4 with lines title "SU2-CalculiX", \
-         "watchpoints/su2-dealii.log" using 1:4 with lines title "SU2-deal.II", \
-         "watchpoints/su2-fenics.log" using 1:4 with lines title "SU2-FEniCS", \
-         "watchpoints/su2-dune.log" using 1:4 with lines title "SU2-DUNE", \
-         "watchpoints/su2-openfoam.log" using 1:4 with lines title "SU2-OpenFOAM", \
-         "watchpoints/su2-solids4foam.log" using 1:4 with lines title "SU2-solids4Foam", \
-         "watchpoints/su2-nutils.log" using 1:4 with lines title "SU2-nutils", \
-         "watchpoints/nutils-calculix.log" using 1:4 with lines title "Nutils-CalculiX", \
-         "watchpoints/nutils-dealii.log" using 1:4 with lines title "Nutils-deal.II", \
-         "watchpoints/nutils-fenics.log" using 1:4 with lines title "Nutils-FEniCS", \
-         "watchpoints/nutils-dune.log" using 1:4 with lines title "Nutils-DUNE", \
-         "watchpoints/nutils-openfoam.log" using 1:4 with lines title "Nutils-OpenFOAM", \
-         "watchpoints/nutils-solids4foam.log" using 1:4 with lines title "Nutils-solids4Foam", \
-         "watchpoints/nutils-nutils.log" using 1:4 with lines title "Nutils-Nutils"
+    plot "watchpoints/openfoam-calculix-v3.1.0.log" using 1:4 with lines title "OpenFOAM-CalculiX", \
+         "watchpoints/openfoam-dealii-v3.1.0.log" using 1:4 with lines title "OpenFOAM-deal.II", \
+         "watchpoints/openfoam-dune-v3.1.0.log" using 1:4 with lines title "OpenFOAM-DUNE", \
+         "watchpoints/openfoam-fenics-v3.1.0.log" using 1:4 with lines title "OpenFOAM-FEniCS", \
+         "watchpoints/openfoam-nutils-v3.1.0.log" using 1:4 with lines title "OpenFOAM-Nutils", \
+         "watchpoints/openfoam-openfoam-v3.1.0.log" using 1:4 with lines title "OpenFOAM-OpenFOAM", \
+         "watchpoints/openfoam-solids4foam-v3.1.0.log" using 1:4 with lines title "OpenFOAM-solids4Foam", \
+         "watchpoints/su2-calculix-v3.1.0.log" using 1:4 with lines title "SU2-CalculiX", \
+         "watchpoints/su2-dealii-v3.1.0.log" using 1:4 with lines title "SU2-deal.II", \
+         "watchpoints/su2-dune-v3.1.0.log" using 1:4 with lines title "SU2-DUNE", \
+         "watchpoints/su2-fenics-v3.1.0.log" using 1:4 with lines title "SU2-FEniCS", \
+         "watchpoints/su2-nutils-v3.1.0.log" using 1:4 with lines title "SU2-nutils", \
+         "watchpoints/su2-openfoam-v3.1.0.log" using 1:4 with lines title "SU2-OpenFOAM", \
+         "watchpoints/su2-solids4foam-v3.1.0.log" using 1:4 with lines title "SU2-solids4Foam", \
+         "watchpoints/nutils-calculix-v3.0.0.log" using 1:4 with lines title "Nutils-CalculiX", \
+         "watchpoints/nutils-dealii-v3.1.0.log" using 1:4 with lines title "Nutils-deal.II", \
+         "watchpoints/nutils-nutils-v3.0.0.log" using 1:4 with lines title "Nutils-Nutils"
+EOF
+
+
+gnuplot -p << EOF
+    set grid
+    set title 'x-displacement of the flap tip (selected combinations)'
+    set xlabel 'time [s]'
+    set ylabel 'x-displacement [m]'
+    set term pngcairo enhanced size 900,654
+    set output "images/tutorials-perpendicular-flap-displacement-selected-watchpoints.png"
+    plot "watchpoints/openfoam-calculix-v3.1.0.log" using 1:4 with lines title "OpenFOAM-CalculiX", \
+         "watchpoints/openfoam-dealii-v3.1.0.log" using 1:4 with lines title "OpenFOAM-deal.II", \
+         "watchpoints/openfoam-fenics-v3.1.0.log" using 1:4 with lines title "OpenFOAM-FEniCS", \
+         "watchpoints/su2-calculix-v3.1.0.log" using 1:4 with lines title "SU2-CalculiX", \
+         "watchpoints/su2-dealii-v3.1.0.log" using 1:4 with lines title "SU2-deal.II", \
+         "watchpoints/su2-fenics-v3.1.0.log" using 1:4 with lines title "SU2-FEniCS", \
+         "watchpoints/nutils-calculix-v3.0.0.log" using 1:4 with lines title "Nutils-CalculiX", \
+         "watchpoints/nutils-dealii-v3.1.0.log" using 1:4 with lines title "Nutils-deal.II"
+EOF
+
+gnuplot -p << EOF
+    set grid
+    set title 'x-displacement of the flap tip (comparison of flow solvers)'
+    set xlabel 'time [s]'
+    set ylabel 'x-displacement [m]'
+    set term pngcairo enhanced size 900,654
+    set output "images/tutorials-perpendicular-flap-displacement-flow-comparison-watchpoints.png"
+    plot "watchpoints/openfoam-calculix-v3.1.0.log" using 1:4 with lines title "OpenFOAM-CalculiX", \
+         "watchpoints/su2-calculix-v3.1.0.log" using 1:4 with lines title "SU2-CalculiX", \
+         "watchpoints/nutils-calculix-v3.0.0.log" using 1:4 with lines title "Nutils-CalculiX", \
+         "watchpoints/fake-calculix-v3.1.0.log" using 1:4 with lines title "fake-CalculiX"
+EOF
+
+gnuplot -p << EOF
+    set grid
+    set title 'x-displacement of the flap tip (incompressible flow with OpenFOAM)'
+    set xlabel 'time [s]'
+    set ylabel 'x-displacement [m]'
+    set term pngcairo enhanced size 900,654
+    set output "images/tutorials-perpendicular-flap-displacement-openfoam-watchpoints.png"
+    plot "watchpoints/openfoam-calculix-v3.1.0.log" using 1:4 with lines title "OpenFOAM-CalculiX", \
+         "watchpoints/openfoam-dealii-v3.1.0.log" using 1:4 with lines title "OpenFOAM-deal.II", \
+         "watchpoints/openfoam-dune-v3.1.0.log" using 1:4 with lines title "OpenFOAM-DUNE", \
+         "watchpoints/openfoam-fenics-v3.1.0.log" using 1:4 with lines title "OpenFOAM-FEniCS", \
+         "watchpoints/openfoam-nutils-v3.1.0.log" using 1:4 with lines title "OpenFOAM-Nutils", \
+         "watchpoints/openfoam-solids4foam-v3.1.0.log" using 1:4 with lines title "OpenFOAM-solids4Foam"
+EOF
+
+gnuplot -p << EOF
+    set grid
+    set title 'x-displacement of the flap tip (compressible flow with SU2)'
+    set xlabel 'time [s]'
+    set ylabel 'x-displacement [m]'
+    set term pngcairo enhanced size 900,654
+    set output "images/tutorials-perpendicular-flap-displacement-su2-watchpoints.png"
+    plot "watchpoints/su2-calculix-v3.1.0.log" using 1:4 with lines title "SU2-CalculiX", \
+         "watchpoints/su2-dealii-v3.1.0.log" using 1:4 with lines title "SU2-deal.II", \
+         "watchpoints/su2-dune-v3.1.0.log" using 1:4 with lines title "SU2-DUNE", \
+         "watchpoints/su2-fenics-v3.1.0.log" using 1:4 with lines title "SU2-FEniCS", \
+         "watchpoints/su2-nutils-v3.1.0.log" using 1:4 with lines title "SU2-nutils", \
+         "watchpoints/su2-solids4foam-v3.1.0.log" using 1:4 with lines title "SU2-solids4Foam"
+EOF
+
+gnuplot -p << EOF
+    set grid
+    set title 'x-displacement of the flap tip (incompressible flow with Nutils)'
+    set xlabel 'time [s]'
+    set ylabel 'x-displacement [m]'
+    set term pngcairo enhanced size 900,654
+    set output "images/tutorials-perpendicular-flap-displacement-nutils-watchpoints.png"
+    plot "watchpoints/nutils-calculix-v3.0.0.log" using 1:4 with lines title "Nutils-CalculiX", \
+         "watchpoints/nutils-dealii-v3.1.0.log" using 1:4 with lines title "Nutils-deal.II", \
+         "watchpoints/nutils-nutils-v3.0.0.log" using 1:4 with lines title "Nutils-Nutils"
+EOF
+
+# Not currently included in the Nutils plots due to long simulation time:
+    #      "watchpoints/nutils-fenics.log" using 1:4 with lines title "Nutils-FEniCS", \
+    #      "watchpoints/nutils-dune.log" using 1:4 with lines title "Nutils-DUNE", \
+    #      "watchpoints/nutils-openfoam.log" using 1:4 with lines title "Nutils-OpenFOAM", \
+    #      "watchpoints/nutils-solids4foam.log" using 1:4 with lines title "Nutils-solids4Foam", \
+
+gnuplot -p << EOF
+    set grid
+    set title 'x-displacement of the flap tip (dummy force data with fluid-fake)'
+    set xlabel 'time [s]'
+    set ylabel 'x-displacement [m]'
+    set term pngcairo enhanced size 900,654
+    set output "images/tutorials-perpendicular-flap-displacement-fake-watchpoints.png"
+    plot "watchpoints/fake-calculix-v3.1.0.log" using 1:4 with lines title "fake-CalculiX", \
+         "watchpoints/fake-dealii-v3.1.0.log" using 1:4 with lines title "fake-deal.II", \
+         "watchpoints/fake-dune-v3.1.0.log" using 1:4 with lines title "fake-DUNE", \
+         "watchpoints/fake-fenics-v3.1.0.log" using 1:4 with lines title "fake-FEniCS", \
+         "watchpoints/fake-nutils-v3.1.0.log" using 1:4 with lines title "fake-Nutils", \
+         "watchpoints/fake-solids4foam-v3.1.0.log" using 1:4 with lines title "fake-solids4Foam"
 EOF
