3a659f9e6016a89192db018e4a13282bdf9434a5
Gerasimos Chourdakis <chourdak@in.tum.de>
2024-04-05 22:35:17 +0200

    CalculiX: Cleanup and ignore .rout files

--- tools/cleaning-tools.sh
+++ tools/cleaning-tools.sh
@@ -59,7 +59,7 @@ clean_calculix() {
         set -e -u
         cd "$1"
         echo "- Cleaning up CalculiX case in $(pwd)"
-        rm -fv ./*.cvg ./*.dat ./*.frd ./*.sta ./*.12d spooles.out dummy
+        rm -fv ./*.cvg ./*.dat ./*.frd ./*.sta ./*.12d ./*.rout spooles.out dummy
         rm -fv WarnNodeMissMultiStage.nam
         rm -fv ./*.eig
         rm -fv ./*.vtk
