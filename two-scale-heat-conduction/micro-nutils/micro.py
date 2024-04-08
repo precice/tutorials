0b546d3b46d3b4de9d02ceda28549ee1dc380b03
Ishaan Desai <ishaandesai@gmail.com>
2024-04-08 12:36:43 +0200

    Formatting

--- two-scale-heat-conduction/micro-nutils/micro.py
+++ two-scale-heat-conduction/micro-nutils/micro.py
@@ -75,14 +75,13 @@ class MicroSimulation:
         target_porosity = 1 - math.pi * self._r_initial ** 2
         print("Target amount of void space = {}".format(target_porosity))
 
-        #dt_initial = 1E-3
-
         # Solve Allen-Cahn equation till we reach target porosity value
-        #psi = 0
-        #while psi < target_porosity:
-        #    print("Solving Allen-Cahn equation to achieve initial target grain structure")
-        #    solphi = self._solve_allen_cahn(self._topo, solphi, 0.5, dt_initial)
-        #    psi = self._get_avg_porosity(self._topo, solphi)
+        # dt_initial = 1E-3
+        # psi = 0
+        # while psi < target_porosity:
+        #     print("Solving Allen-Cahn equation to achieve initial target grain structure")
+        #     solphi = self._solve_allen_cahn(self._topo, solphi, 0.5, dt_initial)
+        #     psi = self._get_avg_porosity(self._topo, solphi)
 
         self._solphi = solphi  # Save solution of phi
         psi = self._get_avg_porosity(self._topo, solphi)
