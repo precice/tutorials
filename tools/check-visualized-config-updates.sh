eaec56a8cf0351cb4a712d17c896a3a77600f795
Gerasimos Chourdakis <chourdak@in.tum.de>
2024-04-09 20:10:19 +0200

    Add a script to check if precice-config.png files have been updated

---
+++ tools/check-visualized-config-updates.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Run this script at the root of the repository to check if any precice-config.png needs to be updated.
+# Requires that precice-config.png files have already been checked into the repository and that
+# visualize-configs.sh has been executed, e.g., in a CI job.
+
+set -e -u
+
+echo "Checking for any config visualizations that need to be updated..."
+
+if git status | grep precice-config.png; then
+  echo "There have been changes. Run ./tools/visualize-configs.sh to update the config visualizations."
+  exit 1
+else
+  echo "All visualizations seem to be up to date."
+fi
+
