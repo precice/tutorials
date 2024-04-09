eb3baae7f9b3a92b766dfb27920bb97d3c7be006
Gerasimos Chourdakis <chourdak@in.tum.de>
2024-04-09 19:15:32 +0200

    Create directory if it does not already exist

--- tools/visualize-configs.sh
+++ tools/visualize-configs.sh
@@ -6,12 +6,13 @@ set -e -u
 tutorials=$(find . -maxdepth 1 -type d -not -name ".*" | sed "s/^.\///")
 
 for tutorial in $tutorials; do
-  if [ "${tutorial}" = tools ] || [ "${tutorial}" = changelog-entries ] || [ "${tutorial}" = partitioned-heat-conduction-direct ] || [ "${tutorial}" = flow-over-heated-plate-nearest-projection ] || [ "${tutorial}" = flow-over-heated-plate-two-meshes ] || [ "${tutorial}" = partitioned-heat-conduction-complex ]; then
+  if [ "${tutorial}" = tools ] || [ "${tutorial}" = changelog-entries ] || [ "${tutorial}" = partitioned-heat-conduction-direct ]; then
     continue
   else
     echo "Visualizing the configuration in $tutorial"
     (
       cd "$tutorial"
+      mkdir -p images
       precice-config-visualizer precice-config.xml | dot -Tpng > "images/tutorials-$tutorial-precice-config.png"
     )
   fi
