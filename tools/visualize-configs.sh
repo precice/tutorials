141a3b529b5a4f2ca6544b7074933a4d68bc3801
Gerasimos Chourdakis <chourdak@in.tum.de>
2024-04-09 19:06:54 +0200

    Fix aste-turbine config

--- tools/visualize-configs.sh
+++ tools/visualize-configs.sh
@@ -6,7 +6,7 @@ set -e -u
 tutorials=$(find . -maxdepth 1 -type d -not -name ".*" | sed "s/^.\///")
 
 for tutorial in $tutorials; do
-  if [ "${tutorial}" = tools ] || [ "${tutorial}" = changelog-entries ] || [ "${tutorial}" = aste-turbine ] || [ "${tutorial}" = partitioned-heat-conduction-direct ] || [ "${tutorial}" = flow-over-heated-plate-nearest-projection ] || [ "${tutorial}" = flow-over-heated-plate-two-meshes ] || [ "${tutorial}" = partitioned-heat-conduction-complex ]; then
+  if [ "${tutorial}" = tools ] || [ "${tutorial}" = changelog-entries ] || [ "${tutorial}" = partitioned-heat-conduction-direct ] || [ "${tutorial}" = flow-over-heated-plate-nearest-projection ] || [ "${tutorial}" = flow-over-heated-plate-two-meshes ] || [ "${tutorial}" = partitioned-heat-conduction-complex ]; then
     continue
   else
     echo "Visualizing the configuration in $tutorial"
