7017353c546a619dad78e06fba491d7fbaacd207
Frédéric Simonis <simonisfrederic@gmail.com>
2024-04-29 12:19:25 +0200

    Directly use micro_manager in two-scale- tutorial

--- two-scale-heat-conduction/micro-nutils/run.sh
+++ two-scale-heat-conduction/micro-nutils/run.sh
@@ -13,16 +13,16 @@ pip install -r requirements.txt
 # Check if no input argument was provided
 if [ -z "$*" ] ; then
   echo "No input argument provided. Micro Manager is launched in serial"
-  python3 run_micro_manager.py
+  micro_manager micro-manager-config.json
 fi
 
 while getopts ":sp" opt; do
   case ${opt} in
   s)
-    python3 run_micro_manager.py
+    micro_manager micro-manager-config.json
     ;;
   p)
-    mpiexec -n "$2" python3 run_micro_manager.py
+    mpiexec -n "$2" micro_manager micro-manager-config.json
     ;;
   *)
     usage
