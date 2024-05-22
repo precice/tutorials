9d28c771242df3cfbcef27ec61eb5c02bbc38124
Ishaan Desai <ishaandesai@gmail.com>
2024-05-22 17:15:04 +0200

    Use Micro Manager executable micro-manager-precice to run the micro simulations

--- two-scale-heat-conduction/micro-dumux/run.sh
+++ two-scale-heat-conduction/micro-dumux/run.sh
@@ -9,16 +9,16 @@ usage() { echo "Usage: cmd [-s] [-p n]" 1>&2; exit 1; }
 # Check if no input argument was provided
 if [ -z "$*" ] ; then
   echo "No input argument provided. Micro Manager is launched in serial"
-  micro_manager micro-manager-config.json
+  micro-manager-precice micro-manager-config.json
 fi
 
 while getopts ":sp" opt; do
   case ${opt} in
   s)
-    micro_manager micro-manager-config.json
+    micro-manager-precice micro-manager-config.json
     ;;
   p)
-    mpiexec -n "$2" micro_manager micro-manager-config.json
+    mpiexec -n "$2" micro-manager-precice micro-manager-config.json
     ;;
   *)
     usage
