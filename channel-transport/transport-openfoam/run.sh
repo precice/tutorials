1d949e5ffc81d2b6d6a3f5f301177abacb6c279b
Frédéric Simonis <simonisfrederic@gmail.com>
2024-06-24 16:10:07 +0200

    It kind of runs

---
+++ channel-transport/transport-openfoam/run.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e -u
+
+. ../../tools/log.sh
+exec > >(tee --append "$LOGFILE") 2>&1
+
+blockMesh
+
+../../tools/run-openfoam.sh "$@"
+. ../../tools/openfoam-remove-empty-dirs.sh && openfoam_remove_empty_dirs
+
+close_log
