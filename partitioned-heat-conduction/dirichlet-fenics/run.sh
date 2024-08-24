2785a59dc6ea2d4f1d1d0115fc6c813ab7edfe70
TobiasEppacher <tobi.eppacher@gmail.com>
2024-08-24 11:29:55 +0200

    Unify run-script

--- partitioned-heat-conduction/dirichlet-fenics/run.sh
+++ partitioned-heat-conduction/dirichlet-fenics/run.sh
@@ -4,6 +4,17 @@ set -e -u
 . ../../tools/log.sh
 exec > >(tee --append "$LOGFILE") 2>&1
 
-python3 ../solver-fenics/heat.py Dirichlet
+if [$1 = "Default"]; then
+    echo "Running simulation with default FEniCS implementation"
+    python3 ../solver-fenics/heat.py Dirichlet
+else if [$1 = "HighOrder"]; then
+    echo "Running simulation with higher order FEniCS implementation"
+    python3 ../solver-fenics/heatHigherOrder.py Dirichlet
+else if [$1 = "SDC"]; then
+    echo "Running simulation with pySDC+FEniCS implementation"
+    python3 ../solver-fenics/heat_pySDC.py Dirichlet
+else
+    echo "Invalid argument. Please provide either 'Default', 'HighOrder', or 'SDC'"
+fi
 
 close_log
--- partitioned-heat-conduction/dirichlet-pySDC-fenics/run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e -u
-
-. ../../tools/log.sh
-exec > >(tee --append "$LOGFILE") 2>&1
-
-python3 ../solver-fenics/heat_pySDC.py Dirichlet
-
-close_log
