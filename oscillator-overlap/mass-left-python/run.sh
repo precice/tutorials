e1efdceb0654211d2dc13bf33d3f8a2b1de33012
Niklas <niklas.vinnitchenko@tum.de>
2024-08-31 15:44:39 +0200

    Add remaining requirements files and remove redundant requirements files

--- oscillator-overlap/mass-left-python/run.sh
+++ oscillator-overlap/mass-left-python/run.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 set -e -u
 
-python3 -m venv .venv
-. .venv/bin/activate
-pip install -r requirements.txt
-
 . ../../tools/log.sh
 exec > >(tee --append "$LOGFILE") 2>&1
+
+python3 -m venv ../solver-python/.venv
+. ../solver-python/.venv/bin/activate
+pip install -r ../solver-python/requirements.txt
 
 python3 ../solver-python/oscillator.py Mass-Left
 
