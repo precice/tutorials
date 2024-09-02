88873b34994347e596e86a62e01e38ee23f31162
Tobias Eppacher <90399710+TobiasEppacher@users.noreply.github.com>
2024-09-02 13:40:56 +0200

    pySDC & FEniCS solver for partitioned-heat-equation (#557)
    Add SDC time stepping as an option for FEniCS-bases partitioned heat conduction scenario.
    ---------
    
    Co-authored-by: Benjamin Rodenberg <benjamin.rodenberg@cit.tum.de>

--- partitioned-heat-conduction/neumann-fenics/run.sh
+++ partitioned-heat-conduction/neumann-fenics/run.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -e -u
 
+python3 -m venv --system-site-packages ../solver-fenics/.venv
+. ../solver-fenics/.venv/bin/activate
+pip install -r ../solver-fenics/requirements.txt
+
 . ../../tools/log.sh
 exec > >(tee --append "$LOGFILE") 2>&1
 
