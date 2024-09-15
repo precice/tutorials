c27d9ec91a274a0a0b47f8977e7b09b156d97132
Niklas <niklas.vinnitchenko@tum.de>
2024-09-15 20:35:28 +0200

    Add suggested changes

--- elastic-tube-3d/solid-fenics/run.sh
+++ elastic-tube-3d/solid-fenics/run.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -e -u
 
+. ../../tools/log.sh
+exec > >(tee --append "$LOGFILE") 2>&1
+
 python3 -m venv --system-site-package .venv
 . .venv/bin/activate
 pip install -r requirements.txt
