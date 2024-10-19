a67bad1d161f8c196b256f7f5fb6acf053b09a1e
NiklasV <139122850+NiklasVin@users.noreply.github.com>
2024-10-19 13:59:26 +0200

    Add missing `requirements.txt` and venvs for Python tutorials (#563)
    * Add requirements.txt files.
    * Update run.sh files.
    
    ---------
    
    Co-authored-by: Benjamin Rodenberg <benjamin.rodenberg@cit.tum.de>

--- channel-transport-reaction/chemical-fenics/run.sh
+++ channel-transport-reaction/chemical-fenics/run.sh
@@ -4,6 +4,10 @@ set -e -u
 . ../../tools/log.sh
 exec > >(tee --append "$LOGFILE") 2>&1
 
+python3 -m venv --system-site-package .venv
+. .venv/bin/activate
+pip install -r requirements.txt
+
 python3 chemical-reaction-advection-diffusion.py
 
 close_log
--- channel-transport-reaction/fluid-fenics/run.sh
+++ channel-transport-reaction/fluid-fenics/run.sh
@@ -4,6 +4,11 @@ set -e -u
 . ../../tools/log.sh
 exec > >(tee --append "$LOGFILE") 2>&1
 
+python3 -m venv --system-site-package .venv
+. .venv/bin/activate
+pip install -r requirements.txt
+
+
 python3 fluid.py
 
 close_log
