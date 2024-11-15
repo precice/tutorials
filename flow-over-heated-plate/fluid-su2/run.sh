bbd606f61f74f3144a07f1147661dc68c922aa51
Gerasimos Chourdakis <gerasimos.chourdakis@ipvs.uni-stuttgart.de>
2024-11-15 08:36:04 +0100

    Create a venv for SU2 tutorials

--- flow-over-heated-plate/fluid-su2/run.sh
+++ flow-over-heated-plate/fluid-su2/run.sh
@@ -1,4 +1,13 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 set -e -u
 
+. ../../tools/log.sh
+exec > >(tee --append "$LOGFILE") 2>&1
+
+python3 -m venv --system-site-packages .venv
+. .venv/bin/activate
+pip install -r requirements.txt
+
 SU2_preCICE_CHT.py -f laminar_config_unsteady.cfg -r --parallel
+
+close_log
--- perpendicular-flap/fluid-su2/run.sh
+++ perpendicular-flap/fluid-su2/run.sh
@@ -4,6 +4,10 @@ set -e -u
 . ../../tools/log.sh
 exec > >(tee --append "$LOGFILE") 2>&1
 
+python3 -m venv --system-site-packages .venv
+. .venv/bin/activate
+pip install -r requirements.txt
+
 SU2_preCICE_FSI.py -f euler_config_unsteady.cfg --parallel
 
 close_log
