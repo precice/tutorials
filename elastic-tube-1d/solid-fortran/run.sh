aa9247ebf49135ea1e4a959a96c9fae0fbc8e8a0
YonatanGM <girmayonatan86@gmail.com>
2025-01-08 12:00:05 +0100

    Get precice.f90 from the fortran module repo in run script

--- elastic-tube-1d/solid-fortran/run.sh
+++ elastic-tube-1d/solid-fortran/run.sh
@@ -4,6 +4,11 @@ set -e -u
 . ../../tools/log.sh || true
 exec > >(tee --append "$LOGFILE") 2>&1 || true
 
+if [ ! -f src/precice.f90 ]; then
+  echo "Fetching precice.f90 (Module for Fortran bindings of preCICE)..."
+  curl -o src/precice.f90 https://raw.githubusercontent.com/precice/fortran-module/master/precice.f90
+fi
+
 if [ ! -d build ]; then
   mkdir build
   cd build
