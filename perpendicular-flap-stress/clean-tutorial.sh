bd0610f1174fb5658043141141b332886348b98d
Benjamin Uekermann <benjamin.uekermann@ipvs.uni-stuttgart.de>
2025-03-24 10:16:55 +0100

    Replace explicit clean tutorial script with link

--- perpendicular-flap-stress/clean-tutorial.sh
+++ perpendicular-flap-stress/clean-tutorial.sh
@@ -1,10 +1,1 @@
-#!/usr/bin/env sh
-set -e -u
-
-# shellcheck disable=SC1091
-. ../tools/cleaning-tools.sh
-
-clean_tutorial .
-clean_precice_logs .
-rm -fv ./*.log
-rm -fv ./*.vtu
+../tools/clean-tutorial-base.sh
