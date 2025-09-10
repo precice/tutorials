4b0ae69c170d8bd7ea05881d79f9ea8fb9f9cb2f
Crazy-Rich-Meghan <delft2021li@gmail.com>
2025-09-11 00:24:52 +0200

    Direct mesh access version for partitioned heat conduction

---
+++ partitioned-heat-conduction-direct/dirichlet-gismo/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e -u
+
+. ../../tools/log.sh
+exec > >(tee --append "$LOGFILE") 2>&1
+
+partitioned-heat-conduction -s 0 ../precice-config.xml --plot
+
+close_log
---
+++ partitioned-heat-conduction-direct/neumann-gismo/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e -u
+
+. ../../tools/log.sh
+exec > >(tee --append "$LOGFILE") 2>&1
+
+partitioned-heat-conduction -s 1 ../precice-config.xml --plot
+
+close_log
