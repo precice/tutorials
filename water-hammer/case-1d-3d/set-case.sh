e02da82d0fa21784c1b333c65adedda9d1801d29
Fran <franmartinezquiles@gmail.com>
2025-10-20 17:03:15 +0200

    STARTED AGAIN TUTORIAL

---
+++ water-hammer/case-1d-3d/set-case.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ "$1" = "1d3d" ]; then
+    ln -sf precice-config-1d-3d.xml precice-config.xml
+    echo "Switched to 1D–3D configuration."
+elif [ "$1" = "3d1d" ]; then
+    ln -sf precice-config-3d-1d.xml precice-config.xml
+    echo "Switched to 3D–1D configuration."
+else
+    echo "Usage: ./set-case.sh [1d3d|3d1d]"
+fi
+
