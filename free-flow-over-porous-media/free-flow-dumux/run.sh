40d03ef372b8299c6dcc4413cfe3c162a2f85a36
Jun Chen <jun.chen@ipvs.uni-stuttgart.de>
2025-11-10 14:13:08 +0100

    remove echo message in the script

--- free-flow-over-porous-media/free-flow-dumux/run.sh
+++ free-flow-over-porous-media/free-flow-dumux/run.sh
@@ -4,7 +4,6 @@ set -e -u
 . ../../tools/log.sh
 exec > >(tee --append "$LOGFILE") 2>&1
 
-echo "Free flow solver is launched in serial."
 ./free_flow_dumux params.input
 
 close_log
--- free-flow-over-porous-media/porous-media-dumux/run.sh
+++ free-flow-over-porous-media/porous-media-dumux/run.sh
@@ -4,7 +4,6 @@ set -e -u
 . ../../tools/log.sh
 exec > >(tee --append "$LOGFILE") 2>&1
 
-echo "Porous medium solver is launched in serial."
 ./porous_media_dumux params.input
 
 close_log
