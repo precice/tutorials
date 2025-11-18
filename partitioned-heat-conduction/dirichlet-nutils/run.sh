d7dcb3585629c966ffc34b51fe237eb3c948d29c
Gerasimos Chourdakis <gerasimos.chourdakis@ipvs.uni-stuttgart.de>
2025-11-18 17:55:41 +0100

    Python tutorials: Allow skipping venv setup (#680)
    closes #679

--- partitioned-heat-conduction/dirichlet-nutils/run.sh
+++ partitioned-heat-conduction/dirichlet-nutils/run.sh
@@ -4,9 +4,12 @@ set -e -u
 . ../../tools/log.sh
 exec > >(tee --append "$LOGFILE") 2>&1
 
-python3 -m venv .venv
-. .venv/bin/activate
-pip install -r requirements.txt && pip freeze > pip-installed-packages.log
+if [ ! -v PRECICE_TUTORIALS_NO_VENV ]
+then
+    python3 -m venv .venv
+    . .venv/bin/activate
+    pip install -r requirements.txt && pip freeze > pip-installed-packages.log
+fi
 
 rm -rf Dirichlet-*.vtk
 NUTILS_RICHOUTPUT=no python3 heat.py
