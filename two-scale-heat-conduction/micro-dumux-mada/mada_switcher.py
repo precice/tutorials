dae606f5262a6fae8c14362e74e3e5680d03a26d
Alex Hocks <alex.hocks@tum.de>
2025-12-01 20:00:21 +0100

    update to new mm version

--- two-scale-heat-conduction/micro-dumux-mada/mada_switcher.py
+++ two-scale-heat-conduction/micro-dumux-mada/mada_switcher.py
@@ -1,19 +1,20 @@
 import numpy as np
 
 
-def switching_function(resolutions, locations, t, inputs, prev_output, active):
-    output = np.zeros_like(resolutions)
+def switching_function(resolution, location, t, input, prev_output):
+    result = 0
     # in the beginning we only want FOM
     if t == 0.0:
-        output[(resolutions > 0) * active] = -1
+        if resolution > 0: result = -1
+        else: result = 0
     # after small init phase, we want dynamic model selection
     # for test purposes we say ROM is accurate in range
     else:
-        concentrations = np.array([d['concentration'] for d in inputs])
-        valid_range_mask = (concentrations > 0.45) * (concentrations < 0.55)
-        fom_mask = resolutions == 0
+        concentration = input['concentration']
+        is_valid_range = 0.45 < concentration < 0.55
+        is_fom = resolution == 0
 
-        output[fom_mask * valid_range_mask * active] = 1
-        output[(~fom_mask) * (~valid_range_mask) * active] = -1
+        if is_fom and is_valid_range: result = 1
+        if not is_fom and not is_valid_range: result = -1
 
-    return output
+    return result
