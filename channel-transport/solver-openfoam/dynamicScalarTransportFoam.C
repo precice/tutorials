719fca6df9da7581176d27c5fb78a12434367ddf
Frédéric Simonis <simonisfrederic@gmail.com>
2025-12-10 16:54:22 +0100

    Replace header

--- channel-transport/solver-openfoam/dynamicScalarTransportFoam.C
+++ channel-transport/solver-openfoam/dynamicScalarTransportFoam.C
@@ -1,59 +1,10 @@
-/*---------------------------------------------------------------------------*\
-  =========                 |
-  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
-   \\    /   O peration     |
-    \\  /    A nd           | www.openfoam.com
-     \\/     M anipulation  |
--------------------------------------------------------------------------------
-    Copyright (C) 2011-2017 OpenFOAM Foundation
--------------------------------------------------------------------------------
-License
-    This file is part of OpenFOAM.
-
-    OpenFOAM is free software: you can redistribute it and/or modify it
-    under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
-    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-    for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
-
-Application
-    dynamicScalarTransportFoam
-
-Group
-    grpBasicSolvers
-
-Description
-    Passive scalar transport equation solver.
-
-    \heading Solver details
-    The equation is given by:
-
-    \f[
-        \ddt{T} + \div \left(\vec{U} T\right) - \div \left(D_T \grad T \right)
-        = S_{T}
-    \f]
-
-    Where:
-    \vartable
-        T       | Passive scalar
-        D_T     | Diffusion coefficient
-        S_T     | Source
-    \endvartable
-
-    \heading Required fields
-    \plaintable
-        T       | Passive scalar
-        U       | Velocity [m/s]
-    \endplaintable
-
-\*---------------------------------------------------------------------------*/
+//
+// This file is based on the OpenFOAM example application scalarTransportFoam
+//
+// GitLab link: https://gitlab.com/openfoam/core/openfoam/-/tree/master/applications/solvers/basic/scalarTransportFoam
+//
+// This modified version doesn't expect a static velocity field U, instead, it repomputes it every time step.
+// Therefore it allows the preCICE adapter to provide the velocities.
 
 #include "fvCFD.H"
 #include "fvOptions.H"
@@ -64,7 +15,7 @@ Description
 int main(int argc, char *argv[])
 {
   argList::addNote(
-      "Passive scalar transport equation solver.");
+      "Dynamic scalar transport equation solver.");
 
 #include "addCheckCaseOptions.H"
 #include "createMesh.H"
