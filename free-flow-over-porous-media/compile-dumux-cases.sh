7e9a2610446d57af6f66346a6dd784aec5846b8a
Jun Chen <jun.chen@ipvs.uni-stuttgart.de>
2025-12-15 15:29:09 +0100

    rename folder for compiling case

--- free-flow-over-porous-media/compile-dumux-cases.sh
+++ free-flow-over-porous-media/compile-dumux-cases.sh
@@ -2,15 +2,15 @@
 set -e -u
 
 (
-cd free-flow-dumux/build-cmake/appl
+cd free-flow-dumux/build-cmake/solver-dumux
 make free_flow_dumux
 )
 
 (
-cd porous-media-dumux/build-cmake/appl
+cd porous-media-dumux/build-cmake/solver-dumux
 make porous_media_dumux
 )
 
 # Move free-flow-dumux and porous-media-dumux executables to the participant folder level
-mv free-flow-dumux/build-cmake/appl/free_flow_dumux free-flow-dumux/
-mv porous-media-dumux/build-cmake/appl/porous_media_dumux porous-media-dumux/
+mv free-flow-dumux/build-cmake/solver-dumux/free_flow_dumux free-flow-dumux/
+mv porous-media-dumux/build-cmake/solver-dumux/porous_media_dumux porous-media-dumux/
