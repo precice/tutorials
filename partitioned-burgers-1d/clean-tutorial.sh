20cab482cb6d4d470088964268a81befd1915936
vidulejs <vidulejs2@gmail.com>
2025-12-15 14:23:29 +0100

    clean.sh to clean-tutorial.sh

--- partitioned-burgers-1d/clean-tutorial.sh
+++ partitioned-burgers-1d/clean-tutorial.sh
@@ -1,1 +1,16 @@
-../tools/clean-tutorial-base.sh
+#!/usr/bin/env sh
+set -e -u
+
+# shellcheck disable=SC1091
+. ../tools/cleaning-tools.sh
+
+clean_tutorial .
+clean_precice_logs .
+rm -fv ./*.log
+rm -fv ./*.vtu
+
+rm -f solver-scipy/full_domain.npz
+rm -f dirichlet-scipy/dirichlet.npz
+rm -f neumann-scipy/neumann.npz
+rm -rf output/
+rm -f initial_condition.npz
