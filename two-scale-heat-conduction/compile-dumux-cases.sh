8b99412dc2cb7a6add34a1c2d16fcd5e66b88118
June <94080048+Fujikawas@users.noreply.github.com>
2026-01-26 16:33:10 +0100

    Improve compilation script for cases with DuMux (#701)
    Allow to compile the DuMux solvers from scratch against other DUNE-modules.

--- two-scale-heat-conduction/compile-dumux-cases.sh
+++ two-scale-heat-conduction/compile-dumux-cases.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env sh
 set -e -u
 
-cd macro-dumux/build-cmake/appl
-make macro_dumux
-cd ../../../micro-dumux/build-cmake/appl
-make
-cd ../../../
+# To compile macro-dumux and micro-dumux from scratch or recompile them after changes
+./dune-common/bin/dunecontrol --opts=./dumux/cmake.opts --only=macro_dumux all
+./dune-common/bin/dunecontrol --opts=./dumux/cmake.opts --only=micro_sim all
+
+# Alternatively, you can manually recompile macro-dumux and micro-dumux when the `build-cmake` folder is present by uncommenting the following lines:
+# (
+# cd macro-dumux/build-cmake/appl
+# make macro_dumux
+# )
+# (
+# cd micro-dumux/build-cmake/appl
+# make micro_sim
+# )
 
 # Move macro-dumux and micro-dumux executables to the participant folder level
 mv macro-dumux/build-cmake/appl/macro_dumux macro-dumux/
-mv micro-dumux/build-cmake/appl/micro_sim.cpython-310-x86_64-linux-gnu.so micro-dumux/
+mv micro-dumux/build-cmake/appl/micro_sim.cpython-*.so micro-dumux/
