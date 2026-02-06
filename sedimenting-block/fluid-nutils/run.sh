f6b6f1ff9c5168f824f81dd37305285a920c5485
David Schneider <david.schneider@ipvs.uni-stuttgart.de>
2026-02-06 09:52:04 +0100

    Copy over from channel transport

---
+++ sedimenting-block/fluid-nutils/run.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e -u
+
+. ../../tools/log.sh
+exec > >(tee --append "$LOGFILE") 2>&1
+
+if [ ! -v PRECICE_TUTORIALS_NO_VENV ]
+then
+    python3 -m venv .venv
+    . .venv/bin/activate
+    pip install -r requirements.txt && pip freeze > pip-installed-packages.log
+fi
+
+python3 fluid.py
+
+close_log
