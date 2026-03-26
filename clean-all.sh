5da2340a8971e90a38b2364c7fc91854d6075002
Gerasimos Chourdakis <gerasimos.chourdakis@ipvs.uni-stuttgart.de>
2026-03-26 14:25:52 +0100

    Revert "Make clean-tutorial.sh executable from any directory (#716)"
    This reverts commit 0c910008ee65b8f87371c2e3b0d5ff269e017283.

--- clean-all.sh
+++ clean-all.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env sh
 set -e -u
 
-# Always operate relative to the directory of this script
-SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-cd "$SCRIPT_DIR"
-
 echo "Cleaning up all tutorials..."
 
 find . -maxdepth 2 -mindepth 2 -name clean-tutorial.sh -execdir sh -c './clean-tutorial.sh' \;
