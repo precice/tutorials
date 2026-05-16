aec6e5b374c75a060bf39fce206130ac1a4f852c
Gerasimos Chourdakis <gerasimos.chourdakis@ipvs.uni-stuttgart.de>
2026-05-16 22:07:54 +0200

    Fix a few more --system-site-package[s] typos

--- channel-transport-reaction/chemical-fenics/run.sh
+++ channel-transport-reaction/chemical-fenics/run.sh
@@ -6,7 +6,7 @@ exec > >(tee --append "$LOGFILE") 2>&1
 
 if [ ! -v PRECICE_TUTORIALS_NO_VENV ]
 then
-    python3 -m venv --system-site-package .venv
+    python3 -m venv --system-site-packages .venv
     . .venv/bin/activate
     pip install -r requirements.txt && pip freeze > pip-installed-packages.log
 fi
