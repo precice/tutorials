9e7dc8927e064378557fbd9209daa267be4eb196
Gerasimos Chourdakis <gerasimos.chourdakis@ipvs.uni-stuttgart.de>
2026-05-27 21:11:42 +0200

    Migrate heat-exchanger/download-meshes.sh into the run.sh scripts

--- heat-exchanger/fluid-outer-openfoam/run.sh
+++ heat-exchanger/fluid-outer-openfoam/run.sh
@@ -4,6 +4,12 @@ set -e -u
 . ../../tools/log.sh
 exec > >(tee --append "$LOGFILE") 2>&1
 
+if [ ! -f constant/polyMesh/boundary ]; then
+  echo "Downloading and extracting the Outer-Fluid mesh..."
+  wget -nv -O - https://syncandshare.lrz.de/dl/fiEZRQ8rcVWRkoyZvANim1R1/polyMesh.org.tar.gz | tar -xzv -C constant
+  mv constant/polyMesh.org constant/polyMesh
+fi
+
 ../../tools/run-openfoam.sh "$@"
 . ../../tools/openfoam-remove-empty-dirs.sh && openfoam_remove_empty_dirs
 
