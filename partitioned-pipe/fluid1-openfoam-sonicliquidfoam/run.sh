76760b2add34e324cc256138e46c25b34cf9d64f
Gerasimos Chourdakis <gerasimos.chourdakis@ipvs.uni-stuttgart.de>
2026-05-28 08:53:45 +0200

    Make partitioned-pipe with sonicliquidfoam automatic

--- partitioned-pipe/fluid1-openfoam-sonicliquidfoam/run.sh
+++ partitioned-pipe/fluid1-openfoam-sonicliquidfoam/run.sh
@@ -4,6 +4,12 @@ set -e -u
 . ../../tools/log.sh
 exec > >(tee --append "$LOGFILE") 2>&1
 
+# The precice-config.xml has the PressureGradient field commented out.
+# That improves the results for the compressible case, so we un-comment it.
+cp -r ../precice-config.xml ../precice-config.xml.orig
+sed -i "s,\<!--,,g" ../precice-config.xml
+sed -i "s,\-->,,g" ../precice-config.xml
+
 blockMesh
 
 ../../tools/run-openfoam.sh "$@"
