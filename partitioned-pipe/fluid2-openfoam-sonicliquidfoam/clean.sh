286e6d3b79dbd2a9c36103e8c14b2d5817ee1b5d
Gerasimos Chourdakis <gerasimos.chourdakis@ipvs.uni-stuttgart.de>
2026-05-28 09:00:36 +0200

    Reverse the operation in the clean.sh

--- partitioned-pipe/fluid2-openfoam-sonicliquidfoam/clean.sh
+++ partitioned-pipe/fluid2-openfoam-sonicliquidfoam/clean.sh
@@ -4,3 +4,8 @@ set -e -u
 . ../../tools/cleaning-tools.sh
 
 clean_openfoam .
+
+if [ -f ../precice-config.xml.orig ]; then
+  echo "Restoring the precice-config.xml from precice-config.xml.orig"
+  cp -r ../precice-config.xml.orig ../precice-config.xml
+fi
