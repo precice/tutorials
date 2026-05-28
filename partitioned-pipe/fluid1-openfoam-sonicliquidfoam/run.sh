7fc78c13c769dd566da3f3c534602623f655c9b9
Gerasimos Chourdakis <gerasimos.chourdakis@ipvs.uni-stuttgart.de>
2026-05-28 08:57:29 +0200

    Guard against successive calls

--- partitioned-pipe/fluid1-openfoam-sonicliquidfoam/run.sh
+++ partitioned-pipe/fluid1-openfoam-sonicliquidfoam/run.sh
@@ -6,9 +6,12 @@ exec > >(tee --append "$LOGFILE") 2>&1
 
 # The precice-config.xml has the PressureGradient field commented out.
 # That improves the results for the compressible case, so we un-comment it.
-cp -r ../precice-config.xml ../precice-config.xml.orig
-sed -i "s,\<!--,,g" ../precice-config.xml
-sed -i "s,\-->,,g" ../precice-config.xml
+if [ ! -f ../precice-config.xml.orig ]; then
+  echo "Modifying the ../precice-config.xml to enable PressureGradient (see precice-config.xml.orig for the original)"
+  cp -r ../precice-config.xml ../precice-config.xml.orig
+  sed -i "s,\<!--,,g" ../precice-config.xml
+  sed -i "s,\-->,,g" ../precice-config.xml
+fi
 
 blockMesh
 
