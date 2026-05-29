9af04467947be7e9a0a50604ecbe2ac5924b9738
Gerasimos Chourdakis <gerasimos.chourdakis@ipvs.uni-stuttgart.de>
2026-05-29 23:44:36 +0200

    Handle race conditions in partitioned-heat-conduction OpenFOAM solver build

--- partitioned-heat-conduction/dirichlet-openfoam/run.sh
+++ partitioned-heat-conduction/dirichlet-openfoam/run.sh
@@ -4,10 +4,18 @@ set -e -u
 . ../../tools/log.sh
 exec > >(tee --append "$LOGFILE") 2>&1
 
+# Build the heatTransfer solver, if not found.
+# Race condition: when running both participants with OpenFOAM,
+# they both need the same solver to be built.
+# The first one builds it, the second one waits.
 if ! command -v heatTransfer > /dev/null 2>&1; then
-  echo "Building the heatTransfer OpenFOAM solver"
-  wclean ../solver-openfoam/
-  wmake ../solver-openfoam/
+  if [ ! -d "../solver-openfoam/Make/${WM_OPTIONS}" ]; then
+    echo "Building the heatTransfer OpenFOAM solver"
+    wmake ../solver-openfoam/
+  else
+    echo "The executable heatTransfer is not found, but the build directory ../solver-openfoam/Make/${WM_OPTIONS} was detected. A build is probably in progress, waiting 20s..."
+    sleep 20
+  fi
 fi
 
 blockMesh
