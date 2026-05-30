1f5b4f0e41919183d690c67e7c9c5dfa8fef1ab0
Gerasimos Chourdakis <gerasimos.chourdakis@ipvs.uni-stuttgart.de>
2026-05-30 11:39:03 +0200

    Move precice.f90 to thirdparty/

--- elastic-tube-1d/fluid-fortran-module/run.sh
+++ elastic-tube-1d/fluid-fortran-module/run.sh
@@ -4,11 +4,11 @@ set -e -u
 . ../../tools/log.sh
 exec > >(tee --append "$LOGFILE") 2>&1
 
-if [ ! -f src/precice.f90 ]; then
+if [ ! -f thirdparty/precice.f90 ]; then
   # Get the preCICE Fortran module. Switch the branch with ./run.sh <branch>.
   DEFAULT_BRANCH="main"
   echo "Fetching precice.f90 from  https://github.com/precice/fortran-module/tree/${1:-$DEFAULT_BRANCH}..."
-  curl -o src/precice.f90 "https://raw.githubusercontent.com/precice/fortran-module/${1:-$DEFAULT_BRANCH}/precice.f90"
+  curl --create-dirs -o thirdparty/precice.f90 "https://raw.githubusercontent.com/precice/fortran-module/${1:-$DEFAULT_BRANCH}/precice.f90"
 fi
 
 if [ ! -d build ]; then
--- elastic-tube-1d/solid-fortran-module/run.sh
+++ elastic-tube-1d/solid-fortran-module/run.sh
@@ -1,22 +1,20 @@
 #!/usr/bin/env bash
 set -e -u
 
-. ../../tools/log.sh || true
+. ../../tools/log.sh
 exec > >(tee --append "$LOGFILE") 2>&1 || true
 
-if [ ! -f src/precice.f90 ]; then
+if [ ! -f thirdparty/precice.f90 ]; then
   # Get the preCICE Fortran module. Switch the branch with ./run.sh <branch>.
   DEFAULT_BRANCH="main"
   echo "Fetching precice.f90 from  https://github.com/precice/fortran-module/tree/${1:-$DEFAULT_BRANCH}..."
-  curl -o src/precice.f90 "https://raw.githubusercontent.com/precice/fortran-module/${1:-$DEFAULT_BRANCH}/precice.f90"
+  curl --create-dirs -o thirdparty/precice.f90 "https://raw.githubusercontent.com/precice/fortran-module/${1:-$DEFAULT_BRANCH}/precice.f90"
 fi
 
 if [ ! -d build ]; then
   mkdir build
-  cd build
-  cmake ..
-  cmake --build .
-  cd ..
+  cmake -S . -B build
+  cmake --build build
 fi
 
 ./build/SolidSolver ../precice-config.xml
