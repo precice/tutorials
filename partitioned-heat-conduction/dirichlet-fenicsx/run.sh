9a15a411204e865f4d6da063163986bc1432a0ab
Gerasimos Chourdakis <gerasimos.chourdakis@ipvs.uni-stuttgart.de>
2026-06-11 20:24:23 +0200

    Fix more inconsistencies

--- partitioned-heat-conduction/dirichlet-fenicsx/run.sh
+++ partitioned-heat-conduction/dirichlet-fenicsx/run.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 set -e -u
 
-if [ ! -v PRECICE_TUTORIALS_NO_VENV ]
-then
-    if [ ! -d .venv ]; then
+if [ ! -v PRECICE_TUTORIALS_NO_VENV ]; then
+    if [ ! -d ".venv" ]; then
         python3 -m venv --system-site-packages .venv
+        source .venv/bin/activate
+        pip install -r ../solver-fenicsx/requirements.txt && pip freeze > pip-installed-packages.log
+    else
+        source .venv/bin/activate
     fi
-    . .venv/bin/activate
-    pip install -r ../solver-fenicsx/requirements.txt
 fi
 
 python3 ../solver-fenicsx/heat.py Dirichlet
