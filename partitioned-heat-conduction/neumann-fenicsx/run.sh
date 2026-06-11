79aabb10b0e6937a27cfd34b02d06762b43fe555
Gerasimos Chourdakis <gerasimos.chourdakis@ipvs.uni-stuttgart.de>
2026-06-11 20:17:49 +0200

    Update handling of venv in partitioned-heat-conduction fenicsx

--- partitioned-heat-conduction/neumann-fenicsx/run.sh
+++ partitioned-heat-conduction/neumann-fenicsx/run.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 set -e -u
 
-if [ ! -d .venv ]; then
+if [ ! -v PRECICE_TUTORIALS_NO_VENV ]
+then
+    if [ ! -d .venv ]; then
         python3 -m venv --system-site-packages .venv
     fi
-. .venv/bin/activate
-pip install -r ../solver-fenicsx/requirements.txt
+    . .venv/bin/activate
+    pip install -r ../solver-fenicsx/requirements.txt
+fi
+
 python3 ../solver-fenicsx/heat.py Neumann
