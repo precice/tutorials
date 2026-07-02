47e4964af1abdb80608fec8df8d4c10f0a7a42c2
Gerasimos Chourdakis <gerasimos.chourdakis@ipvs.uni-stuttgart.de>
2026-07-02 11:21:45 +0200

    Make partitioned-heat-conduction-direct/clean-tutorial.sh a link

--- partitioned-heat-conduction-direct/clean-tutorial.sh
+++ partitioned-heat-conduction-direct/clean-tutorial.sh
@@ -1,12 +1,1 @@
-#!/usr/bin/env sh
-set -e -u
-
-# Determine the tutorial directory (where the symlink is located)
-TUTORIAL_DIR="$(cd "$(dirname "$0")" && pwd)"
-cd "$TUTORIAL_DIR"
-
-# shellcheck disable=SC1091
-. ../tools/cleaning-tools.sh
-
-clean_tutorial .
-
+../tools/clean-tutorial-base.sh
