cafa40e70536fccaeefe7a6be8d291f6684a7fdf
Gerasimos Chourdakis <gerasimos.chourdakis@ipvs.uni-stuttgart.de>
2026-07-15 12:46:31 +0200

    Simplify script

--- tools/openfoam-remove-empty-dirs.sh
+++ tools/openfoam-remove-empty-dirs.sh
@@ -1,27 +1,33 @@
 #!/usr/bin/env bash
 
-# Cleaning up stray functionObjectProperties files, see https://github.com/precice/openfoam-adapter/issues/26
+# Cleaning up stray functionObjectProperties files to not polute the post-processed time steps,
+# see https://github.com/precice/openfoam-adapter/issues/26
 openfoam_remove_empty_dirs() {
 	(
-		set -e -u -pipefail
-		echo "Cleaning up any time directories without results"
+		set -e -u
+		echo "Cleaning up any time directories without results to make post-processing easier..."
 
-		for d in [0-9]* [0-9]*.[0-9]*; do
-                        if ! [ -f "${d}/U" ] && ! [ -f "${d}/T" ] && ! [ -f "${d}/U.gz" ] && ! [ -f "${d}/T.gz" ] && ! [ -f "${d}/D" ] && ! [ -f "${d}/pointD" ] && ! [ -f "${d}/DD" ] && ! [ -f "${d}/pointDD" ] && ! [ -f "${d}/D.gz" ] && ! [ -f "${d}/pointD.gz" ] && ! [ -f "${d}/DD.gz" ] && ! [ -f "${d}/pointDD.gz" ]; then
-				rm -rf "${d}"
-			fi
-		done
-		if [[ $(("ls | grep processor | wc -l")) -gt 0 ]]; then
-			for pd in processor*; do
-				cd "${pd}"
-				for d in [0-9]* [0-9]*.[0-9]*; do
-                                        if ! [ -f "${d}/U" ] && ! [ -f "${d}/T" ] && ! [ -f "${d}/U.gz" ] && ! [ -f "${d}/T.gz" ] && ! [ -f "${d}/D" ] && ! [ -f "${d}/pointD" ] && ! [ -f "${d}/DD" ] && ! [ -f "${d}/pointDD" ] && ! [ -f "${d}/D.gz" ] && ! [ -f "${d}/pointD.gz" ] && ! [ -f "${d}/DD.gz" ] && ! [ -f "${d}/pointDD.gz" ]; then
-						rm -rf "${d}"
+		DIRECTORIES_TO_CHECK=("." "processor"*)
+		OPENFOAM_RESULT_FILES=("p" "U" "T" "D" "pointD" "DD" "pointDD")
+
+		# Search the current and every processor* directory for common results files.
+		# If there are none, remove the directory.
+		for pd in "${DIRECTORIES_TO_CHECK[@]}"; do
+				for d in "${pd}"/[0-9]* "${pd}"/[0-9]*.[0-9]*; do
+					KEEP_DIRECTORY=false
+					for r in "${OPENFOAM_RESULT_FILES[@]}"; do
+						# OpenFOAM can be configured to store files either as compressed or uncompressed
+						if [[ -f "${d}/${r}" ]] || [[ -f "${d}/${r}.gz" ]]; then
+							KEEP_DIRECTORY=true
+							break;
+						fi
+					done
+					if ! [[ ${KEEP_DIRECTORY} == true ]]; then
+						# None of the expected result files found - delete the directory
+						rm -rfv "${d}"
 					fi
 				done
-				cd ..
-			done
-		fi
-                echo "Done."
+		done
+		echo "Done."
 	)
 }
