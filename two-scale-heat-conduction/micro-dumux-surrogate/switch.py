770dc187ccf3528e8d2cc4180c082679c74e7d96
Ishaan Desai <ishaandesai@gmail.com>
2026-08-07 14:09:52 +0200

    Give files appropriate names

---
+++ two-scale-heat-conduction/micro-dumux-surrogate/switch.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+def switching_function(resolution, location, t, input, prev_output):
+    result = 0
+    # Only use the full-order model for the first time step
+    if t == 0.0:
+        if resolution > 0:
+            result = -1
+        else:
+            result = 0
+    # after small init phase, we want dynamic model selection
+    # for test purposes we say ROM is accurate in range
+    else:
+        concentration = input['concentration']
+        is_valid_range = 0.45 < concentration < 0.55
+        is_fom = resolution == 0
+
+        if is_fom and is_valid_range:
+            result = 1
+        if not is_fom and not is_valid_range:
+            result = -1
+
+    return result
