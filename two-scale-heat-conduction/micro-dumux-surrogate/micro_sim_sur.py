67f602996d2b674a59f117cf3df175511faa684a
Ishaan Desai <ishaandesai@gmail.com>
2026-08-07 17:24:15 +0200

    Working state in folder micro-dumux-surrogate

--- two-scale-heat-conduction/micro-dumux-surrogate/micro_sim_sur.py
+++ two-scale-heat-conduction/micro-dumux-surrogate/micro_sim_sur.py
@@ -1,10 +1,6 @@
 """
 Micro simulation Surrogate, requrie previous computation of surrogate model
 """
-import os
-import subprocess
-from bayesvalidrox import PyLinkForwardModel, Input, PCE, ExpDesigns, Engine
-import h5py
 import joblib
 import numpy as np
 import math
@@ -14,7 +10,12 @@ class MicroSimulation:
 
     def __init__(self, sim_id):
         """
-        Constructor of MicroSimulation class.
+        Get the micro-scale model from the BayesValidRox surrogate model.
+
+        Parameters
+        ----------
+        sim_id : int
+            The simulation ID for the micro-scale simulation.
         """
         self._sim_id = sim_id
         self._state = None
@@ -27,30 +28,59 @@ class MicroSimulation:
 
     def initialize(self):
         output_data = dict()
-        output_data["k_00"] = 0.4912490635619572
-        output_data["k_11"] = 0.4912490635989945
-        output_data["porosity"] = 0.4933482661391027
+        output_data["K00"] = 0.4912490635619572
+        output_data["K11"] = 0.4912490635989945
+        output_data["Porosity"] = 0.4933482661391027
 
         if self._sim_id == 0:
-            output_data["k_00"] = 0.4912490640081466
-            output_data["k_11"] = 0.4912490640081367
+            output_data["K00"] = 0.4912490640081466
+            output_data["K11"] = 0.4912490640081367
 
         return output_data
 
     def get_state(self):
+        """
+        Get the current state of the micro-scale simulation.
+
+        Returns
+        -------
+        state : dict
+            The current state of the micro-scale simulation.
+        """
         return self._state
 
     def set_state(self, state):
+        """
+        Set the current state of the micro-scale simulation.
+
+        Parameters
+        ----------
+        state : dict
+            The state to set for the micro-scale simulation.
+        """
         self._state = state
 
     def solve(self, macro_data, dt):
-        model_eval, _ = self._model.eval_metamodel(np.array([macro_data["concentration"]])[:, np.newaxis])
+        """
+        Solve the micro-scale simulation using the surrogate model.
+
+        Parameters
+        ----------
+        macro_data : dict
+            The macro-scale data required for the micro-scale simulation.
+        dt : float
+            The time step for the micro-scale simulation.
+
+        Returns
+        -------
+        output_data : dict
+            The output data from the micro-scale simulation.
+        """
+        model_eval, _ = self._model.eval_metamodel(np.array([macro_data["Concentration"]])[:, np.newaxis])
         output_data = dict()
-        output_data["k_00"] = model_eval["k_00"][0][0]
-        output_data["k_01"] = model_eval["k_01"][0][0]
-        output_data["k_10"] = model_eval["k_10"][0][0]
-        output_data["k_11"] = model_eval["k_11"][0][0]
-        output_data["porosity"] = model_eval["porosity"][0][0]
-        output_data["grain_size"] = math.sqrt((1 - model_eval["porosity"][0][0]) / math.pi)
+        output_data["K00"] = model_eval["K00"][0][0]
+        output_data["K11"] = model_eval["K11"][0][0]
+        output_data["Porosity"] = model_eval["Porosity"][0][0]
+        output_data["grain_size"] = math.sqrt((1 - model_eval["Porosity"][0][0]) / math.pi)
 
         return output_data
--- two-scale-heat-conduction/micro-dumux-surrogate/surrogate_workflow.py
+++ two-scale-heat-conduction/micro-dumux-surrogate/surrogate_workflow.py
@@ -10,49 +10,43 @@ import matplotlib.pyplot as plt
 def create_snapshots() -> None:
     """
     Create snapshots of the DuMuX model in micro-dumux/ using the Micro Manager.
-    The snapshots are saved in the specified directory.
     """
     # What effect do uniformly distributed concentration samples have on the quality of the surrogate model?
     concentration_samples = np.linspace(0.0, 0.9, 500)
 
-    with h5py.File("input_samples.hdf5", "w") as f:
-        f.create_dataset("concentration", data=concentration_samples)
+    # The name of the HDF5 file should be the same as the parameter_file_name in micro-manager-snapshot-config.json
+    with h5py.File("macro-concentration-samples.hdf5", "w") as f:
+        f.create_dataset("Concentration", data=concentration_samples)
 
     # Run the Micro Manager to create snapshot database
-    subprocess.call('micro-manager-precice --snapshot micro-manager-snapshot-config.json', shell=True)
+    subprocess.run(
+        ["micro-manager-precice", "--snapshot", "micro-manager-snapshot-config.json"],
+        check=True,
+    )
 
 
-def read_snapshots(snapshots_dir: str) -> tuple:
+def read_snapshots() -> tuple:
     """
     Read the snapshots created by the Micro Manager and return the inputs and outputs.
     The inputs are the concentration samples, and the outputs are the porosity and conductivity matrix values.
-
-    Parameters
-    ----------
-    snapshots_dir : str
-        The directory where the snapshots are stored.
 
     Returns
     -------
     tuple
         A tuple containing the inputs (concentration samples) and outputs (porosity and conductivity values).
     """
-    with h5py.File(os.path.join(snapshots_dir, "snapshot_data.hdf5"), "r") as f:
-        concentration_data = f["concentration"][:]
-        porosity_data = f["porosity"][:]
-        k_00_data = f["k_00"][:]
-        k_01_data = f["k_01"][:]
-        k_10_data = f["k_10"][:]
-        k_11_data = f["k_11"][:]
+    with h5py.File("micro-dumux-snapshots.hdf5", "r") as f:
+        concentration_data = f["Concentration"][:]
+        porosity_data = f["Porosity"][:]
+        k_00_data = f["K00"][:]
+        k_11_data = f["K11"][:]
 
     concentration = np.swapaxes(np.array([concentration_data]), 0, 1)
     porosity = np.swapaxes(np.array([porosity_data]), 0, 1)
     k_00 = np.swapaxes(np.array([k_00_data]), 0, 1)
-    k_01 = np.swapaxes(np.array([k_01_data]), 0, 1)
-    k_10 = np.swapaxes(np.array([k_10_data]), 0, 1)
     k_11 = np.swapaxes(np.array([k_11_data]), 0, 1)
 
-    outputs = {"porosity": porosity, "k_00": k_00, "k_01": k_01, "k_10": k_10, "k_11": k_11, "x_values": np.array([0])}
+    outputs = {"Porosity": porosity, "K00": k_00, "K11": k_11, "x_values": np.array([0])}
     return concentration, outputs
 
 
@@ -102,10 +96,14 @@ def split_samples(X, y, n_valid):
     return X_train, y_train, X_valid, y_valid
 
 
-def create_surrogate(snapshots_dir: str) -> tuple:
+def create_surrogate() -> tuple:
     """
     Create a surrogate model from the Micro Manager snapshots.
 
+    Returns
+    -------
+    tuple
+        A tuple containing the validation samples and corresponding model evaluations.
     """
     # We create a fake model from model.py because we directly provide the
     # input and outputs from the Micro Manager snapshots.
@@ -113,16 +111,16 @@ def create_surrogate(snapshots_dir: str) -> tuple:
     model.py_file = "model"
     model.name = "micro-dumux-surrogate"
     model.link_type = "function"
-    model.output_names = ["porosity", "k_00", "k_01", "k_10", "k_11"]
+    model.output_names = ["Porosity", "K00", "K11"]
 
-    x, y = read_snapshots(snapshots_dir)
+    x, y = read_snapshots()
 
     # Split the samples into training and validation sets
     n_valid = 200
     x_train, y_train, x_valid, y_valid = split_samples(x, y, n_valid)
 
     inputs = Input()
-    inputs.add_marginals(name="concentration", dist_type="unif", parameters=[0, 0.5])
+    inputs.add_marginals(name="Concentration", dist_type="unif", parameters=[0, 0.5])
 
     exp_design = ExpDesigns(inputs)
     exp_design.x = x_train
@@ -144,7 +142,7 @@ def create_surrogate(snapshots_dir: str) -> tuple:
     return x_valid, y_valid
 
 
-def validate_surrogate(x_valid, y_valid, model_name="micro-dumux-surrogate.pkl"):
+def validate_surrogate(x_valid, y_valid, model_name="micro-dumux-surrogate.pkl") -> None:
     """
     Validate the surrogate model using the validation samples and outputs.
 
@@ -166,31 +164,29 @@ def validate_surrogate(x_valid, y_valid, model_name="micro-dumux-surrogate.pkl")
 
     # Compare predictions with true values
     plt.figure()
-    plt.scatter(y_valid["porosity"], y_metamod["porosity"])
+    plt.scatter(y_valid["Porosity"], y_metamod["Porosity"])
     plt.xlabel("True Values")
     plt.ylabel("Predictions")
     plt.title(f"Validation: porosity")
     plt.plot([0.4, 1.1], [0.4, 1.1], "k--")
     plt.xlim(0.4, 1.1)
     plt.ylim(0.4, 1.1)
-    plt.show()
+    plt.savefig("surrogate-validation-porosity.pdf", bbox_inches="tight")
 
     plt.figure()
-    plt.scatter(x_valid, y_valid["porosity"])
-    plt.xlabel("valid concentration")
-    plt.ylabel("valid porosity")
-    plt.title("IO: con to poro")
-    plt.show()
+    plt.scatter(x_valid, y_valid["Porosity"])
+    plt.xlabel("Validation Concentration")
+    plt.ylabel("Validation Porosity")
+    plt.title("IO: Concentration to Porosity")
+    plt.savefig("surrogate-validation-concentration-porosity.pdf", bbox_inches="tight")
 
 
 def main():
-    snapshots_dir = "output"
-    if not os.path.exists(snapshots_dir):
-        os.makedirs(snapshots_dir)
-
-    # os.chdir(snapshots_dir)
+    """
+    Create snaphots of the DuMuX model, create a surrogate model from the snapshots, and validate the surrogate model.
+    """
     create_snapshots()
-    x_valid, y_valid = create_surrogate(snapshots_dir)
+    x_valid, y_valid = create_surrogate()
     validate_surrogate(x_valid, y_valid)
 
 
