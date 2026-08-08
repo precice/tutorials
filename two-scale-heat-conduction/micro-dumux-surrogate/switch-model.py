60f3ad2b2540cb521da3ee2ac2bd5225b8fbc2b1
Ishaan Desai <ishaandesai@gmail.com>
2026-08-08 12:26:32 +0200

    Changing switching function file name

--- two-scale-heat-conduction/micro-dumux-surrogate/switch-model.py
+++ two-scale-heat-conduction/micro-dumux-surrogate/switch-model.py
@@ -1,24 +1,48 @@
 import numpy as np
+from typing import Dict
 
 
-def switching_function(resolution, location, t, input, prev_output):
-    result = 0
+def switching_function(model_res: int, location: np.ndarray, t: float, input: Dict, prev_output: Dict):
+    """
+    Determine which model to use based on the resolution, location, time, input, and previous output.
+
+    Parameters
+    ----------
+    model_res : int
+        The resolution level of the model (0 for full-order model, >0 for reduced-order model).
+    location : np.ndarray
+        The macro-scale coordinates corresponding to the micro-scale simulation.
+    t : float
+        The current time step.
+    input : Dict
+        The input data for the model, including concentration.
+    prev_output : dict
+        The output from the previous time step.
+
+    Returns
+    -------
+    model_res_offset : int
+        The offset to change the model used from the provided hierarchy of models.
+    """
+    model_res_offset = 0
+
     # Only use the full-order model for the first time step
     if t == 0.0:
-        if resolution > 0:
-            result = -1
+        if model_res > 0:
+            model_res_offset = -1
         else:
-            result = 0
-    # after small init phase, we want dynamic model selection
-    # for test purposes we say ROM is accurate in range
+            model_res_offset = 0
+
+    # After the first time step, the model is selected dynamically based on
+    # the concentration value and the model resolution.
     else:
-        concentration = input['concentration']
-        is_valid_range = 0.45 < concentration < 0.55
-        is_fom = resolution == 0
+        concentration = input["Concentration"]
+        is_valid_range = 0.2 < concentration < 0.3
+        is_fom = model_res == 0
 
         if is_fom and is_valid_range:
-            result = 1
+            model_res_offset = 1
         if not is_fom and not is_valid_range:
-            result = -1
+            model_res_offset = -1
 
-    return result
+    return model_res_offset
