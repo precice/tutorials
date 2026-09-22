98d08ea0957a089a5611e83d61dd3f9509a5e2c1
Gerasimos Chourdakis <gerasimos.chourdakis@ipvs.uni-stuttgart.de>
2026-09-22 16:27:07 +0200

    Add warning in  solid-openfoam and remove decomposeParDict

--- perpendicular-flap/solid-openfoam/run.sh
+++ perpendicular-flap/solid-openfoam/run.sh
@@ -3,6 +3,9 @@
 . ../../tools/log.sh
 exec > >(tee --append "$LOGFILE") 2>&1
 
+echo "[WARNING] This case is only provided for quick testing, it gives significantly different results than other cases in this tutorial, and does not work in parallel. See solid-solids4foam for a better OpenFOAM-based solid case.  Discussion on https://github.com/precice/tutorials/issues/515."
+sleep 5
+
 blockMesh
 
 # Compile boundary condition
